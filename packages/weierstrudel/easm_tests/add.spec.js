c52cb44d338a2a2925e89dac756e049035af3e8a
zac-williamson <blorktronics@gmail.com>
2018-12-08 11:22:07 +0000

    updated addition macros

--- packages/weierstrudel/easm_tests/add.spec.js
+++ packages/weierstrudel/easm_tests/add.spec.js
@@ -1,8 +1,9 @@
-const Runtime = require('../parser/runtime');
-const bn128Reference = require('../js_snippets/bn128_reference');
 const chai = require('chai');
 
-const { expect, assert } = chai;
+const Runtime = require('../parser/runtime');
+const bn128Reference = require('../js_snippets/bn128_reference');
+
+const { expect } = chai;
 
 const testHelper = `
 #include "./easm_modules/add.easm"
@@ -11,7 +12,17 @@ const testHelper = `
 #define Y2 = takes(0) returns(1) { 0x00 }
 #define PRECOMPUTE_TABLE_ADD_IMPL = takes(3) returns(11) {
     PRECOMPUTE_TABLE_ADD<P,P,P,X2,Y2>()
-}`;
+}
+
+#define P2_LOCATION_TEST = takes(0) returns(3) {
+    0x00
+    0x20
+}
+
+#define ADD_MAIN_IMPL = takes(3) returns(3) {
+    ADD_MAIN<P2_LOCATION_TEST,P,2P>()
+}
+`;
 
 
 describe('bn128 add', () => {
@@ -24,7 +35,6 @@ describe('bn128 add', () => {
         const { x: x1, y: y1, z: z1 } = bn128Reference.randomPointJacobian();
         const { zz, zzz } = bn128Reference.zFactors({ x2, y2 }, { x1, y1, z1 });
 
-        const { x, y, z } = bn128Reference.randomPointJacobian();
         const initialMemory = [{
             index: 32,
             value: bn128Reference.p.sub(x2),
@@ -33,7 +43,7 @@ describe('bn128 add', () => {
             value: y2,
         }];
         const reference = bn128Reference.mixedAdd(x2, y2, x1, y1, z1);
-        const { stack, memory } = await add('PRECOMPUTE_TABLE_ADD_IMPL', [x1, bn128Reference.p.sub(y1), z1], initialMemory);
+        const { stack } = await add('PRECOMPUTE_TABLE_ADD_IMPL', [x1, bn128Reference.p.sub(y1), z1], initialMemory);
         expect(stack.length).to.equal(11);
         const [x1Out, y1Out, pA, zzzOut, pB, pC, zzOut, pD, x3, y3, z3] = stack;
         expect(pA.eq(bn128Reference.p)).to.equal(true);
@@ -47,6 +57,26 @@ describe('bn128 add', () => {
         expect(z3.umod(pA).eq(reference.z)).to.equal(true);
         expect(zzOut.umod(pA).eq(zz)).to.equal(true);
         expect(zzzOut.umod(pA).eq(zzz)).to.equal(true);
-    
+    });
+
+    it('macro ADD_MAIN correctly caluclates mixed coordinate point addition', async () => {
+        const { x: x2, y: y2 } = bn128Reference.randomPoint();
+        const { x: x1, y: y1, z: z1 } = bn128Reference.randomPointJacobian();
+        const reference = bn128Reference.mixedAdd(x2, y2, x1, y1, z1);
+        const y1Neg = bn128Reference.p.sub(y1);
+        const x2Neg = bn128Reference.p.sub(x2);
+        const initialMemory = [{
+            index: 0,
+            value: x2Neg,
+        }, {
+            index: 32,
+            value: y2,
+        }];
+        const { stack } = await add('ADD_MAIN_IMPL', [x1, y1Neg, z1], initialMemory);
+        expect(stack.length).to.equal(3);
+        const [x3, y3, z3] = stack;
+        expect(x3.umod(bn128Reference.p).eq(reference.x)).to.equal(true);
+        expect(y3.umod(bn128Reference.p).eq(bn128Reference.p.sub(reference.y))).to.equal(true);
+        expect(z3.umod(bn128Reference.p).eq(reference.z)).to.equal(true);
     });
 });
