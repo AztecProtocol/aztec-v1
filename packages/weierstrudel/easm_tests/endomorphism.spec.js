34691d9e5410ae56cb33e1e82457fac3515ceeb0
zac-williamson <blorktronics@gmail.com>
2018-12-08 11:23:10 +0000

    updated endomorphism scalar decomposition macro

--- packages/weierstrudel/easm_tests/endomorphism.spec.js
+++ packages/weierstrudel/easm_tests/endomorphism.spec.js
@@ -1,11 +1,28 @@
-const Runtime = require('../parser/runtime');
-const { n, lambda } = require('../js_snippets/bn128_reference');
 const chai = require('chai');
-const referenceWnaf = require('../js_snippets/wnaf_reference_implementation');
 const BN = require('bn.js');
 const crypto = require('crypto');
+const EC = require('elliptic');
 
-const { expect, assert } = chai;
+const Runtime = require('../parser/runtime');
+const {
+    n,
+    lambda,
+    p,
+    beta,
+    randomPoint,
+} = require('../js_snippets/bn128_reference');
+
+const { expect } = chai;
+
+// eslint-disable-next-line new-cap
+const referenceCurve = new EC.curve.short({
+    a: '0',
+    b: '3',
+    p: p.toString(16),
+    n: n.toString(16),
+    gRed: false,
+    g: ['1', '2'],
+});
 
 describe('endomorphism split', () => {
     let endomorphism;
@@ -13,11 +30,25 @@ describe('endomorphism split', () => {
         endomorphism = new Runtime('./easm_modules/endomorphism.easm');
     });
     it('macro ENDOMORPHISM correctly splits scalar k into half-length scalars k1, k2', async () => {
-        let k = new BN(crypto.randomBytes(32), 16);
+        const k = new BN(crypto.randomBytes(32), 16).umod(n);
         const { stack } = await endomorphism('ENDOMORPHISM', [k]);
         expect(stack.length).to.equal(2);
         expect(stack[1].sub(stack[0].mul(lambda)).umod(n).eq(k.umod(n))).to.equal(true);
         expect(stack[1].bitLength() <= 128).to.equal(true);
         expect(stack[0].bitLength() <= 128).to.equal(true);
+
+        const pointData = randomPoint();
+
+        const point = referenceCurve.point(pointData.x, pointData.y);
+
+        const endoPoint = referenceCurve.point(
+            pointData.x.mul(beta).umod(p),
+            p.sub(pointData.y)
+        );
+
+        const expected = point.mul(k);
+
+        const result = point.mul(stack[1]).add(endoPoint.mul(stack[0]));
+        expect(result.eq(expected)).to.equal(true);
     });
 });
