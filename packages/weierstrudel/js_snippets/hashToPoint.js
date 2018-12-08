c1e47bf1c431f9c4e604614efd565fc7e229dedd
zac-williamson <blorktronics@gmail.com>
2018-12-08 11:24:24 +0000

    eslint fixes

--- packages/weierstrudel/js_snippets/hashToPoint.js
+++ packages/weierstrudel/js_snippets/hashToPoint.js
@@ -7,28 +7,23 @@ const pRed = BN.red(p);
 const b = new BN('3', 10).toRed(pRed);
 const zero = new BN('0', 10).toRed(pRed);
 
-const baseString = "Just Read The Instructions!";
-const asciiString = 0x4a75737420526561642054686520496e737472756374696f6e7321;
-const asciiStringRLP = 0x00000000004a75737420526561642054686520496e737472756374696f6e7321;
-const asciiHash = 0x6f1e57ce61731388001f7e47ffd4312f4cfac1db86e494d68bd9044f1646f592;
-hashToPoint.getPoint = () => {
-    let xBase = new BN("6f1e57ce61731388001f7e47ffd4312f4cfac1db86e494d68bd9044f1646f592", 16);
-    let xRed = xBase.toRed(pRed);
-    let xx = xRed.redSqr();
-    let xxx = xx.redMul(xRed);
-    let yy = xxx.redAdd(b);
-    let y = yy.redSqrt();
-    console.log('y = ', y.fromRed().toString(16));
-    console.log('yy = ', yy.fromRed().toString(16));
-    console.log('recovered yy = ', y.redSqr().fromRed().toString(16));
+hashToPoint.baseString = 'Just Read The Instructions!';
+hashToPoint.asciiString = 0x4a75737420526561642054686520496e737472756374696f6e7321;
+hashToPoint.asciiStringRLP = 0x00000000004a75737420526561642054686520496e737472756374696f6e7321;
+hashToPoint.asciiHash = 0x6f1e57ce61731388001f7e47ffd4312f4cfac1db86e494d68bd9044f1646f592;
+
+hashToPoint.getPoint = (hash) => {
+    const xBase = new BN(hash, 16);
+    const xRed = xBase.toRed(pRed);
+    const xx = xRed.redSqr();
+    const xxx = xx.redMul(xRed);
+    const yy = xxx.redAdd(b);
+    const y = yy.redSqrt();
     if (y.redSqr().redSub(yy).cmp(zero) !== 0) {
-        throw new Error('Cannot find root?');
+        throw new Error('Cannot find root, pick another hash');
     }
-    console.log('x = ', xRed.fromRed().toString(16));
-    console.log('y = ', y.fromRed().toString(16));
-}
+};
+
 // output x = 0xe55bae89f0fd3348f7ef2dafcd180741df7ecb8b600ffbc1397ec21654cfb04
 // output y = 0xb11e774e1efc39981995b18fbb59cf3a164507d0ce503e6660205d1f46f85f3
 module.exports = hashToPoint;
-
-hashToPoint.getPoint();
--- packages/weierstrudel/js_snippets/wnaf_reference_implementation.js
+++ packages/weierstrudel/js_snippets/wnaf_reference_implementation.js
@@ -1,35 +1,24 @@
 const BN = require('bn.js');
-const toBytes32 = require('./toBytes32');
-const crypto = require('crypto');
+
+// TODO: Change wnaf implementation to this
 // reference wnaf implenentation to test smart contract against.
 // performs standard window NAF method of dividing-by-2 until least significant bit is high
-function alternativeWnaf(scalar) {
+/* function alternativeWnaf(scalar) {
     let next = scalar;
-    let wnaf = [];
+    const wnaf = [];
     let o = 0;
-    let count = 0;
     while (!next.eq(new BN(0))) {
         const i = next.zeroBits();
-        o = o + i;
+        o += i;
         next = next.shrn(i);
-        let m = next.and(new BN(31));
+        const m = next.and(new BN(31));
         wnaf[o] = m;
         if (m.gt(new BN(16))) {
             next = next.add(new BN(32));
         }
         next = next.sub(m);
-        count++;
     }
- //   console.log('count = ', count);
-}
-// so count is from 20 to 23
-// so we can hard-code 20 and then jump
-
-for (let i = 0; i < 100; i++) {
-    const scalar = new BN(crypto.randomBytes(32), 16).maskn(127);
-    alternativeWnaf(scalar);
-}
-
+} */
 
 module.exports = function calculateWnaf(scalar) {
     // assume 'scalar' is a BN
@@ -40,8 +29,6 @@ module.exports = function calculateWnaf(scalar) {
     const fifteen = new BN('15', 10);
     const thirtyOne = new BN('31', 10);
     const thirtyTwo = new BN('32', 10);
-    const twoFiveFive = new BN('255', 10);
-    let i = 0;
 
     function recurse(currentScalar, iterator = 0, wnafArray = []) {
         if (currentScalar.eq(zero)) {
@@ -49,45 +36,17 @@ module.exports = function calculateWnaf(scalar) {
         }
         if (currentScalar.and(one).eq(one)) {
             const m = currentScalar.and(thirtyOne);
-            let newWnafArray = [...wnafArray];
+            const newWnafArray = [...wnafArray];
 
             newWnafArray[iterator] = m;
             let newScalar = currentScalar.sub(m.and(thirtyOne));
             if (m.and(thirtyOne).gt(fifteen)) {
                 newScalar = newScalar.add(thirtyTwo);
             }
-            return recurse(newScalar.div(two), iterator += 1, newWnafArray);
-        } else {
-            return recurse(currentScalar.div(two), iterator += 1, [...wnafArray, new BN('0', 10)]);
+            return recurse(newScalar.div(two), iterator + 1, newWnafArray);
         }
+        return recurse(currentScalar.div(two), iterator + 1, [...wnafArray, new BN('0', 10)]);
     }
     const wnafData = recurse(scalar);
-    let lookupTableString = '';
-
-    wnafData.forEach((val, index) => {
-        const v = val || new BN('0', 10);
-        const next = v.lt(new BN('16', 10)) ? `0${val.toString(16)}` : val.toString(16);
-        lookupTableString = `${lookupTableString}${next}`;
-    });
-
-    const stringArray = function sliceWnafString(wnafString, accumulator = []) {
-        if (wnafString && wnafString.length > 0) {
-            return sliceWnafString(
-                wnafString.slice(64, wnafString.length),
-                [
-                    ...accumulator,
-                    wnafString.slice(0, 64),
-                ]
-            );
-        }
-        return accumulator;
-    }(lookupTableString);
-
-    return stringArray.map((s, i) => {
-        if (i === stringArray.length - 1) {
-            return new BN(toBytes32(s, 'right'), 16);
-        } else {
-            return new BN(s, 16);
-        }
-    });
-}
+    return wnafData;
+};
