ac764d883925396f33215556256e2015a12d8f55
zac-williamson <blorktronics@gmail.com>
2018-12-26 17:14:18 +0000

    refactored WNAF to have reduced number of conditional jumps and swap ops.
    Reduced code size of WNAF macro by re-using literals instead of invoking PUSH

--- packages/weierstrudel/huff_tests/wnaf.spec.js
+++ packages/weierstrudel/huff_tests/wnaf.spec.js
@@ -15,21 +15,17 @@ const testHelper = `
     WNAF()
 }
 
-#define ENDO_WNAF_FIXED_IMPL = takes(2) returns(1) {
+#define ENDO_WNAF_FIXED_IMPL = takes(6) returns(1) {
     SET_WNAF_TABLE()
     ENDO_WNAF_FIXED()
 }
 
-#define ENDO_WNAF_FIXED_B_IMPL = takes(2) returns(1) {
-    SET_WNAF_TABLE()
-    ENDO_WNAF_FIXED_B()
-}
-
-#define WNAF_SLICE_A_IMPL = takes(2) returns(1) {
+#define WNAF_SLICE_A_IMPL = takes(6) returns(1) {
     SET_WNAF_TABLE()
     WNAF_SLICE_A()
 }
-#define WNAF_SLICE_B_IMPL = takes(2) returns(1) {
+
+#define WNAF_SLICE_B_IMPL = takes(6) returns(1) {
     SET_WNAF_TABLE()
     WNAF_SLICE_B()
 }
@@ -39,9 +35,17 @@ describe.only('sparse wnaf', function describe() {
     this.timeout(30000);
     let wnaf;
     let thirtyTwo;
+    let wnafStartingStack;
     before(() => {
         wnaf = new Runtime(testHelper);
         thirtyTwo = new BN(32);
+
+        wnafStartingStack = [
+            new BN('ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe0', 16),
+            new BN(16),
+            new BN('1fe0', 16),
+            new BN(269),
+        ];
     });
     it('macro WNAF correctly calculates a w=5 windowed non adjacent form of a 254 bit number', async () => {
         const testVar = bn128Reference.randomPoint().x;
@@ -84,48 +88,33 @@ describe.only('sparse wnaf', function describe() {
         }
     });
 
+    it('macro WNAF_SLICE_A correctly calclates a wnaf slice', async () => {
+        const w = new BN('1110111100101110', 2);
+        const baseOffset = 800;
+        const { memory, stack } = await wnaf('WNAF_SLICE_A_IMPL', [...wnafStartingStack, w, new BN(baseOffset)], [], []);
+        expect(new BN(memory[832], 16).umod(thirtyTwo).toString(10)).to.equal('23');
+        expect(stack.length).to.equal(6);
+        expect(stack[4].toString(10)).to.equal('832');
+        expect(stack[5].toString(2)).to.equal('111011110100000');
+    });
+
+    it('macro WNAF_SLICE_B correctly calclates a wnaf slice', async () => {
+        const w = new BN('1110111100101110', 2);
+        const baseOffset = 800;
+        const { memory, stack } = await wnaf('WNAF_SLICE_B_IMPL', [...wnafStartingStack, new BN(baseOffset), w], [], []);
+        expect(new BN(memory[832], 16).umod(thirtyTwo).toString(10)).to.equal('23');
+        expect(stack.length).to.equal(6);
+        expect(stack[5].toString(10)).to.equal('832');
+        expect(stack[4].toString(2)).to.equal('111011110100000');
+    });
+
     it('macro ENDO_WNAF_FIXED correctly calculates a w=5 NAF of a 127-bit number, with added memory offset', async () => {
         const scalar = bn128Reference.randomScalar();
         let { k1: endoScalar } = endomorphism.endoSplit(scalar);
         const reference = referenceWnaf.wnaf(endoScalar);
         endoScalar = endoScalar.or(new BN('1000000000000000000000000000000000', 16));
         const baseOffset = 1025;
-        const { memory } = await wnaf('ENDO_WNAF_FIXED_IMPL', [new BN(baseOffset), endoScalar], [], []);
-        for (let i = 0; i < reference.length; i += 1) {
-            const memoryOffset = baseOffset + (i * 32);
-            if (reference[i] && reference[i].gt(new BN(0))) {
-                expect(reference[i].eq(new BN(memory[memoryOffset], 16).umod(thirtyTwo))).to.equal(true);
-            }
-        }
-    });
-
-    it('macro WNAF_SLICE_A correctly calclates a wnaf slice', async () => {
-        const w = new BN('1110111100101110', 2);
-        const baseOffset = 800;
-        const { memory, stack } = await wnaf('WNAF_SLICE_A_IMPL', [w, new BN(baseOffset)], [], []);
-        expect(new BN(memory[832], 16).umod(thirtyTwo).toString(10)).to.equal('23');
-        expect(stack.length).to.equal(2);
-        expect(stack[0].toString(10)).to.equal('832');
-        expect(stack[1].toString(2)).to.equal('111011110100000');
-    });
-
-    it('macro WNAF_SLICE_B correctly calclates a wnaf slice', async () => {
-        const w = new BN('1110111100101110', 2);
-        const baseOffset = 800;
-        const { memory, stack } = await wnaf('WNAF_SLICE_B_IMPL', [new BN(baseOffset), w], [], []);
-        expect(new BN(memory[832], 16).umod(thirtyTwo).toString(10)).to.equal('23');
-        expect(stack.length).to.equal(2);
-        expect(stack[1].toString(10)).to.equal('832');
-        expect(stack[0].toString(2)).to.equal('111011110100000');
-    });
-
-    it('macro ENDO_WNAF_FIXED_B correctly calculates a w=5 NAF of a 127-bit number, with added memory offset', async () => {
-        const scalar = bn128Reference.randomScalar();
-        let { k1: endoScalar } = endomorphism.endoSplit(scalar);
-        const reference = referenceWnaf.wnaf(endoScalar);
-        endoScalar = endoScalar.or(new BN('1000000000000000000000000000000000', 16));
-        const baseOffset = 1025;
-        const { memory } = await wnaf('ENDO_WNAF_FIXED_B_IMPL', [endoScalar, new BN(baseOffset)], [], []);
+        const { memory } = await wnaf('ENDO_WNAF_FIXED_IMPL', [...wnafStartingStack, endoScalar, new BN(baseOffset)], [], []);
         for (let i = 0; i < reference.length; i += 1) {
             const memoryOffset = baseOffset + (i * 32);
             if (reference[i] && reference[i].gt(new BN(0))) {
