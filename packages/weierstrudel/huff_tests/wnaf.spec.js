ededdae819487ee5c222aac5847c79bba2bcef1f
zac-williamson <blorktronics@gmail.com>
2018-12-26 18:11:07 +0000

    added macro to wnaf.huff that computes a set of WNAFS and returns memory location of greatest wnaf entry

--- packages/weierstrudel/huff_tests/wnaf.spec.js
+++ packages/weierstrudel/huff_tests/wnaf.spec.js
@@ -15,24 +15,24 @@ const testHelper = `
     WNAF()
 }
 
-#define ENDO_WNAF_FIXED_IMPL = takes(6) returns(1) {
+#define ENDO_WNAF_FIXED_IMPL = takes(8) returns(1) {
     SET_WNAF_TABLE()
     ENDO_WNAF_FIXED()
 }
 
-#define WNAF_SLICE_A_IMPL = takes(6) returns(1) {
+#define WNAF_SLICE_A_IMPL = takes(8) returns(1) {
     SET_WNAF_TABLE()
     WNAF_SLICE_A()
 }
 
-#define WNAF_SLICE_B_IMPL = takes(6) returns(1) {
+#define WNAF_SLICE_B_IMPL = takes(8) returns(1) {
     SET_WNAF_TABLE()
     WNAF_SLICE_B()
 }
 `;
 
 describe.only('sparse wnaf', function describe() {
-    this.timeout(30000);
+    this.timeout(5000);
     let wnaf;
     let thirtyTwo;
     let wnafStartingStack;
@@ -45,6 +45,8 @@ describe.only('sparse wnaf', function describe() {
             new BN(16),
             new BN('1fe0', 16),
             new BN(269),
+            new BN(0),
+            new BN(0),
         ];
     });
     it('macro WNAF correctly calculates a w=5 windowed non adjacent form of a 254 bit number', async () => {
@@ -93,9 +95,9 @@ describe.only('sparse wnaf', function describe() {
         const baseOffset = 800;
         const { memory, stack } = await wnaf('WNAF_SLICE_A_IMPL', [...wnafStartingStack, w, new BN(baseOffset)], [], []);
         expect(new BN(memory[832], 16).umod(thirtyTwo).toString(10)).to.equal('23');
-        expect(stack.length).to.equal(6);
-        expect(stack[4].toString(10)).to.equal('832');
-        expect(stack[5].toString(2)).to.equal('111011110100000');
+        expect(stack.length).to.equal(8);
+        expect(stack[6].toString(10)).to.equal('832');
+        expect(stack[7].toString(2)).to.equal('111011110100000');
     });
 
     it('macro WNAF_SLICE_B correctly calclates a wnaf slice', async () => {
@@ -103,9 +105,9 @@ describe.only('sparse wnaf', function describe() {
         const baseOffset = 800;
         const { memory, stack } = await wnaf('WNAF_SLICE_B_IMPL', [...wnafStartingStack, new BN(baseOffset), w], [], []);
         expect(new BN(memory[832], 16).umod(thirtyTwo).toString(10)).to.equal('23');
-        expect(stack.length).to.equal(6);
-        expect(stack[5].toString(10)).to.equal('832');
-        expect(stack[4].toString(2)).to.equal('111011110100000');
+        expect(stack.length).to.equal(8);
+        expect(stack[7].toString(10)).to.equal('832');
+        expect(stack[6].toString(2)).to.equal('111011110100000');
     });
 
     it('macro ENDO_WNAF_FIXED correctly calculates a w=5 NAF of a 127-bit number, with added memory offset', async () => {
@@ -122,4 +124,116 @@ describe.only('sparse wnaf', function describe() {
             }
         }
     });
+
+    it('macro WNAF_INIT correctly sets up wnaf stack state', async () => {
+        const scalars = [
+            bn128Reference.randomScalar(),
+            bn128Reference.randomScalar(),
+            bn128Reference.randomScalar(),
+            bn128Reference.randomScalar(),
+        ];
+        const points = [
+            bn128Reference.randomPoint(),
+            bn128Reference.randomPoint(),
+            bn128Reference.randomPoint(),
+            bn128Reference.randomPoint(),
+        ];
+
+        const calldata = [
+            { index: 0, value: points[0].x },
+            { index: 32 * 1, value: points[0].y },
+            { index: 32 * 2, value: points[1].x },
+            { index: 32 * 3, value: points[1].y },
+            { index: 32 * 4, value: points[2].x },
+            { index: 32 * 5, value: points[2].y },
+            { index: 32 * 6, value: points[3].x },
+            { index: 32 * 7, value: points[3].y },
+            { index: 32 * 8, value: scalars[0] },
+            { index: 32 * 9, value: scalars[1] },
+            { index: 32 * 10, value: scalars[2] },
+            { index: 32 * 11, value: scalars[3] },
+        ];
+
+        const { stack } = await wnaf('WNAF_INIT', [], [], calldata);
+
+        expect(stack.length).to.equal(7);
+        expect(stack[0].toString(16)).to.equal('1000000000000000000000000000000000');
+        expect(stack[1].toString(16)).to.equal('ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe0');
+        expect(stack[2].toString(10)).to.equal('16');
+        expect(stack[3].toString(16)).to.equal('1fe0');
+        expect(stack[4].toString(10)).to.equal('269');
+        expect(stack[5].toString(16)).to.equal('1047');
+        expect(stack[6].toString(16)).to.equal('100');
+    });
+
+    it('macro COMPUTE_WNAFS will correctly calculate w=5 endo split wnafs for multiple scalars', async () => {
+        const scalars = [
+            bn128Reference.randomScalar(),
+            bn128Reference.randomScalar(),
+            bn128Reference.randomScalar(),
+            bn128Reference.randomScalar(),
+        ];
+        const points = [
+            bn128Reference.randomPoint(),
+            bn128Reference.randomPoint(),
+            bn128Reference.randomPoint(),
+            bn128Reference.randomPoint(),
+        ];
+
+        const calldata = [
+            { index: 0, value: points[0].x },
+            { index: 32 * 1, value: points[0].y },
+            { index: 32 * 2, value: points[1].x },
+            { index: 32 * 3, value: points[1].y },
+            { index: 32 * 4, value: points[2].x },
+            { index: 32 * 5, value: points[2].y },
+            { index: 32 * 6, value: points[3].x },
+            { index: 32 * 7, value: points[3].y },
+            { index: 32 * 8, value: scalars[0] },
+            { index: 32 * 9, value: scalars[1] },
+            { index: 32 * 10, value: scalars[2] },
+            { index: 32 * 11, value: scalars[3] },
+        ];
+        const { stack, memory } = await wnaf('COMPUTE_WNAFS', [], [], calldata);
+
+
+        const endoScalars = scalars.reduce((acc, s) => {
+            const { k1, k2 } = endomorphism.endoSplit(s);
+            return [...acc, k2, k1];
+        }, []);
+
+        const referenceWnafs = endoScalars.map(s => referenceWnaf.wnaf(s));
+
+        const startingOffset = new BN('1047', 16);
+        let maxEntry = 0;
+        for (let j = 0; j < referenceWnafs.length; j += 1) {
+            const reference = referenceWnafs[j];
+            // eslint-disable-next-line no-bitwise
+            const isOdd = j & 1;
+            const baseOffset = startingOffset - (j) + (isOdd * 2);
+            for (let i = 0; i < reference.length; i += 1) {
+                const memoryOffset = baseOffset + (i * 32);
+                if (reference[i] && reference[i].gt(new BN(0))) {
+                    maxEntry = Math.max(maxEntry, i);
+                    expect(reference[i].toString(16)).to.equal(new BN(memory[memoryOffset], 16).umod(thirtyTwo).toString(16));
+                }
+            }
+        }
+
+        expect(stack.length).to.equal(1);
+        expect(stack[0].toString(16)).to.equal(new BN(4160 + (maxEntry * 32)).toString(16));
+    });
 });
+
+/*
+0x1000000000000000000000000000000000 // b
+0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe0 // mask b
+16 // 16 mask b
+0x1fe0 // 0x1fe0 16 mask b
+269 // 269 0x1fe0 16 mask b
+
+// compute calldata and memory offsets
+0x60 calldatasize div // n
+dup1 0x10f3 add       // m n
+swap1 0x40 mul        // s m 269 0x1fe0 16 mask b
+*/
