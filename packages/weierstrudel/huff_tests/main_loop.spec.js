b05e0dd99740420057e1d7d6848c2905824cb4c8
zac-williamson <blorktronics@gmail.com>
2019-01-01 22:27:46 +0000

    added macro that performs multi-exponentiation loop as a hardcoded sequence.
    (minor issue that bytecode is >> max contract code size...)

--- packages/weierstrudel/huff_tests/main_loop.spec.js
+++ packages/weierstrudel/huff_tests/main_loop.spec.js
@@ -37,7 +37,7 @@ const referenceCurve = new EC.curve.short({
 });
 
 
-describe('bn128 main loop', function describe() {
+describe.only('bn128 main loop', function describe() {
     this.timeout(10000);
     let main;
     before(() => {
