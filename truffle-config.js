94ce309de4d6fd4dbee95a4b3dfa8f53830ea995
Paul Berg <hello@paulrberg.com>
2019-01-04 15:33:00 +0200

    Added private key functionality to truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 const { toWei, toHex } = require('web3-utils');
 const HDWalletProvider = require('truffle-hdwallet-provider');
 
+// You must specify PRIVATE_KEY and INFURA_API_KEY in your .env file
+// Feel free to replace PRIVATE_KEY with a MNEMONIC to use an hd wallet
 function createProvider(infuraUrl) {
-    if (!process.env.MNEMONIC || !process.env.INFURA_API_KEY) {
-        console.log('Please load your MNEMONIC and INFURA_API_KEY in a .env file');
+    if (!process.env.PRIVATE_KEY || !process.env.MNEMONIC) {
+        console.log('Please set either your PRIVATE_KEY or MNEMONIC');
+        process.exit(1);
+    }
+    if (!process.env.INFURA_API_KEY) {
+        console.log('Please set your INFURA_API_KEY');
         process.exit(1);
     }
     return () => {
-        return new HDWalletProvider(process.env.MNEMONIC, infuraUrl + process.env.INFURA_API_KEY);
+        return new HDWalletProvider(process.env.PRIVATE_KEY || process.env.MNEMONIC, infuraUrl + process.env.INFURA_API_KEY);
     };
 }
 
@@ -51,7 +57,7 @@ module.exports = {
             network_id: '4',
         },
         ropsten: {
-            provider: createProvider('https://ropsten.infura.io'),
+            provider: createProvider('https://ropsten.infura.io/'),
             gas: 4600000,
             gasPrice: toHex(toWei('10', 'gwei')),
             network_id: '3',
