2180f5cd900bdbebaad0da854a59c44f8ddea31e
thomas-waite <thomas.waite4@gmail.com>
2019-01-07 14:44:07 +0000

    Updated comments and made some of them more explanatory

--- contracts/AZTEC/AtomicSwap.sol
+++ contracts/AZTEC/AtomicSwap.sol
@@ -12,7 +12,6 @@ library AtomicSwapInterface {
  * Calling this internally from another function will lead to memory mutation and undefined behaviour.
  * The intended use case is to call this externally via `staticcall`. External calls to OptimizedAZTEC can be treated as pure functions as this contract contains no storage and makes no external calls (other than to precompiles)
  * Copyright Spilbury Holdings Ltd 2018. All rights reserved.
- * Note: Elements of the memory map are currently unpopulated. This is deliberate - to keep the memory maps consistent across different use case of the AZTEC protocol
  **/
 contract AtomicSwap {
     /**
@@ -24,7 +23,7 @@ contract AtomicSwap {
     function() external payable {
         assembly {
 
-            // We don't check for function signatures, there's only one function that ever gets called: validateJoinSplit()
+            // We don't check for function signatures, there's only one function that ever gets called: validateAtomicSwap()
             // We still assume calldata is offset by 4 bytes so that we can represent this contract through a compatible ABI
             validateAtomicSwap()
 
@@ -33,7 +32,7 @@ contract AtomicSwap {
             revert(0x00, 0x20)
 
             /**
-             * @dev Validate an AZTEC protocol JoinSplit zero-knowledge proof
+             * @dev Validate an AZTEC protocol atomic-swap zero-knowledge proof
              * Calldata Map is
              * 0x04:0x24       = calldata location of start of ```note``` dynamic array
              * 0x24:0x44       = Fiat-Shamir heuristicified random challenge
@@ -61,8 +60,6 @@ contract AtomicSwap {
              * 0x300:???       = block of memory that contains (\gamma_i, \sigma_i)_{i=0}^{n-1} concatenated with (B_i)_{i=0}^{n-1}
              **/
             function validateAtomicSwap() {
-                // calldatacopy(0x00, 0x04, sub(calldatasize, 0x04))
-                // return(0x00, sub(calldatasize, 0x04))
                 /*
                 ///////////////////////////////////////////  SETUP  //////////////////////////////////////////////
                 */
@@ -78,7 +75,7 @@ contract AtomicSwap {
                 mstore(0x2e0, caller) // store the msg.sender, to be hashed later
 
                 hashCommitments(notes, n)
-                let b := add(0x300, mul(n, 0x80)) // setting the pointer to the point in memory where the commitments end
+                let b := add(0x300, mul(n, 0x80)) // setting the pointer to the location in memory where the commitments end
 
                 /*
                 ///////////////////////////  CALCULATE BLINDING FACTORS  /////////////////////////////////////
@@ -148,18 +145,15 @@ contract AtomicSwap {
                     if iszero(result) { mstore(0x00, 400) revert(0x00, 0x20) }
                     b := add(b, 0x40) // increase B pointer by 2 words
                 }
-                // return (add(0x300, mul(n, 0x80)), mul(0x40, 4))
                 // Both bid notes already exist in their revelant AZTEC note registries - so can inductively infer that the ask notes are in the required range
                 // Therefore, don't need a range proof
-
-
+            
                 /*
                 ////////////////////  RECONSTRUCT INITIAL CHALLENGE AND VERIFY A MATCH  ////////////////////////////////
                 */
 
-
                 // We now have the note commitments and the calculated blinding factors in a block of memory
-                // starting at 0x2e0, of size (b - 0x2a0).
+                // starting at 0x2e0, of size (b - 0x2e0).
                 // Hash this block to reconstruct the initial challenge and validate that they match
                 let expected := mod(keccak256(0x2e0, sub(b, 0x2e0)), gen_order)
 
