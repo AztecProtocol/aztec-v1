0cefc964d30724da61fe35427bef5f14676f2fd9
thomas-waite <thomas.waite4@gmail.com>
2019-01-07 16:04:23 +0000

    Added a check to ensure the correct number of notes has been inputted to the verifier
    If incorrect number of notes, then contract will throw

--- contracts/AZTEC/AtomicSwap.sol
+++ contracts/AZTEC/AtomicSwap.sol
@@ -68,6 +68,16 @@ contract AtomicSwap {
                 mstore(0xa0, 8489654445897228341090914135473290831551238522473825886865492707826370766375) // h_y
                 let notes := add(0x04, calldataload(0x04))
                 let n := calldataload(notes)
+                
+                mstore(0x00, n)
+                return(0x00, 0x20)
+
+                if eq(eq(n, 0x04), 0) { // eq(n, 4) will resolve to 0 if n != 4
+
+                    mstore(0x00, 400) // 400 error code - due to incorrect number of notes supplied
+                    revert(0x00, 0x20)
+
+                }
 
                 let gen_order := 0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001
                 let challenge := mod(calldataload(0x24), gen_order)
