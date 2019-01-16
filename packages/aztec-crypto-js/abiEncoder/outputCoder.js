909b0a33ce4b4ed23725dc500d49c40f881c2a27
zac-williamson <blorktronics@gmail.com>
2019-01-16 10:40:07 +0000

    fixed outputCoder's abi decoding functions to work
    outputCoder.encodeProofOutputs now prepends '0x' to result

--- packages/aztec-crypto-js/abiEncoder/outputCoder.js
+++ packages/aztec-crypto-js/abiEncoder/outputCoder.js
@@ -1,4 +1,3 @@
-const BN = require('bn.js');
 const { padLeft } = require('web3-utils');
 
 const bn128 = require('../bn128/bn128');
@@ -6,21 +5,21 @@ const secp256k1 = require('../secp256k1/secp256k1');
 
 const outputCoder = {};
 
-outputCoder.decodeNote = (note) => {
-    const length = parseInt(note.slice(0, 32), 16);
-    if ((note.length / 2) !== 0xe1 || (length !== 0xe1)) {
-        throw new Error(`invalid length of ${note}`);
+outputCoder.decodeOutputNote = (note) => {
+    const length = parseInt(note.slice(0x00, 0x40), 16);
+    if (length !== 0xc1) {
+        throw new Error(`invalid length of ${length.toString(16)}`);
     }
-    const owner = note.slice(32, 64);
-    const noteHash = note.slice(64, 96);
-    const metadataLength = parseInt(note.slice(96, 128), 16);
+    const owner = `0x${note.slice(0x58, 0x80)}`;
+    const noteHash = `0x${note.slice(0x80, 0xc0)}`;
+    const metadataLength = parseInt(note.slice(0xc0, 0x100), 16);
     if (metadataLength !== 0x61) {
-        throw new Error(`invalid metadata length of ${note}`);
+        throw new Error(`invalid metadata length of ${metadataLength}`);
     }
-    const gamma = bn128.decompress(new BN(note.slice(128, 160)));
-    const sigma = bn128.decompress(new BN(note.slice(160, 192)));
+    const gamma = bn128.decompressHex(note.slice(0x100, 0x140));
+    const sigma = bn128.decompressHex(note.slice(0x140, 0x180));
 
-    const ephemeral = secp256k1.decompress(new BN(note.slice(192, 224)), new BN(note.slice(224, 225)));
+    const ephemeral = secp256k1.decompressHex(note.slice(0x180, 0x1c2));
     return {
         owner,
         noteHash,
@@ -30,33 +29,48 @@ outputCoder.decodeNote = (note) => {
     };
 };
 
-outputCoder.decodeNotes = (notes) => {
-    const byteLength = notes.slice(0, 32);
-    const n = parseInt(notes.slice(32, 64), 16);
-    if (byteLength !== ((n * 0x101) + 0x20)) {
-        throw new Error(`expected ${byteLength} to equal ${((n * 0x101) + 0x20)}, of ${notes}`);
+outputCoder.decodeInputNote = (note) => {
+    const length = parseInt(note.slice(0x00, 0x40), 16);
+    if (length !== 0xa0) {
+        throw new Error(`invalid input note length of ${length.toString(16)}`);
     }
+    const owner = `0x${note.slice(0x58, 0x80)}`;
+    const noteHash = `0x${note.slice(0x80, 0xc0)}`;
+    const metadataLength = parseInt(note.slice(0xc0, 0x100), 16);
+    if (metadataLength !== 0x40) {
+        throw new Error(`invalid metadata length of ${metadataLength}`);
+    }
+    const gamma = bn128.decompressHex(note.slice(0x100, 0x140));
+    const sigma = bn128.decompressHex(note.slice(0x140, 0x180));
+    return {
+        owner,
+        noteHash,
+        gamma,
+        sigma,
+    };
+};
+
+
+outputCoder.decodeNotes = (notes, isOutput) => {
+    const n = parseInt(notes.slice(0x40, 0x80), 16);
     return [...new Array(n)].map((x, i) => {
-        const noteOffset = parseInt(notes.slice(64 + (i * 32), 96 + (i * 32)), 16);
-        return outputCoder.decodeNote(notes.slice(noteOffset));
+        const noteOffset = parseInt(notes.slice(0x80 + (i * 0x40), 0xc0 + (i * 0x40)), 16);
+        if (isOutput) {
+            return outputCoder.decodeOutputNote(notes.slice(noteOffset * 2));
+        }
+        return outputCoder.decodeInputNote(notes.slice(noteOffset * 2));
     });
 };
 
 outputCoder.decodeProofOutput = (proofOutput) => {
-    const byteLength = parseInt(proofOutput.slice(0, 32), 16);
-    const inputNotesOffset = parseInt(proofOutput.slice(32, 64), 16);
-    const outputNotesOffset = parseInt(proofOutput.slice(64, 92), 16);
-    const publicOwner = proofOutput.slice(92, 128);
-    const publicValue = proofOutput.slice(128, 160);
-    const inputNotes = outputCoder.decodeNotes(proofOutput.slice(inputNotesOffset));
-    const outputNotes = outputCoder.decodeNotes(proofOutput.slice(outputNotesOffset));
+    // const byteLength = parseInt(proofOutput.slice(0x00, 0x40), 16);
+    const inputNotesOffset = parseInt(proofOutput.slice(0x40, 0x80), 16);
+    const outputNotesOffset = parseInt(proofOutput.slice(0x80, 0xc0), 16);
+    const publicOwner = `0x${proofOutput.slice(0xd8, 0x100)}`;
+    const publicValue = parseInt(proofOutput.slice(0x100, 0x140), 16);
+    const inputNotes = outputCoder.decodeNotes(proofOutput.slice(inputNotesOffset * 2), false);
+    const outputNotes = outputCoder.decodeNotes(proofOutput.slice(outputNotesOffset * 2), true);
 
-    const numNotes = inputNotes.length + outputNotes.length;
-    const notesLength = 0x80 + (0x101 * numNotes);
-    const proofDataLength = 0xa0 + notesLength;
-    if (byteLength !== proofDataLength) {
-        throw new Error(`abi.decodeProofOutput, expected ${byteLength} to equal ${proofDataLength}, of ${proofOutput}`);
-    }
     return {
         inputNotes,
         outputNotes,
@@ -67,20 +81,17 @@ outputCoder.decodeProofOutput = (proofOutput) => {
 
 outputCoder.decodeProofOutputs = (proofOutputsHex) => {
     const proofOutputs = proofOutputsHex.slice(2);
-    const { length } = proofOutputs;
-    if (parseInt(proofOutputs.slice(0, 32), 16) !== (length / 2)) {
-        throw new Error(`${proofOutputsHex} length does not match ${length / 2}`);
-    }
-    const numOutputs = parseInt(proofOutputs.slice(32, 64), 16);
+
+    const numOutputs = parseInt(proofOutputs.slice(0x40, 0x80), 16);
     return [...new Array(numOutputs)].map((x, i) => {
-        const outputOffset = parseInt(proofOutputs.slice(64 + (i * 32), 96 + (i * 32)), 16);
-        return outputCoder.decodeProofOutput(proofOutputs.slice(outputOffset));
+        const outputOffset = parseInt(proofOutputs.slice(0x80 + (i * 0x40), 0xc0 + (i * 0x40)), 16);
+        return outputCoder.decodeProofOutput(proofOutputs.slice(outputOffset * 2));
     });
 };
 
-outputCoder.encodeNote = (note) => {
+outputCoder.encodeOutputNote = (note) => {
     const encoded = [...new Array(7)];
-    encoded[0] = padLeft('e1', 64);
+    encoded[0] = padLeft('c1', 64);
     encoded[1] = padLeft(note.owner.slice(2), 64);
     encoded[2] = padLeft(note.noteHash.slice(2), 64);
     encoded[3] = padLeft('61', 64);
@@ -90,15 +101,31 @@ outputCoder.encodeNote = (note) => {
     return encoded.join('');
 };
 
-outputCoder.encodeNotes = (notes) => {
-    const encodedNotes = notes.map(note => outputCoder.encodeNote(note));
+outputCoder.encodeInputNote = (note) => {
+    const encoded = [...new Array(7)];
+    encoded[0] = padLeft('a0', 64);
+    encoded[1] = padLeft(note.owner.slice(2), 64);
+    encoded[2] = padLeft(note.noteHash.slice(2), 64);
+    encoded[3] = padLeft('40', 64);
+    encoded[4] = padLeft(bn128.compress(note.gamma.x.fromRed(), note.gamma.y.fromRed()).toString(16), 64);
+    encoded[5] = padLeft(bn128.compress(note.sigma.x.fromRed(), note.sigma.y.fromRed()).toString(16), 64);
+    return encoded.join('');
+};
+
+outputCoder.encodeNotes = (notes, isOutput) => {
+    let encodedNotes;
+    if (isOutput) {
+        encodedNotes = notes.map(note => outputCoder.encodeOutputNote(note, isOutput));
+    } else {
+        encodedNotes = notes.map(note => outputCoder.encodeInputNote(note, isOutput));
+    }
     const offsetToData = 0x40 + (0x20 * encodedNotes.length);
     const noteLengths = encodedNotes.reduce((acc, p) => {
         return [...acc, acc[acc.length - 1] + (p.length / 2)];
     }, [offsetToData]);
 
     const encoded = [
-        padLeft(noteLengths.slice(-1)[0].toString(16), 64),
+        padLeft((noteLengths.slice(-1)[0] - 0x20).toString(16), 64),
         padLeft(notes.length.toString(16), 64),
         ...noteLengths.slice(0, -1).map(n => padLeft(n.toString(16), 64)),
         ...encodedNotes,
@@ -112,10 +139,10 @@ outputCoder.encodeProofOutput = ({
     publicOwner,
     publicValue,
 }) => {
-    const encodedInputNotes = outputCoder.encodeNotes(inputNotes);
-    const encodedOutputNotes = outputCoder.encodeNotes(outputNotes);
+    const encodedInputNotes = outputCoder.encodeNotes(inputNotes, false);
+    const encodedOutputNotes = outputCoder.encodeNotes(outputNotes, true);
     const encoded = [...new Array(6)];
-    encoded[0] = padLeft((0xa0 + ((encodedInputNotes.length + encodedOutputNotes.length) / 2)).toString(16), 64);
+    encoded[0] = padLeft((0x80 + ((encodedInputNotes.length + encodedOutputNotes.length) / 2)).toString(16), 64);
     encoded[1] = padLeft('a0', 64);
     encoded[2] = padLeft((0xa0 + (encodedInputNotes.length / 2)).toString(16), 64);
     encoded[3] = padLeft(publicOwner.slice(2), 64);
@@ -133,12 +160,12 @@ outputCoder.encodeProofOutputs = (proofOutputs) => {
     }, [offsetToData]);
 
     const encoded = [
-        padLeft(proofLengths.slice(-1)[0].toString(16), 64),
+        padLeft((proofLengths.slice(-1)[0] - 0x20).toString(16), 64),
         padLeft(proofOutputs.length.toString(16), 64),
         ...proofLengths.slice(0, -1).map(n => padLeft(n.toString(16), 64)),
         ...encodedProofOutputs,
     ];
-    return encoded.join('');
+    return `0x${encoded.join('')}`;
 };
 
 module.exports = outputCoder;
