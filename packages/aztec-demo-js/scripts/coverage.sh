66f3103a1be9e9d6137c5f4a9c16f870f82ee34d
Paul Berg <hello@paulrberg.com>
2019-01-17 17:28:10 +0000

    Polished shell scripts

--- packages/aztec-demo-js/scripts/coverage.sh
+++ packages/aztec-demo-js/scripts/coverage.sh
@@ -1,3 +1,1 @@
-echo "Using Mocha " ./node_modules/.bin/mocha --version
-
-NODE_ENV=TEST ./node_modules/.bin/nyc ./node_modules/.bin/mocha ./resources --trace-warnings --exit --colors --recursive --reporter spec
+scripts/coverage.sh
--- packages/aztec-demo-js/scripts/test.sh
+++ packages/aztec-demo-js/scripts/test.sh
@@ -14,6 +14,14 @@ if ! [ ganache_running ]; then
   exit 1
 fi
 
+if ! [ -e ../build ]; then
+  echo "First compile and migrate contracts with truffle"
+  exit 1
+fi
+
 echo "Using Mocha $(./node_modules/.bin/mocha --version)"
+
+rm ./contracts
+ln -s ../build/contracts
 
 NODE_ENV=TEST ./node_modules/.bin/mocha ./resources --trace-warnings --exit --colors --recursive --reporter spec
