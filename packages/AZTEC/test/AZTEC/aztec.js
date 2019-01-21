d7f9c126c4fee7c96b91313567885e37ec6acfb0
Paul Berg <hello@paulrberg.com>
2019-01-21 16:28:23 +0000

    Merged the cryptographyEngine branch

--- packages/AZTEC/test/AZTEC/aztec.js
+++ packages/AZTEC/test/AZTEC/aztec.js
@@ -5,8 +5,8 @@ const { padLeft, sha3 } = require('web3-utils');
 const crypto = require('crypto');
 
 // ### Internal Dependencies
-const aztecProof = require('../../aztec-crypto-js/proof');
-const proofHelpers = require('../../aztec-crypto-js/proof/helpers');
+const aztecProof = require('../../aztec-crypto-js/proof/joinSplit');
+const proofHelpers = require('../../aztec-crypto-js/proof/joinSplit/helpers');
 const exceptions = require('../exceptions');
 const { t2, GROUP_MODULUS } = require('../../aztec-crypto-js/params');
 
