37c1f1a857dfb2f899f036cf572a31960a2184de
Paul Berg <hello@paulrberg.com>
2019-01-21 20:46:25 +0000

    Switched to using solhint instead of solium

--- contracts/ERC20/ERC20Mintable.sol
+++ contracts/ERC20/ERC20Mintable.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.24;
 
-import "./ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
 /**
  * @title ERC20Mintable
  * @dev ERC20 minting logic
@@ -8,20 +9,14 @@ import "./ERC20.sol";
  * Anybody can mint - STRICTLY FOR TEST PURPOSES
  */
 contract ERC20Mintable is ERC20 {
-  /**
-   * @dev Function to mint tokens
-   * @param to The address that will receive the minted tokens.
-   * @param value The amount of tokens to mint.
-   * @return A boolean that indicates if the operation was successful.
-   */
-  function mint(
-    address to,
-    uint256 value
-  )
-    public
-    returns (bool)
-  {
-    _mint(to, value);
-    return true;
-  }
+    /**
+    * @dev Function to mint tokens
+    * @param to The address that will receive the minted tokens.
+    * @param value The amount of tokens to mint.
+    * @return A boolean that indicates if the operation was successful.
+    */
+    function mint(address to, uint256 value) public returns (bool) {
+        _mint(to, value);
+        return true;
+    }
 }
