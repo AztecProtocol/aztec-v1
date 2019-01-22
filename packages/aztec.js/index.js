ad0439fbe3aeaad4fd31793f3b88dcb0fa35679e
Paul Berg <hello@paulrberg.com>
2019-01-22 15:32:12 +0000

    Optimised aztec.js for npm

--- packages/aztec.js/index.js
+++ packages/aztec.js/index.js
@@ -1,17 +1,18 @@
 /* eslint-disable import/no-dynamic-require */
-
 const path = require('path');
 
-const bn128 = require(path.join(__dirname, 'src', 'bn128'));
-const eip712 = require(path.join(__dirname, 'src', 'eip712'));
-const keccak = require(path.join(__dirname, 'src', 'keccak'));
-const note = require(path.join(__dirname, 'src', 'note'));
-const params = require(path.join(__dirname, 'src', 'params'));
-const proof = require(path.join(__dirname, 'src', 'proof'));
-const secp256k1 = require(path.join(__dirname, 'src', 'secp256k1'));
-const setup = require(path.join(__dirname, 'src', 'setup'));
+const abiEncoder = require(path.join(__dirname, 'lib', 'abiEncoder'));
+const bn128 = require(path.join(__dirname, 'lib', 'bn128'));
+const eip712 = require(path.join(__dirname, 'lib', 'eip712'));
+const keccak = require(path.join(__dirname, 'lib', 'keccak'));
+const note = require(path.join(__dirname, 'lib', 'note'));
+const params = require(path.join(__dirname, 'lib', 'params'));
+const proof = require(path.join(__dirname, 'lib', 'proof'));
+const secp256k1 = require(path.join(__dirname, 'lib', 'secp256k1'));
+const setup = require(path.join(__dirname, 'lib', 'setup'));
 
 module.exports = {
+    abiEncoder,
     bn128,
     eip712,
     keccak,
