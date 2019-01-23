46d0d2d687dbbc684ec4bb1d25083f51a11c3612
thomas-waite <thomas.waite4@gmail.com>
2019-01-23 18:12:47 +0000

    Integrated bilateral swaps

---
+++ packages/aztec.js/src/proof/bilateralSwap/index.js
@@ -0,0 +1,161 @@
+/**
+ * Constructs AZTEC bilateral swap zero-knowledge proofs
+ *
+ * @module proof
+ */
+const BN = require('bn.js');
+const { padLeft } = require('web3-utils');
+const Keccak = require('../../keccak');
+const bn128 = require('../../bn128');
+
+const helpers = require('./helpers');
+
+const { groupReduction } = bn128;
+
+const bilateralSwap = {};
+bilateralSwap.helpers = helpers;
+
+/**
+ * Construct AZTEC bilateral swap proof transcript
+ *
+ * @method constructProof
+ * @param {Array[Note]} notes array of AZTEC notes
+ * @returns {{proofData:Array[string]}, {challenge: string}} - proof data and challenge
+ */
+bilateralSwap.constructBilateralSwap = (notes, sender) => {
+    // finalHash is used to create final proof challenge
+    const finalHash = new Keccak();
+
+    finalHash.appendBN(new BN(sender.slice(2), 16));
+
+    notes.forEach((note) => {
+        finalHash.append(note.gamma);
+        finalHash.append(note.sigma);
+    });
+
+    const bkArray = [];
+    const blindingFactors = notes.map((note, i) => {
+        let bk = bn128.randomGroupScalar();
+        const ba = bn128.randomGroupScalar();
+        let B;
+
+        /*
+        Explanation of the below if/else
+        - The purpose is to set bk1 = bk3 and bk2 = bk4
+        - i is used as an indexing variable, to keep track of whether we are at a maker note or taker note
+        - All bks are stored in a bkArray. When we arrive at the taker notes, we set bk equal to the bk of the corresponding
+          maker note. This is achieved by 'jumping back' 2 index positions (i - 2) in the bkArray, and setting the current
+          bk equal to the element at the resulting position.
+        */
+
+        // Maker notes
+        if (i <= 1) {
+            B = note.gamma.mul(bk).add(bn128.h.mul(ba));
+        } else { // taker notes
+            bk = bkArray[i-2];
+            B = note.gamma.mul(bk).add(bn128.h.mul(ba));
+        }
+        finalHash.append(B);
+        bkArray.push(bk);
+
+        return {
+            bk,
+            ba,
+            B,
+        };
+    });
+
+    const challenge = finalHash.keccak(groupReduction);
+
+    const proofData = blindingFactors.map((blindingFactor, i) => {
+        const kBar = ((notes[i].k.redMul(challenge)).redAdd(blindingFactor.bk)).fromRed();
+        const aBar = ((notes[i].a.redMul(challenge)).redAdd(blindingFactor.ba)).fromRed();     
+        return [
+            `0x${padLeft(kBar.toString(16), 64)}`,
+            `0x${padLeft(aBar.toString(16), 64)}`,
+            `0x${padLeft(notes[i].gamma.x.fromRed().toString(16), 64)}`,
+            `0x${padLeft(notes[i].gamma.y.fromRed().toString(16), 64)}`,
+            `0x${padLeft(notes[i].sigma.x.fromRed().toString(16), 64)}`,
+            `0x${padLeft(notes[i].sigma.y.fromRed().toString(16), 64)}`,
+        ];
+    });
+    return {
+        proofData,
+        challenge: `0x${padLeft(challenge.toString(16), 64)}`,
+    };
+};
+
+/**
+ * Verify AZTEC bilateral swap proof transcript
+ *
+ * @method verifyBilateralSwap
+ * @param {Array[proofData]} proofData - proofData array of AZTEC notes
+ * @param {big number instance} challenge - challenge variable used in zero-knowledge protocol 
+ * @returns {number} - returns 1 if proof is validated, throws an error if not
+ */
+bilateralSwap.verifyBilateralSwap = (proofData, challenge, sender) => {
+    const proofDataBn = helpers.toBnAndAppendPoints(proofData);
+    const formattedChallenge = new BN(challenge.slice(2), 16);
+
+    const finalHash = new Keccak();
+
+    finalHash.appendBN(new BN(sender.slice(2), 16));
+
+    proofDataBn.forEach((proofElement) => {
+        finalHash.append(proofElement[6]);
+        finalHash.append(proofElement[7]);
+    });
+
+    const kBarArray = [];
+
+    // Validate that the commitments lie on the bn128 curve
+    proofDataBn.map((proofElement, i) => {
+        helpers.validateOnCurve(proofElement[2], proofElement[3]); // checking gamma point
+        helpers.validateOnCurve(proofElement[4], proofElement[5]); // checking sigma point
+    });
+
+    proofDataBn.map((proofElement, i) => {
+        let kBar = proofElement[0];
+        const aBar = proofElement[1];
+        const gamma = proofElement[6];
+        const sigma = proofElement[7];
+        let B;
+
+        /*
+        Explanation of the below if/else
+        - The purpose is to set kBar1 = kBar3 and kBar2 = kBar4
+        - i is used as an indexing variable, to keep track of whether we are at a maker note or taker note
+        - All kBars are stored in a kBarArray. When we arrive at the taker notes, we set bk equal to the bk of the corresponding 
+          maker note. This is achieved by 'jumping back' 2 index positions (i - 2) in the kBarArray, and setting the current
+          kBar equal to the element at the resulting position.
+        */
+
+        // Maker notes
+        if (i <= 1) {
+            B = gamma.mul(kBar).add(bn128.h.mul(aBar)).add(sigma.mul(formattedChallenge).neg());
+        } else { // taker notes
+            kBar = kBarArray[i-2];
+            B = gamma.mul(kBar).add(bn128.h.mul(aBar)).add(sigma.mul(formattedChallenge).neg());
+        }
+
+        finalHash.append(B);
+        kBarArray.push(kBar);
+
+        return {
+            kBar,
+            B,
+        };
+    });
+    
+    const recoveredChallenge = finalHash.keccak(groupReduction);
+    const finalChallenge = `0x${padLeft(recoveredChallenge.toString(16), 64)}`;
+
+    // Check if the recovered challenge, matches the original challenge. If so, proof construction is validated
+    if (finalChallenge !== challenge) {
+        throw new Error('proof validation failed');
+    } else {
+        return true;
+    }
+};
+
+module.exports = bilateralSwap;
