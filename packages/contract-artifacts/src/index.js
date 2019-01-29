683657cb9b0d3326e424b0ae70b26404eb1dd04d
Paul Berg <hello@paulrberg.com>
2019-01-29 14:32:15 +0000

    Added contract-artifacts package

---
+++ packages/contract-artifacts/src/index.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const ACE = path.join(__dirname, '..', 'artifacts', 'ACE.json');
+const AZTEC = path.join(__dirname, '..', 'artifacts', 'AZTEC.json');
+const AZTECERC20Bridge = path.join(__dirname, '..', 'artifacts', 'AZTECERC20Bridge.json');
+const AZTECInterface = path.join(__dirname, '..', 'artifacts', 'AZTECInterface.json');
+const AZTECJoinSplit = path.join(__dirname, '..', 'artifacts', 'AZTECJoinSplit.json');
+const AZTECJoinSplitInterface = path.join(__dirname, '..', 'artifacts', 'AZTECJoinSplitInterface.json');
+const Doorbell = path.join(__dirname, '..', 'artifacts', 'Doorbell.json');
+const ERC20 = path.join(__dirname, '..', 'artifacts', 'ERC20.json');
+const ERC20Interface = path.join(__dirname, '..', 'artifacts', 'ERC20Interface.json');
+const ERC20Mintable = path.join(__dirname, '..', 'artifacts', 'ERC20Mintable.json');
+const Extractor = path.join(__dirname, '..', 'artifacts', 'Extractor.json');
+const IERC20 = path.join(__dirname, '..', 'artifacts', 'IERC20.json');
+const JoinSplitABIEncoder = path.join(__dirname, '..', 'artifacts', 'JoinSplitABIEncoder.json');
+const NoteUtilies = path.join(__dirname, '..', 'artifacts', 'NoteUtilies.json');
+const Validator = path.join(__dirname, '..', 'artifacts', 'Validator.json');
+
+module.exports = {
+    ACE,
+    AZTEC,
+    AZTECERC20Bridge,
+    AZTECInterface,
+    AZTECJoinSplit,
+    AZTECJoinSplitInterface,
+    Doorbell,
+    ERC20,
+    ERC20Interface,
+    ERC20Mintable,
+    Extractor,
+    IERC20,
+    JoinSplitABIEncoder,
+    NoteUtilies,
+    Validator,
+};
