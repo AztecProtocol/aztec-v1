dde96f05eb422f77279c038bdb8272b6deb09a81
zac-williamson <blorktronics@gmail.com>
2019-01-31 12:30:13 +0000

    added some NoteRegistry.js tests
    removed '.only' from ACE.js

--- packages/protocol/test/ACE/ACE.js
+++ packages/protocol/test/ACE/ACE.js
@@ -6,15 +6,6 @@ const { exceptions } = require('@aztec/dev-utils');
 
 // ### Internal Dependencies
 
-/*
-    const aztecProof = require('aztec.js/proof/joinSplit');
-    const abiEncoder = require('../../aztec-crypto-js/abiEncoder');
-    const outputCoder = require('../../aztec-crypto-js/abiEncoder/outputCoder');
-    const { t2 } = require('../../aztec-crypto-js/params');
-    const secp256k1 = require('../../aztec-crypto-js/secp256k1');
-    const sign = require('../../aztec-crypto-js/eip712/sign');
-    const note = require('../../../aztec-crypto-js/note');
-*/
 const {
     proof,
     abiEncoder,
@@ -85,7 +76,7 @@ function encodeJoinSplitTransaction({
 const hx = new BN('7673901602397024137095011250362199966051872585513276903826533215767972925880', 10);
 const hy = new BN('8489654445897228341090914135473290831551238522473825886865492707826370766375', 10);
 
-contract.only('ACE', (accounts) => {
+contract('ACE', (accounts) => {
     // Creating a collection of tests that should pass
     describe('initialization tests', () => {
         let ace;
@@ -170,7 +161,6 @@ contract.only('ACE', (accounts) => {
             }));
             const proofOutput = outputCoder.getProofOutput(expectedOutput, 0);
             proofHash = outputCoder.hashProofOutput(proofOutput);
-            console.log('proof hash = ', proofHash);
         });
 
         it('will validate a join-split transaction!', async () => {
