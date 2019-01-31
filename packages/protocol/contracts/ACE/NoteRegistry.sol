be0cb55685d698a3054c6e3af4dde340735c55f7
zac-williamson <blorktronics@gmail.com>
2019-01-31 12:27:08 +0000

    Changed `NoteData` in `NoteRegistry.sol` to `Note`
    Fixed `NoteRegistry` constructor - correctly links ERC20 token

--- packages/protocol/contracts/ACE/NoteRegistry.sol
+++ packages/protocol/contracts/ACE/NoteRegistry.sol
@@ -2,12 +2,13 @@ pragma solidity 0.4.24;
 
 import "../../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
+import "./NoteUtilities.sol";
 import "./ACE.sol";
 
 contract NoteRegistry {
     using NoteUtilities for bytes;
 
-    struct NoteData {
+    struct Note {
         uint8 status;
         bytes5 createdOn;
         bytes5 destroyedOn;
@@ -25,23 +26,29 @@ contract NoteRegistry {
     Flags public flags;
     ERC20 public linkedToken;
     ACE public ace;
+
     uint256 public linkedTokenScalingFactor;
     address public registryOwner;
-    mapping(bytes32 => NoteData) public registry;
+    mapping(bytes32 => Note) public registry;
 
     constructor(
         bool _isPrivate,
         bool _isTracked,
+        uint256 _linkedTokenScalingFactor,
         address _linkedToken,
+        address _ace,
         address _owner
     ) public {
         flags = Flags(_isPrivate, _isTracked);
-        if (linkedToken != address(0)) {
+        if (_linkedToken != address(0)) {
             linkedToken = ERC20(_linkedToken);
         }
+        linkedTokenScalingFactor = _linkedTokenScalingFactor;
         registryOwner = _owner;
-        ace = ACE(msg.sender);
+        ace = ACE(_ace);
     }
+
+    event Debug(address test);
 
     function updateNoteRegistry(bytes _proofOutput, uint16 _proofType, address _proofSender) public returns (bool) {
         require(msg.sender == registryOwner);
@@ -63,41 +70,41 @@ contract NoteRegistry {
             require(flags.isPrivate == false, "private assets cannot have a public value!");
             require(flags.isTracked == true, "untracked assets cannot accept tokens!");
             if (publicValue < 0) {
-                require(linkedToken.transferFrom(publicOwner, this, uint256(-publicValue)), "transfer failed!");
                 totalSupply += uint256(-publicValue);
+                require(linkedToken.transferFrom(publicOwner, this, uint256(-publicValue)), "transfer failed!");
             } else {
-                require(linkedToken.transfer(publicOwner, uint256(publicValue)), "transfer failed!");
                 totalSupply -= uint256(publicValue);
+                require(linkedToken.transfer(publicOwner, uint256(publicValue)), "transfer failed!");
             }
         }
-
+        return true;
     }
 
     function updateInputNotes(bytes memory inputNotes) internal {
         for (uint i = 0; i < inputNotes.getLength(); i += 1) {
             (address owner, bytes32 noteHash,) = inputNotes.get(i).extractNote();
-            NoteData storage noteData = registry[noteHash];
-            require(noteData.status == 1, "input note does not exist!");
-            require(noteData.owner == owner, "input note owner does not match!");
-            noteData.status = uint8(2);
+            Note storage note = registry[noteHash];
+            require(note.status == 1, "input note does not exist!");
+            require(note.owner == owner, "input note owner does not match!");
+            note.status = uint8(2);
             // AZTEC uses timestamps to measure the age of a note, on timescales of days/months
             // The 900-ish seconds a miner can manipulate a timestamp should have little effect
             // solhint-disable-next-line not-rely-on-time
-            noteData.destroyedOn = bytes5(now);
+            note.destroyedOn = bytes5(now);
         }
     }
 
     function updateOutputNotes(bytes memory outputNotes) internal {
         for (uint i = 0; i < outputNotes.getLength(); i += 1) {
             (address owner, bytes32 noteHash,) = outputNotes.get(i).extractNote();
-            NoteData storage noteData = registry[noteHash];
-            require(noteData.status == 0, "output note exists!");
-            noteData.status = uint8(1);
+            Note storage note = registry[noteHash];
+            require(note.status == 0, "output note exists!");
+            note.status = uint8(1);
             // AZTEC uses timestamps to measure the age of a note on timescales of days/months
             // The 900-ish seconds a miner can manipulate a timestamp should have little effect
             // solhint-disable-next-line not-rely-on-time
-            noteData.createdOn = bytes5(now);
-            noteData.owner = owner;
+            note.createdOn = bytes5(now);
+            note.owner = owner;
         }
     }
 }
