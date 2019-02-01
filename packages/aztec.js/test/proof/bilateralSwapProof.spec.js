ca89ba1af207c1dd0e12e638df04377df4f7827e
thomas-waite <thomas.waite4@gmail.com>
2019-02-01 14:44:29 +0000

    Implemented PR feedback on the bilateralSwap tests for Javascript proof construction and verification code.
    Changes mainly focus around adding more tests. The tests added validate that the verification algo will reject the proof for the following scenarios:
    
    1) Challenge replaced with a random challenge
    
    2) Balancing relationship in proof statement is not satisfied (k1 != k3 || k2 != k4)
    
    3) Note data replaced with a random variable
    
    4) A blinding factor is replaced with a point at infinity
    
    5) A note element (gamma, sigma) is replaced with a point at infinity
    
    Als, organised the tests into two files - now have a Javascript proof construction set of tests and proof verification set of tests.

--- packages/aztec.js/test/proof/bilateralSwapProof.spec.js
+++ packages/aztec.js/test/proof/bilateralSwapProof.spec.js
@@ -1,135 +1,95 @@
 /* global, beforeEach, it:true */
 const BN = require('bn.js');
 const chai = require('chai');
-const web3Utils = require('web3-utils');
+const crypto = require('crypto');
+const { padLeft, randomHex, sha3 } = require('web3-utils');
 
-
+const bn128 = require('../../src/bn128');
 const bilateralProof = require('../../src/proof/bilateralSwap');
 const helpers = require('../../src/proof/bilateralSwap/helpers');
 const Keccak = require('../../src/keccak');
+const { K_MAX } = require('../../src/params');
 
 
 const { expect } = chai;
 
 
-describe('Validating bilateral swap proof construction and verification algos', () => {
-    describe('Validate properties of the proof construction algo', () => {
-        let testNotes;
-        let sender;
+function generateNoteValue() {
+    return new BN(crypto.randomBytes(32), 16).umod(new BN(K_MAX)).toNumber();
+}
 
-        beforeEach(() => {
-            testNotes = helpers.makeTestNotes([10, 20], [10, 20]);
 
-            // Dummy, random sender address for proof of concept
-            sender = web3Utils.randomHex(20);
-        });
+describe.only('AZTEC bilateral swap proof construction tests', () => {
+    let testNotes;
+    let sender;
 
-        it('validate that the bilateral swap blinding scalar relations are satisfied i.e. bk1 = bk3 and bk2 = bk4', () => {
-            const finalHash = new Keccak();
+    beforeEach(() => {
+        testNotes = helpers.makeTestNotes([10, 20], [10, 20]);
 
-            testNotes.forEach((note) => {
-                finalHash.append(note.gamma);
-                finalHash.append(note.sigma);
-            });
-
-            const { blindingFactors } = helpers.getBlindingFactorsAndChallenge(testNotes, finalHash);
-
-            const testk1 = (blindingFactors[0].bk).toString(16);
-            const testk2 = (blindingFactors[1].bk).toString(16);
-            const testk3 = (blindingFactors[2].bk).toString(16);
-            const testk4 = (blindingFactors[3].bk).toString(16);
-
-            expect(testk1).to.equal(testk3);
-            expect(testk2).to.equal(testk4);
-        });
-
-        it('validate that the proof data contains correct number of proof variables and is well formed', () => {
-            const { proofData } = bilateralProof.constructBilateralSwap(testNotes, sender);
-            expect(proofData.length).to.equal(4);
-            expect(proofData[0].length).to.equal(6);
-            expect(proofData[1].length).to.equal(6);
-            expect(proofData[2].length).to.equal(6);
-            expect(proofData[3].length).to.equal(6);
-        });
-
-        it('validate that the proof is correct, using the validation algo', () => {
-            const { proofData, challenge } = bilateralProof.constructBilateralSwap(testNotes, sender);
-            const result = bilateralProof.verifyBilateralSwap(proofData, challenge, sender);
-            expect(result).to.equal(true);
-        });
-
-        it('validate failure when inputs points are not on the curve', () => {
-            // Setting the x and y coordinates of the commitment to zero - i.e. not on the curve
-            // Should then fail to be validated, as points aren't on the curve
-            const zeroes = `${web3Utils.padLeft('0', 64)}`;
-            const noteString = `${zeroes}${zeroes}${zeroes}${zeroes}${zeroes}${zeroes}`;
-            const challengeString = `0x${web3Utils.padLeft(sender.slice(2), 64)}${web3Utils.padLeft('132', 64)}
-                                        ${web3Utils.padLeft('1', 64)}${noteString}`;
-            const challenge = web3Utils.sha3(challengeString, 'hex');
-            const proofData = [[`0x${web3Utils.padLeft('132', 64)}`, '0x0', '0x0', '0x0', '0x0', '0x0']];
-
-            try {
-                bilateralProof.verifyBilateralSwap(proofData, challenge, sender);
-            } catch (err) {
-                console.log('point not on the curve');
-            }
-        });
+        // Dummy, random sender address for proof of concept
+        sender = randomHex(20);
     });
 
-    describe('validate properties of the proof validation algo', () => {
-        let proofData;
-        let challenge;
-        let sender;
+    it('bilateralProof.constructBilateralSwap creates a proof where blinding scalar relations are satisfied i.e. bk1 = bk3 and bk2 = bk4', () => {
+        const finalHash = new Keccak();
 
-        beforeEach(() => {
-            const testNotes = helpers.makeTestNotes([10, 20], [10, 20]);
-
-            // Dummy, random sender address for proof of concept
-            sender = web3Utils.randomHex(20);
-
-            ({ proofData, challenge } = bilateralProof.constructBilateralSwap(testNotes, sender));
+        testNotes.forEach((note) => {
+            finalHash.append(note.gamma);
+            finalHash.append(note.sigma);
         });
 
-        it('validate that the kbar relations are satisfied i.e. kbar1 = kbar3 and kbar2 = kbar4', () => {
-            const proofDataBn = helpers.toBnAndAppendPoints(proofData);
-            const formattedChallenge = new BN(challenge.slice(2), 16);
+        const { blindingFactors } = helpers.getBlindingFactorsAndChallenge(testNotes, finalHash);
 
-            const finalHash = new Keccak();
+        const testk1 = (blindingFactors[0].bk).toString(16);
+        const testk2 = (blindingFactors[1].bk).toString(16);
+        const testk3 = (blindingFactors[2].bk).toString(16);
+        const testk4 = (blindingFactors[3].bk).toString(16);
 
-            proofDataBn.forEach((proofElement) => {
-                finalHash.append(proofElement[6]);
-                finalHash.append(proofElement[7]);
-            });
-
-            const { recoveredBlindingFactors } = helpers.recoverBlindingFactorsAndChallenge(proofDataBn,
-                formattedChallenge,
-                finalHash);
-
-            const testkBar1 = (recoveredBlindingFactors[0].kBar).toString(16);
-            const testkBar2 = (recoveredBlindingFactors[1].kBar).toString(16);
-            const testkBar3 = (recoveredBlindingFactors[2].kBar).toString(16);
-            const testkBar4 = (recoveredBlindingFactors[3].kBar).toString(16);
-
-            expect(testkBar1).to.equal(testkBar3);
-            expect(testkBar2).to.equal(testkBar4);
-        });
+        expect(testk1).to.equal(testk3);
+        expect(testk2).to.equal(testk4);
     });
 
-    describe('validate that proof construction algo is valid, using validation algo', () => {
-        let testNotes;
-        let sender;
+    it('bilateralProof.constructBilateralSwap creates a proof with well-formed outputs', () => {
+        const { proofData } = bilateralProof.constructBilateralSwap(testNotes, sender);
+        expect(proofData.length).to.equal(4);
+        console.log('proofData[0]', proofData[0]);
+        expect(proofData[0].length).to.equal(6);
+        expect(proofData[1].length).to.equal(6);
+        expect(proofData[2].length).to.equal(6);
+        expect(proofData[3].length).to.equal(6);
+    });
 
-        beforeEach(() => {
-            testNotes = helpers.makeTestNotes([10, 20], [10, 20]);
+    it('bilateralProof.constructBilateralSwap will throw for input notes of random value', () => {
+        const wrongNotes = helpers.makeTestNotes([generateNoteValue(), generateNoteValue()], [generateNoteValue(), generateNoteValue()]);
+        const { proofData, challenge } = bilateralProof.constructBilateralSwap(wrongNotes, sender);
 
-            // Dummy, random sender address for proof of concept
-            sender = web3Utils.randomHex(20);
-        });
+        try {
+            bilateralProof.verifier.verifyBilateralSwap(proofData, challenge, sender);
+        } catch (err) {
+            console.log('note inputs have wrong balancing relationship');
+        }
+    });
 
-        it('validate that the proof is correct, using the validation algo', () => {
-            const { proofData, challenge } = bilateralProof.constructBilateralSwap(testNotes, sender);
-            const result = bilateralProof.verifyBilateralSwap(proofData, challenge, sender);
-            expect(result).to.equal(true);
-        });
+    it('bilateralProof.constructBilateralSwap will throw for input points not on the curve', () => {
+        // Setting the x and y coordinates of the commitment to zero - i.e. not on the curve
+        // Should then fail to be validated, as points aren't on the curve
+        const zeroes = `${padLeft('0', 64)}`;
+        const noteString = [...Array(6)].reduce(acc => `${acc}${zeroes}`, '');
+        const challengeString = `${sender}${padLeft('132', 64)}${padLeft('1', 64)}${noteString}`;
+        const challenge = `0x${new BN(sha3(challengeString, 'hex').slice(2), 16).umod(bn128.curve.n).toString(16)}`;
+
+        const proofData = [...new Array(4)].map(() => [...new Array(6)].map(() => '0x00'));
+        // Making the kBars satisfy the proof relation, to ensure it's not an incorrect
+        // balancing relationship that causes the test to fail
+        proofData[0][0] = '0x1000000000000000000000000000000000000000000000000000000000000000'; // k_1
+        proofData[1][0] = '0x2000000000000000000000000000000000000000000000000000000000000000'; // k_2
+        proofData[2][0] = '0x1000000000000000000000000000000000000000000000000000000000000000'; // k_3
+        proofData[3][0] = '0x2000000000000000000000000000000000000000000000000000000000000000'; // k_4
+
+        try {
+            bilateralProof.verifier.verifyBilateralSwap(proofData, challenge, sender);
+        } catch (err) {
+            console.log('point not on the curve');
+        }
     });
 });
