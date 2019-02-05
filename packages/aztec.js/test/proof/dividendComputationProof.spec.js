0b98ace6d259501ad053ef923b23b5ea4f31fcee
thomas-waite <thomas.waite4@gmail.com>
2019-02-05 18:28:43 +0000

    Implemented PR feedback for tests associated with JS proof and verification
    1) dividendComputationProof.spec.js - seperated out proof tests into it's own module (this one)
    
    2) dividendComputationVerifier.spec.js - seperated verification tests out into it's own module (this one). Added tests that check:
    - Challenge replaced with a random challenge
    - Balancing relationship in proof statement is not satisfied (k1 != k3 || k2 != k4)
    - Note data replaced with a random variable
    - A blinding factor is replaced with a point at infinity
    - A note element (gamma, sigma) is replaced with a point at infinity

---
+++ packages/aztec.js/test/proof/dividendComputationProof.spec.js
@@ -0,0 +1,44 @@
+/* global, beforeEach, it:true */
+const chai = require('chai');
+const web3Utils = require('web3-utils');
+
+const dividendComputation = require('../../src/proof/dividendComputation');
+
+const { expect } = chai;
+
+
+describe('Dividend computation proof construction tests', () => {
+    let testNotes;
+    let sender;
+    let za;
+    let zb;
+
+    beforeEach(() => {
+        /*
+        Test case:
+        - k_in = 90
+        - Interest rate = 5%
+        - k_out = 4
+        - k_res = 5
+        - za = 5
+        - zb = 100
+        */
+
+        testNotes = dividendComputation.helpers.makeTestNotes([90], [4, 50]);
+        za = 100;
+        zb = 5;
+
+        // Dummy, random sender address for proof of concept
+        sender = web3Utils.randomHex(20);
+    });
+
+    it('dividendComputation.constructProof outputs proof data with correct number of proof variables and is well formed', () => {
+        const { proofDataUnformatted, proofData, challenge } = dividendComputation.constructProof(testNotes, za, zb, sender);
+        expect(proofDataUnformatted.length).to.equal(3);
+        expect(proofData.length).to.equal(18);
+        expect(challenge.length).to.equal(66);
+        expect(proofDataUnformatted[0].length).to.equal(6);
+        expect(proofDataUnformatted[1].length).to.equal(6);
+        expect(proofDataUnformatted[2].length).to.equal(6);
+    });
+});
