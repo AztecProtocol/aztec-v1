fd8128e1c50173d0e859106fc98652aacbbe7572
Paul Berg <hello@paulrberg.com>
2019-02-06 16:55:03 +0000

    Switch to using require-dir

--- packages/contract-artifacts/src/index.js
+++ packages/contract-artifacts/src/index.js
@@ -1,35 +1,5 @@
 const path = require('path');
+const requireDir = require('require-dir');
 
-const ACE = require(path.join(__dirname, '..', 'artifacts', 'ACE.json'));
-const AZTEC = require(path.join(__dirname, '..', 'artifacts', 'AZTEC.json'));
-const AZTECERC20Bridge = require(path.join(__dirname, '..', 'artifacts', 'AZTECERC20Bridge.json'));
-const AZTECInterface = require(path.join(__dirname, '..', 'artifacts', 'AZTECInterface.json'));
-const AZTECJoinSplit = require(path.join(__dirname, '..', 'artifacts', 'AZTECJoinSplit.json'));
-const AZTECJoinSplitInterface = require(path.join(__dirname, '..', 'artifacts', 'AZTECJoinSplitInterface.json'));
-const Doorbell = require(path.join(__dirname, '..', 'artifacts', 'Doorbell.json'));
-const ERC20 = require(path.join(__dirname, '..', 'artifacts', 'ERC20.json'));
-const ERC20Interface = require(path.join(__dirname, '..', 'artifacts', 'ERC20Interface.json'));
-const ERC20Mintable = require(path.join(__dirname, '..', 'artifacts', 'ERC20Mintable.json'));
-const Extractor = require(path.join(__dirname, '..', 'artifacts', 'Extractor.json'));
-const IERC20 = require(path.join(__dirname, '..', 'artifacts', 'IERC20.json'));
-const JoinSplitABIEncoder = require(path.join(__dirname, '..', 'artifacts', 'JoinSplitABIEncoder.json'));
-const NoteUtilities = require(path.join(__dirname, '..', 'artifacts', 'NoteUtilities.json'));
-const Validator = require(path.join(__dirname, '..', 'artifacts', 'Validator.json'));
-
-module.exports = {
-    ACE,
-    AZTEC,
-    AZTECERC20Bridge,
-    AZTECInterface,
-    AZTECJoinSplit,
-    AZTECJoinSplitInterface,
-    Doorbell,
-    ERC20,
-    ERC20Interface,
-    ERC20Mintable,
-    Extractor,
-    IERC20,
-    JoinSplitABIEncoder,
-    NoteUtilities,
-    Validator,
-};
+const artifacts = requireDir(path.join(__dirname, '..', 'artifacts'));
+module.exports = artifacts;
