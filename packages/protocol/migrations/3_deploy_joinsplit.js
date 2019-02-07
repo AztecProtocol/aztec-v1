b14de3adad6a991c2c10f2e81be1171101e6ed92
Paul Berg <hello@paulrberg.com>
2019-02-07 16:03:55 +0000

    Fix failing tests by adding the old aztec migrations

--- packages/protocol/migrations/3_deploy_joinsplit.js
+++ packages/protocol/migrations/3_deploy_joinsplit.js
@@ -1,21 +1,12 @@
 /* global artifacts */
-const { NetworkId } = require('@aztec/contract-addresses');
-const { constants: { FAKE_NETWORK_ID } } = require('@aztec/dev-utils');
-
 const JoinSplit = artifacts.require('./JoinSplit.sol');
 const JoinSplitInterface = artifacts.require('./JoinSplitInterface.sol');
 
 JoinSplit.abi = JoinSplitInterface.abi;
 
-const getNetworkId = (network) => {
-    const capitalisedNetwork = network.charAt(0).toUpperCase() + network.slice(1);
-    return NetworkId[capitalisedNetwork] || FAKE_NETWORK_ID;
-};
-
-module.exports = (deployer, network) => {
+module.exports = (deployer) => {
     // just a bytecode switcheroo, nothing to see here...
     // JoinSplit.bytecode = JoinSplit.bytecode.replace('JoinSplitInterface', 'JoinSplit');
     // JoinSplit.deployedBytecode = JoinSplit.deployedBytecode.replace('JoinSplitInterface', 'JoinSplit');
-    const networkId = getNetworkId(network);
-    return deployer.deploy(JoinSplit, networkId);
+    return deployer.deploy(JoinSplit, deployer.network_id);
 };
