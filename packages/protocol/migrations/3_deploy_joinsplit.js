c10877410e2aa12be798315cd109e61d4950b563
Paul Berg <hello@paulrberg.com>
2019-02-12 15:33:52 +0000

    Set CRS and proof on ACE after deployment

--- packages/protocol/migrations/3_deploy_joinsplit.js
+++ packages/protocol/migrations/3_deploy_joinsplit.js
@@ -1,9 +1,15 @@
 /* global artifacts */
+const ACE = artifacts.require('./ACE.sol');
 const JoinSplit = artifacts.require('./JoinSplit.sol');
 const JoinSplitInterface = artifacts.require('./JoinSplitInterface.sol');
 
 JoinSplit.abi = JoinSplitInterface.abi;
 
 module.exports = (deployer) => {
-    return deployer.deploy(JoinSplit, deployer.network_id);
+    return deployer.deploy(JoinSplit, deployer.network_id).then(async ({ address: joinSplitAddress }) => {
+        const proofId = 1;
+        const isBalanced = true;
+        const ace = await ACE.at(ACE.address);
+        await ace.setProof(proofId, joinSplitAddress, isBalanced);
+    });
 };
