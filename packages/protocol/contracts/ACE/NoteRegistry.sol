a802099dd081e8610e1a24ba9193ef9c1d96412c
Paul Berg <hello@paulrberg.com>
2019-02-19 18:16:22 +0000

    Remove AZTEC, AZTECERC20Bridge and demo
    Change version in contracts to ^0.5.0

--- packages/protocol/contracts/ACE/NoteRegistry.sol
+++ packages/protocol/contracts/ACE/NoteRegistry.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.24;
+pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
--- packages/protocol/contracts/ERC20/ERC20Mintable.sol
+++ packages/protocol/contracts/ERC20/ERC20Mintable.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
@@ -11,12 +11,12 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 contract ERC20Mintable is ERC20 {
     /**
     * @dev Function to mint tokens
-    * @param to The address that will receive the minted tokens.
-    * @param value The amount of tokens to mint.
+    * @param _to The address that will receive the minted tokens.
+    * @param _value The amount of tokens to mint.
     * @return A boolean that indicates if the operation was successful.
     */
-    function mint(address to, uint256 value) public returns (bool) {
-        _mint(to, value);
+    function mint(address _to, uint256 _value) public returns (bool) {
+        _mint(_to, _value);
         return true;
     }
 }
