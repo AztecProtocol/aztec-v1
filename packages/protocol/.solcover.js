f97c14417b38bba620a50b5ed8268f5d047a2497
Paul Berg <hello@paulrberg.com>
2019-02-21 19:50:10 +0000

    Switch to v5 compatible fork of solidity-coverage

--- packages/protocol/.solcover.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    norpc: true,
-    testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
-    compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
-    skipFiles: [
-      'node_modules'
-    ]
-  };
