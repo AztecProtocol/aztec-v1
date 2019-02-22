393bbbf44169252720cea86839156ac3b892ee72
Paul Berg <hello@paulrberg.com>
2019-02-22 15:15:28 +0000

    Nuke exceptions from dev-utils

--- packages/dev-utils/src/index.js
+++ packages/dev-utils/src/index.js
@@ -1,9 +1,7 @@
 const path = require('path');
 
 const constants = require(path.join(__dirname, 'constants'));
-const exceptions = require(path.join(__dirname, 'exceptions'));
 
 module.exports = {
     constants,
-    exceptions,
 };
