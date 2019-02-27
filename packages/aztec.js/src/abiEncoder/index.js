2765886388b077f6e1ed5d16015b2fdf8a41815e
zac-williamson <blorktronics@gmail.com>
2019-02-27 17:04:24 +0000

    fixed outputCoder expecting wrong format

--- packages/aztec.js/src/abiEncoder/index.js
+++ packages/aztec.js/src/abiEncoder/index.js
@@ -1,9 +1,11 @@
 const outputCoder = require('./outputCoder');
 const inputCoder = require('./inputCoder');
 const encoderFactory = require('./encoderFactory');
+const noteCoder = require('./noteCoder');
 
 module.exports = {
     encoderFactory,
     inputCoder,
     outputCoder,
+    noteCoder,
 };
--- packages/aztec.js/src/bn128/mcl/mcl_c512.js
+++ packages/aztec.js/src/bn128/mcl/mcl_c512.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 var Module = function(Module) {
   Module = Module || {};
 
