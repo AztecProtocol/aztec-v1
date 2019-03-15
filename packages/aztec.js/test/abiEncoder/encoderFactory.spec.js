f8c8e091544fe0489427dc357d37380589d76fd7
thomas-waite <thomas.waite4@gmail.com>
2019-03-15 13:37:26 +0000

    Implement test for mint proodata ABI encoding - proof.spec.js
    Minor tidying up elsewhere - remove .only on some tests and import local versions of npm modules

--- packages/aztec.js/test/abiEncoder/encoderFactory.spec.js
+++ packages/aztec.js/test/abiEncoder/encoderFactory.spec.js
@@ -7,6 +7,7 @@ const HexString = require('./HexString');
 const bilateralProof = require('../../src/proof/bilateralSwap');
 const joinSplitProof = require('../../src/proof/joinSplit');
 const dividendComputationProof = require('../../src/proof/dividendComputation');
+const mintProof = require('../../src/proof/mint');
 
 const abiEncoder = require('../../src/abiEncoder');
 const secp256k1 = require('../../src/secp256k1');
@@ -228,6 +229,76 @@ describe('inputCoder tests', () => {
             const offsetToMetadata = parseInt(result.slice(0xc0, 0xe0), 16);
             const metadataLength = parseInt(result.slice(offsetToMetadata - 0x20, offsetToMetadata), 16);
             expect(parseInt(result.slice(offsetToMetadata, offsetToMetadata + 0x20), 16)).to.equal(2);
+
+            const recoveredMetadata = result.slice(offsetToMetadata - 0x20, offsetToMetadata + metadataLength);
+            expect(recoveredMetadata).to.equal(abiEncoder.encoderFactory.encodeMetadata(outputNotes).data);
+        });
+    });
+
+
+    describe('mint tests work', () => {
+        it('mint is correctly formatted', () => {
+            // Setup
+            let accounts = [];
+            let notes = [];
+
+            const numNotes = 4;
+            const noteValues = [50, 30, 10, 10];
+            accounts = [...new Array(numNotes)].map(() => secp256k1.generateAccount());
+            notes = accounts.map(({ publicKey }, i) => {
+                return note.create(publicKey, noteValues[i]);
+            });
+
+            const inputNotes = notes.slice(0, 1);
+            const outputNotes = notes.slice(1, 4);
+
+            const senderAddress = accounts[0].address;
+
+            // Main
+            const {
+                proofData,
+                challenge,
+            } = mintProof.constructProof([...inputNotes, ...outputNotes], senderAddress, 0);
+
+            const inputOwners = inputNotes.map(n => n.owner);
+            const outputOwners = outputNotes.map(n => n.owner);
+
+            const result = new HexString(abiEncoder.inputCoder.mint(
+                proofData,
+                challenge,
+                inputOwners,
+                outputOwners,
+                outputNotes
+            ).slice(2));
+
+            expect(result.slice(0x00, 0x20)).to.equal(padLeft(challenge.slice(2), 64));
+
+            const offsetToProofData = parseInt(result.slice(0x20, 0x40), 16);
+            expect(parseInt(result.slice(offsetToProofData - 0x20, offsetToProofData), 16)).to.equal(4); // there are 4 notes in this test
+            const recoveredProofData = new HexString(result.slice(offsetToProofData, offsetToProofData + (4 * 0xc0)));
+
+            for (let i = 0; i < numNotes; i += 1) {
+                const recoveredNote = recoveredProofData.slice((i * 0xc0), ((i * 0xc0) + 0xc0));
+                expect(recoveredNote).to.equal(proofData[i].map(p => p.slice(2)).join(''));
+            }
+
+            const numInputNotes = 1;
+            const offsetToInputOwners = parseInt(result.slice(0x40, 0x60), 16);
+            expect(parseInt(result.slice(offsetToInputOwners - 0x20, offsetToInputOwners), 16)).to.equal(numInputNotes); // 1 input note
+            const recoveredInputOwners = new HexString(result.slice(offsetToInputOwners, offsetToInputOwners + 0x60));
+            expect(recoveredInputOwners.slice(0x00, 0x20)).to.equal(padLeft(inputOwners[0].slice(2).toLowerCase(), 64));
+
+
+            const numOutputNotes = 3;
+            const offsetToOutputOwners = parseInt(result.slice(0x60, 0x80), 16);
+            expect(parseInt(result.slice(offsetToOutputOwners - 0x20, offsetToOutputOwners), 16)).to.equal(numOutputNotes); // 3 output notes
+            const recoveredOutputOwners = new HexString(result.slice(offsetToOutputOwners, offsetToOutputOwners + (2 * 0x20)));
+            expect(recoveredOutputOwners.slice(0x00, 0x20)).to.equal(padLeft(outputOwners[0].slice(2).toLowerCase(), 64));
+            expect(recoveredOutputOwners.slice(0x20, 0x40)).to.equal(padLeft(outputOwners[1].slice(2).toLowerCase(), 64));
+            const offsetToMetadata = parseInt(result.slice(0x80, 0xa0), 16);
+
+            const metadataLength = parseInt(result.slice(offsetToMetadata - 0x20, offsetToMetadata), 16);
+            expect(parseInt(result.slice(offsetToMetadata, offsetToMetadata + 0x20), 16)).to.equal(numOutputNotes);
 
             const recoveredMetadata = result.slice(offsetToMetadata - 0x20, offsetToMetadata + metadataLength);
             expect(recoveredMetadata).to.equal(abiEncoder.encoderFactory.encodeMetadata(outputNotes).data);
