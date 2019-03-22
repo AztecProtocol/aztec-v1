036853aac4258ec8426d52aaebb6ba4f3da70862
Paul Berg <hello@paulrberg.com>
2019-03-22 09:33:15 +0100

    Use the updated sig names in protocol

--- packages/protocol/contracts/ACE/validators/bilateralSwap/BilateralSwapABIEncoder.sol
+++ packages/protocol/contracts/ACE/validators/bilateralSwap/BilateralSwapABIEncoder.sol
@@ -1,6 +1,11 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 library BilateralSwapABIEncoder {
+
+    // keccak256 hash of "JoinSplitSignature(uint24 proof,bytes32[4] note,uint256 challenge,address sender)"
+    bytes32 constant internal JOIN_SPLIT_SIGNATURE_TYPE_HASH =
+        0x904692743a9f431a791d777dd8d42e18e79888579fa6807b5f17b14020210e30;
+
     /**
     * New calldata map
     * 0x04:0x24      = calldata location of proofData byte array - pointer to the proofData. 
@@ -21,6 +26,7 @@ library BilateralSwapABIEncoder {
     **/
 
     function encodeAndExit() internal pure {
+        bytes32 typeHash = JOIN_SPLIT_SIGNATURE_TYPE_HASH;
         assembly {
             // set up initial variables
             let notes := add(0x104, calldataload(0x144))
@@ -39,7 +45,7 @@ library BilateralSwapABIEncoder {
             // 0xe0 = challenge
             // 0x100 = sender
             // type hash of 'JOIN_SPLIT_SIGNATURE'
-            mstore(0x80, 0x21853faea366a53b2f6a3cbf1da39ef94d2dbb994639a58005781220badbd5df)
+            mstore(0x80, typeHash)
             mstore(0xa0, 0x10002)
             mstore(0xe0, calldataload(0x124)) // challenge
             mstore(0x100, calldataload(0x24))
