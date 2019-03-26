024c61c796d57a53aa1f50d1364517293ba286d5
zac-williamson <blorktronics@gmail.com>
2019-03-26 14:56:25 +0000

    minor tweaks to utility libraries

--- packages/protocol/contracts/libs/LibEIP712.sol
+++ packages/protocol/contracts/libs/LibEIP712.sol
@@ -1,8 +1,6 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 /*
-  Copyright Spilsbury Holdings Ltd 2018.
-  
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
--- packages/protocol/contracts/libs/SafeMath8.sol
+++ packages/protocol/contracts/libs/SafeMath8.sol
@@ -3,29 +3,25 @@ pragma solidity >=0.5.0 <= 0.6.0;
 library SafeMath8 {
     
     function mul(uint8 a, uint8 b) internal pure returns (uint8) {
-        if (a == 0) {
-            return 0;
-        }
-        uint8 c = a * b;
-        assert(c / a == b);
-        return c;
+        uint256 c = uint256(a) * uint256(b);
+        require(c < 256, "uint8 mul triggered integer overflow");
+        return uint8(c);
     }
 
     function div(uint8 a, uint8 b) internal pure returns (uint8) {
         // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint8 c = a / b;
         // assert(a == b * c + a % b); // There is no case in which this doesn’t hold
-        return c;
+        return a / b;
     }
 
     function sub(uint8 a, uint8 b) internal pure returns (uint8) {
-        assert(b <= a);
+        require(b <= a, "uint8 sub triggered integer underflow");
         return a - b;
     }
 
     function add(uint8 a, uint8 b) internal pure returns (uint8) {
         uint8 c = a + b;
-        assert(c >= a);
+        require(c >= a, "uint8 add triggered integer overflow");
         return c;
     }
 }
