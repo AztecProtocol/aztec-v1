843540a0eb31f308d2b06e72575205187ffbf4bd
thomas-waite <thomas.waite4@gmail.com>
2019-03-26 10:40:01 +0000

    Implement ZkAssetBurnable.sol and accompanying test

---
+++ packages/protocol/test/ZkAsset/ZkAssetBurnable.js
@@ -0,0 +1,91 @@
+/* global artifacts, expect, contract, beforeEach, it:true */
+// ### External Dependencies
+const BN = require('bn.js');
+const crypto = require('crypto');
+const { padLeft } = require('web3-utils');
+
+
+// ### Internal Dependencies
+// eslint-disable-next-line object-curly-newline
+const { note, proof, secp256k1 } = require('aztec.js');
+const { constants, proofs: { BURN_PROOF } } = require('@aztec/dev-utils');
+
+// ### Artifacts
+const ERC20Mintable = artifacts.require('./contracts/ERC20/ERC20Mintable');
+const ACE = artifacts.require('./contracts/ACE/ACE');
+const AdjustSupply = artifacts.require('./contracts/ACE/validators/AdjustSupply');
+const AdjustSupplyInterface = artifacts.require('./contracts/ACE/validators/AdjustSupplyInterface');
+const ZkAssetBurnable = artifacts.require('./contracts/ZkAsset/ZkAssetBurnable');
+
+
+AdjustSupply.abi = AdjustSupplyInterface.abi;
+
+contract.only('ZkAssetBurnable', (accounts) => {
+    describe('success states', () => {
+        let aztecAccounts = [];
+        let notes = [];
+        let ace;
+        let proofData;
+        let erc20;
+        let zkAssetBurnable;
+        let scalingFactor;
+        let aztecAdjustSupply;
+        let oldTotalBurned;
+
+        beforeEach(async () => {
+            ace = await ACE.new({ from: accounts[0] });
+            aztecAccounts = [...new Array(4)].map(() => secp256k1.generateAccount());
+            const noteValues = [50, 0, 30, 20]; // note we do not use this third note, we create one
+            notes = aztecAccounts.map(({ publicKey }, i) => {
+                return note.create(publicKey, noteValues[i]);
+            });
+
+            await ace.setCommonReferenceString(constants.CRS);
+            aztecAdjustSupply = await AdjustSupply.new();
+            await ace.setProof(BURN_PROOF, aztecAdjustSupply.address);
+
+            const newTotalBurned = notes[0];
+            const adjustedNotes = notes.slice(2, 4);
+
+            // Creating a fixed note
+            const a = padLeft('1', 64);
+            const k = padLeft('0', 8);
+            const ephemeral = secp256k1.ec.keyFromPrivate(crypto.randomBytes(32));
+            const viewingKey = `0x${a}${k}${padLeft(ephemeral.getPublic(true, 'hex'), 66)}`;
+            oldTotalBurned = note.fromViewKey(viewingKey);
+            // TODO: add default note to common reference string
+
+            const canMint = true;
+            const canBurn = true;
+            const canConvert = false;
+
+
+            erc20 = await ERC20Mintable.new();
+            scalingFactor = new BN(10);
+            zkAssetBurnable = await ZkAssetBurnable.new(
+                ace.address,
+                erc20.address,
+                scalingFactor,
+                canMint,
+                canBurn,
+                canConvert,
+                { from: accounts[0] }
+            );
+
+            ({ proofData } = proof.burn.encodeBurnTransaction({
+                newTotalBurned,
+                oldTotalBurned,
+                adjustedNotes,
+                senderAddress: zkAssetBurnable.address,
+            }));
+
+            console.log('sender address: ', accounts[0]);
+            console.log('zkAsset address: ', zkAssetBurnable.address);
+        });
+
+        it('should complete a burn operation', async () => {
+            const { receipt } = await zkAssetBurnable.confidentialBurn(BURN_PROOF, proofData);
+            expect(receipt.status).to.equal(true);
+        });
+    });
+});
