d5a794cea2fd3a39da0fa5e1402b635d14221826
thomas-waite <thomas.waite4@gmail.com>
2019-03-29 10:57:43 +0000

    Implement PR feedback
    Refactor mint and burn proof construction to check if the number of inputted notes is sufficient (via proofUtils.parseInputs) and then to call the joinSplit proof construction code. Similar refactoring for mint and burn proof verification
    
    Refactored proofUtils.parseInputs() to include a proofIdentifier argument, which checks note number if proof is mint or burn
    
    Similarly, refactored proofUtils.convertTranscript() to push INCORRECT_NOTE_NUMBER error into error array if mint or burn and wrong num notes
    
    Added in a

--- packages/aztec.js/src/proof/mint/verifier.js
+++ packages/aztec.js/src/proof/mint/verifier.js
@@ -5,94 +5,44 @@
  * @memberof module:proof.mint
  */
 const utils = require('@aztec/dev-utils');
-const BN = require('bn.js');
-const { padLeft } = require('web3-utils');
 
-const bn128 = require('../../bn128');
-const Keccak = require('../../keccak');
-const proofUtils = require('../proofUtils');
+const joinSplit = require('../joinSplit');
 
 const { errorTypes } = utils.constants;
 
-const { groupReduction } = bn128;
 
 const verifier = {};
 
 /**
- * Verify an AZTEC mint zero-knowledge proof
- *
- * @method verifyProof
- * @memberof module:proof.mint.verifier
- * @param {string[]} proofData AZTEC join-split zero-knowledge proof data
- * @param {number} m number of input notes
- * @param {string} challengeHex hex-string formatted proof challenge
- * @param {string} sender Ethereum address of transaction sender
- */
+* Verify an AZTEC mint zero-knowledge proof
+*
+* @method verifyProof
+* @memberof module:proof.burn.verifier
+* @param {string[]} proofData AZTEC join-split zero-knowledge proof data
+* @param {number} m number of input notes
+* @param {string} challengeHex hex-string formatted proof challenge
+* @param {string} sender Ethereum address of transaction sender
+*/
+
 verifier.verifyProof = (proofData, challengeHex, sender) => {
     const m = 1;
-    const errors = [];
+    let proofDataArray;
 
-    const {
-        rollingHash,
-        notes,
-        challenge,
-    } = proofUtils.convertTranscript(proofData, m, challengeHex, errors, 'mint');
-
-    const finalHash = new Keccak();
-    finalHash.appendBN(new BN(sender.slice(2), 16));
-    finalHash.data = [...finalHash.data, ...rollingHash.data];
-
-    let x = new BN(0).toRed(groupReduction);
-
-    let pairingGammas;
-    let pairingSigmas;
-
-    notes.forEach((note, i) => {
-        let { kBar, aBar } = note;
-        let c = challenge;
-
-        if (i >= m) {
-            x = rollingHash.keccak(groupReduction);
-            kBar = kBar.redMul(x);
-            aBar = aBar.redMul(x);
-            c = challenge.redMul(x);
-        }
-
-        const sigma = note.sigma.mul(c).neg();
-        const B = note.gamma.mul(kBar)
-            .add(bn128.h.mul(aBar))
-            .add(sigma);
-        if (i === m) {
-            pairingGammas = note.gamma;
-            pairingSigmas = sigma.neg();
-        } else if (i > m) {
-            pairingGammas = pairingGammas.add(note.gamma.mul(c));
-            pairingSigmas = pairingSigmas.add(sigma);
-        }
-        if (B.isInfinity()) {
-            errors.push(errorTypes.BAD_BLINDING_FACTOR);
-            finalHash.appendBN(new BN(0));
-            finalHash.appendBN(new BN(0));
-        } else if (B.x.fromRed().eq(new BN(0)) && B.y.fromRed().eq(new BN(0))) {
-            errors.push(errorTypes.BAD_BLINDING_FACTOR);
-            finalHash.append(B);
-        } else {
-            finalHash.append(B);
-        }
-    });
-    const challengeResponse = finalHash.keccak(groupReduction);
-    const finalChallenge = `0x${padLeft(challengeResponse.toString(16), 64)}`;
-
-    if (finalChallenge !== challengeHex) {
-        errors.push(errorTypes.CHALLENGE_RESPONSE_FAIL);
+    if (!Array.isArray(proofData)) {
+        proofDataArray = [proofData];
+    } else {
+        proofDataArray = proofData;
     }
-    const valid = errors.length === 0;
-    return {
-        valid,
-        errors,
-        pairingGammas,
-        pairingSigmas,
-    };
+
+    const result = joinSplit.verifier.verifyProof(proofData, m, challengeHex, sender);
+
+    const numNotes = proofDataArray.length;
+
+    if (numNotes < 2) {
+        result.errors.push(errorTypes.INCORRECT_NOTE_NUMBER);
+    }
+
+    return result;
 };
 
 module.exports = verifier;
