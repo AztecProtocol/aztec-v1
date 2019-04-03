b41f88d33404803ff0a7200c3ceafd7d02942cf7
thomas-waite <thomas.waite4@gmail.com>
2019-04-03 16:30:50 +0100

    Implement majority of bilateralSwaps additional tests in protocol/.../validators/bilateralSwap/index.js
    Introduce a constructProofTest() method in aztec.js/.../proof/bilateralSwap/index.js, for use in some of the tests
    
    Increase truffle gas limit in truffle-config - to allow tests with large numbers of notes

--- packages/aztec.js/src/proof/bilateralSwap/index.js
+++ packages/aztec.js/src/proof/bilateralSwap/index.js
@@ -156,6 +156,111 @@ bilateralSwap.encodeBilateralSwapTransaction = ({
         },
     ]).slice(0x42)}`;
     return { proofData, expectedOutput };
+/**
+ * Strictly for testing purposes only. 
+ * 
+ * Construct AZTEC bilateral swap proof transcript, with additional testVariable input. 
+ * Used for testing purposes - allows the chosen variable to not be inputted into the hash
+ * used to construct the challenge
+ *
+ * @method constructProofTest
+ * @param {note[]} notes array of AZTEC notes
+ * @param {address} sender address from which the transaction is sent
+ * @param {string} testVariable variable which will not be added into the hash for the challenge,
+ * strictly for testing purposes
+ * @returns {string[]} - proof data
+ * @returns {string} - challenge
+ */
+bilateralSwap.constructProofTest = (notes, sender, testVariable) => {
+    const bkArray = [];
+    let challenge;
+
+    notes.forEach((note) => {
+        const gammaOnCurve = bn128.curve.validate(note.gamma);
+        const sigmaOnCurve = bn128.curve.validate(note.sigma);
+
+        if ((gammaOnCurve === false) || (sigmaOnCurve === false)) {
+            throw customError(
+                errorTypes.NOT_ON_CURVE,
+                {
+                    message: 'A group element is not on the bn128 curve',
+                    gammaOnCurve,
+                    sigmaOnCurve,
+                    note,
+                }
+            );
+        }
+    });
+
+    proofUtils.parseInputs(notes, sender);
+
+    const blindingFactors = notes.map((note, i) => {
+        let bk = bn128.randomGroupScalar();
+        const ba = bn128.randomGroupScalar();
+        let B;
+
+
+        /*
+        Explanation of the below if/else
+        - The purpose is to set bk1 = bk3 and bk2 = bk4
+        - i is used as an indexing variable, to keep track of whether we are at a maker note or taker note
+        - All bks are stored in a bkArray. When we arrive at the taker notes, we set bk equal to the bk of the corresponding
+          maker note. This is achieved by 'jumping back' 2 index positions (i - 2) in the bkArray, and setting the current
+          bk equal to the element at the resulting position.
+        */
+
+        // Maker notes
+        if (i <= 1) {
+            B = note.gamma.mul(bk).add(bn128.h.mul(ba));
+        } else { // taker notes
+            bk = bkArray[i - 2];
+            B = note.gamma.mul(bk).add(bn128.h.mul(ba));
+        }
+        bkArray.push(bk);
+
+        return {
+            bk,
+            ba,
+            B,
+        };
+    });
+
+    if (testVariable === 'notes') {
+        challenge = proofUtils.computeChallenge(sender, blindingFactors);
+    } else if (testVariable === 'sender') {
+        challenge = proofUtils.computeChallenge(notes, blindingFactors);
+    } else if (testVariable === 'blindingFactors') {
+        challenge = proofUtils.computeChallenge(sender, notes);
+    } else {
+        challenge = proofUtils.computeChallenge(sender, notes, blindingFactors);
+    }
+
+    const proofData = blindingFactors.map((blindingFactor, i) => {
+        let kBar;
+
+        // Only set the first 2 values of kBar - the third and fourth are later inferred
+        // from a cryptographic relation. Set the third and fourth to random values
+        if (i <= 1) {
+            kBar = ((notes[i].k.redMul(challenge)).redAdd(blindingFactor.bk)).fromRed();
+        } else {
+            kBar = padLeft(new BN(crypto.randomBytes(32), 16).umod(bn128.curve.n).toString(16), 64);
+        }
+
+        const aBar = ((notes[i].a.redMul(challenge)).redAdd(blindingFactor.ba)).fromRed();
+
+        return [
+            `0x${padLeft(kBar.toString(16), 64)}`,
+            `0x${padLeft(aBar.toString(16), 64)}`,
+            `0x${padLeft(notes[i].gamma.x.fromRed().toString(16), 64)}`,
+            `0x${padLeft(notes[i].gamma.y.fromRed().toString(16), 64)}`,
+            `0x${padLeft(notes[i].sigma.x.fromRed().toString(16), 64)}`,
+            `0x${padLeft(notes[i].sigma.y.fromRed().toString(16), 64)}`,
+        ];
+    });
+    return {
+        proofData,
+        challenge: `0x${padLeft(challenge.toString(16), 64)}`,
+    };
 };
 
 module.exports = bilateralSwap;
