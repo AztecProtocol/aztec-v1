6db773e2cc433e0122ac716aa04bed8c77ebd914
Paul Berg <hello@paulrberg.com>
2019-04-06 16:34:47 +0100

    Nuke out IntegerUtils

--- packages/protocol/contracts/libs/LibEIP712.sol
+++ packages/protocol/contracts/libs/LibEIP712.sol
@@ -1,21 +1,11 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-/*
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
-  Forked from https://github.com/0xProject/0x-monorepo
-*/
-
+/**
+ * @title Library of EIP712 utility constants and functions
+ * @author AZTEC
+ * Copyright Spilbury Holdings Ltd 2019. All rights reserved.
+ **/
 contract LibEIP712 {
 
     // EIP712 Domain Name value
