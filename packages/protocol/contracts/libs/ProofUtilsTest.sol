c7140b39ccbf24f6e7c92cfaea2f55bde7c581d2
Paul Berg <hello@paulrberg.com>
2019-04-06 16:54:08 +0100

    Write tests for ProofUtils

--- packages/protocol/contracts/libs/ProofUtilsTest.sol
+++ packages/protocol/contracts/libs/ProofUtilsTest.sol
@@ -1,11 +1,14 @@
 pragma solidity >= 0.5.0 <0.6.0;
 
+import "./ProofUtils.sol";
+
 /**
  * @title Library of proof utility functions
  * @author AZTEC
  * Copyright Spilbury Holdings Ltd 2019. All rights reserved.
  **/
-library ProofUtils {
+contract ProofUtilsTest {
+    using ProofUtils for uint24;
 
     /**
      * @dev We compress three uint8 numbers into only one uint24 to save gas.
@@ -13,12 +16,7 @@ library ProofUtils {
      * @param proof The compressed uint24 number.
      * @return A tuple (uint8, uint8, uint8) representing the epoch, category and proofId.
      */
-    function getProofComponents(uint24 proof) internal pure returns (uint8 epoch, uint8 category, uint8 id) {
-        assembly {
-            id := and(proof, 0xff)
-            category := and(div(proof, 0x100), 0xff)
-            epoch := and(div(proof, 0x10000), 0xff)
-        }
-        return (epoch, category, id);
+    function getProofComponents(uint24 proof) public pure returns (uint8 epoch, uint8 category, uint8 id) {
+        return proof.getProofComponents();
     }
 }
