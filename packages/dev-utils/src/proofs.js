3f1f6a8fa1b2b9d9bbbefe7cd237424b00e38ed6
Paul Berg <hello@paulrberg.com>
2019-04-12 16:12:50 +0100

    feat(dev-utils): add BILATERAL_SWAP_PROOF and DIVIDEND_PROOF

--- packages/dev-utils/src/proofs.js
+++ packages/dev-utils/src/proofs.js
@@ -3,12 +3,15 @@
  * so we need to compress three uint8 (epoch, category, id) by shifting the
  * epoch and the category by 16 and 8 bits, respectively. For example:
  *
- * 65537 = 1 * 256**(2) + 0 * 256**(1) + 1 * 256**(0)
+ * 65793 = 1 * 256**(2) + 1 * 256**(1) + 1 * 256**(0)
  */
 module.exports = {
+    BILATERAL_SWAP_PROOF: '65794',
     BOGUS_PROOF: '65538',
     BURN_PROOF: '66305',
+    DIVIDEND_PROOF: '66561',
     JOIN_SPLIT_PROOF: '65793',
     MINT_PROOF: '66049',
+    // TODO: remove this duplicate value
     UTILITY_PROOF: '66561',
 };
