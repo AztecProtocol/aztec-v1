283a3271a8fbdab28850cf37ad92ca0a87840a21
thomas-waite <thomas.waite4@gmail.com>
2019-04-15 12:17:19 +0100

    docs: add docs for the contract

--- packages/protocol/contracts/ERC1724/ZkAssetMintable.sol
+++ packages/protocol/contracts/ERC1724/ZkAssetMintable.sol
@@ -16,8 +16,7 @@ import "./ZkAssetOwnable.sol";
  * @author AZTEC
  * @dev A contract defining the standard interface and behaviours of a confidential mintable asset. 
  * Copyright Spilbury Holdings Ltd 2019. All rights reserved.
-**/
-
+ **/
 contract ZkAssetMintable is ZkAssetOwnable {
     event UpdateTotalMinted(bytes32 noteHash, bytes noteData);
     address public owner;
