97565ef088796ab35f3a0a999e528a9ac3dfb015
thomas-waite <thomas.waite4@gmail.com>
2019-04-17 11:00:49 +0100

    test(protocol): implement test for zk public range proof validator
    [ci skip]

---
+++ packages/protocol/test/ACE/validators/publicRange/index.js
@@ -0,0 +1,91 @@
+/* global artifacts, expect, contract, beforeEach, it:true */
+// ### External Dependencies
+const sinon = require('sinon');
+const BN = require('bn.js');
+const crypto = require('crypto');
+const truffleAssert = require('truffle-assertions');
+const { sha3, padLeft } = require('web3-utils');
+
+// ### Internal Dependencies
+const {
+    secp256k1,
+    note,
+    proof: { proofUtils, publicRange },
+    abiEncoder: { outputCoder, inputCoder, encoderFactory },
+    bn128,
+    keccak,
+} = require('aztec.js');
+const { constants } = require('@aztec/dev-utils');
+
+// ### Artifacts
+
+const PublicRange = artifacts.require('./PublicRange');
+const PublicRangeInterface = artifacts.require('./PublicRangeInterface');
+
+PublicRange.abi = PublicRangeInterface.abi;
+
+const Keccak = keccak;
+
+contract.only('Public range proof tests', (accounts) => {
+    let publicRangeContract;
+    describe('Success States', () => {
+        beforeEach(async () => {
+            publicRangeContract = await PublicRange.new({
+                from: accounts[0],
+            });
+
+            publicRangeContract = await PublicRange.new({
+                from: accounts[0],
+            });
+        });
+
+        it('validate public range proof using JavaScript validator', async () => {
+            const noteValues = [50, 40];
+            const numNotes = noteValues.length;
+            const aztecAccounts = [...new Array(numNotes)].map(() => secp256k1.generateAccount());
+            const notes = aztecAccounts.map(({ publicKey }, i) => {
+                return note.create(publicKey, noteValues[i]);
+            });
+
+            const inputNotes = notes.slice(0, 1);
+            const outputNotes = notes.slice(1, 2);
+            const u = 10;
+            const senderAddress = accounts[0];
+
+            const { proofData, challenge } = publicRange.constructProof([...inputNotes, ...outputNotes], u, senderAddress);
+            const { valid } = publicRange.verifier.verifyProof(proofData, challenge, senderAddress, u);
+            expect(valid).to.equal(true);
+        });
+
+        it.only('should validate encoding of a public range zero-knowledge proof', async () => {
+            const noteValues = [50, 40];
+            const numNotes = noteValues.length;
+            const aztecAccounts = [...new Array(numNotes)].map(() => secp256k1.generateAccount());
+            const notes = aztecAccounts.map(({ publicKey }, i) => {
+                return note.create(publicKey, noteValues[i]);
+            });
+
+            const inputNotes = notes.slice(0, 1);
+            const outputNotes = notes.slice(1, 2);
+            const u = 10;
+            const senderAddress = accounts[0];
+
+            const { proofData, challenge } = publicRange.encodePublicRangeTransaction({
+                inputNotes,
+                outputNotes,
+                u,
+                senderAddress,
+            });
+
+
+            const result = await publicRangeContract.validatePublicRange(proofData, accounts[0], constants.CRS, {
+                from: accounts[0],
+                gas: 4000000,
+            });
+
+            console.log('original challenge: ', challenge);
+            console.log('recovered challenge: ', result);
+            expect(result).to.equal(challenge);
+        });
+    });
+});
