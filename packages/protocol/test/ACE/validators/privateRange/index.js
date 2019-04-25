84b01f0491b2ed398f3d9a2001422e9e9efe51ab
thomas-waite <thomas.waite4@gmail.com>
2019-04-25 14:40:41 +0100

    test(protocol): implement further success, failure private range tests

--- packages/protocol/test/ACE/validators/privateRange/index.js
+++ packages/protocol/test/ACE/validators/privateRange/index.js
@@ -3,6 +3,7 @@
 const truffleAssert = require('truffle-assertions');
 const { padLeft, sha3 } = require('web3-utils');
 const crypto = require('crypto');
+const BN = require('bn.js');
 
 // ### Internal Dependencies
 const { constants } = require('@aztec/dev-utils');
@@ -11,7 +12,8 @@ const {
     proof: { privateRange },
     note,
     secp256k1,
-    abiEncoder: { inputCoder },
+    abiEncoder: { inputCoder, outputCoder, encoderFactory },
+    bn128,
 } = require('aztec.js');
 
 // ### Artifacts
@@ -20,7 +22,7 @@ const PrivateRangeInterface = artifacts.require('./PrivateRangeInterface');
 
 PrivateRange.abi = PrivateRangeInterface.abi;
 
-contract('PrivateRange', (accounts) => {
+contract.only('PrivateRange', (accounts) => {
     let privateRangeContract;
     describe('Success States', () => {
         beforeEach(async () => {
@@ -38,7 +40,7 @@ contract('PrivateRange', (accounts) => {
             const comparisonNote = notes[1];
             const utilityNote = notes[2];
 
-            const { proofData } = privateRange.encodePrivateRangeTransaction({
+            const { proofData, expectedOutput } = privateRange.encodePrivateRangeTransaction({
                 originalNote,
                 comparisonNote,
                 utilityNote,
@@ -52,11 +54,118 @@ contract('PrivateRange', (accounts) => {
 
             const result = await privateRangeContract.validatePrivateRange(proofData, accounts[0], constants.CRS, opts);
 
-            expect(result).to.equal(true);
+            expect(result).to.equal(expectedOutput);
+        });
+
+        it('validate it works with input notes of zero value', async () => {
+            const noteValues = [10, 0, 10];
+            const aztecAccounts = [...new Array(3)].map(() => secp256k1.generateAccount());
+            const notes = [...aztecAccounts.map(({ publicKey }, i) => note.create(publicKey, noteValues[i]))];
+
+            const originalNote = notes[0];
+            const comparisonNote = notes[1];
+            const utilityNote = notes[2];
+
+            const { proofData, expectedOutput } = privateRange.encodePrivateRangeTransaction({
+                originalNote,
+                comparisonNote,
+                utilityNote,
+                senderAddress: accounts[0],
+            });
+
+            const opts = {
+                from: accounts[0],
+                gas: 4000000,
+            };
+
+            const result = await privateRangeContract.validatePrivateRange(proofData, accounts[0], constants.CRS, opts);
+
+            expect(result).to.equal(expectedOutput);
+        });
+
+        it('validate it works with output notes of zero value', async () => {
+            const noteValues = [0, 0, 0];
+            const aztecAccounts = [...new Array(3)].map(() => secp256k1.generateAccount());
+            const notes = [...aztecAccounts.map(({ publicKey }, i) => note.create(publicKey, noteValues[i]))];
+
+            const originalNote = notes[0];
+            const comparisonNote = notes[1];
+            const utilityNote = notes[2];
+
+            const { proofData, expectedOutput } = privateRange.encodePrivateRangeTransaction({
+                originalNote,
+                comparisonNote,
+                utilityNote,
+                senderAddress: accounts[0],
+            });
+
+            const opts = {
+                from: accounts[0],
+                gas: 4000000,
+            };
+
+            const result = await privateRangeContract.validatePrivateRange(proofData, accounts[0], constants.CRS, opts);
+
+            expect(result).to.equal(expectedOutput);
+        });
+
+        it('validate success when challenge has GROUP_MODULUS added to it', async () => {
+            const noteValues = [10, 4, 6];
+            const aztecAccounts = [...new Array(3)].map(() => secp256k1.generateAccount());
+            const notes = [...aztecAccounts.map(({ publicKey }, i) => note.create(publicKey, noteValues[i]))];
+
+            const originalNote = notes[0];
+            const comparisonNote = notes[1];
+            const utilityNote = notes[2];
+            const senderAddress = accounts[0];
+
+            const { proofData: proofDataRaw, challenge } = privateRange.constructProof(
+                [originalNote, comparisonNote, utilityNote],
+                senderAddress,
+            );
+
+            const challengeBN = new BN(challenge.slice(2), 16);
+            const challengePlusGroupModulus = `0x${challengeBN.add(constants.GROUP_MODULUS).toString(16)}`;
+
+            const inputNotes = [originalNote, comparisonNote];
+            const outputNotes = [utilityNote];
+            const inputOwners = inputNotes.map((m) => m.owner);
+            const outputOwner = [utilityNote.owner];
+
+            const proofData = inputCoder.privateRange(
+                proofDataRaw,
+                challengePlusGroupModulus,
+                inputOwners,
+                outputOwner,
+                outputNotes,
+            );
+
+            const publicOwner = constants.addresses.ZERO_ADDRESS;
+            const publicValue = 0;
+
+            const expectedOutput = `0x${outputCoder
+                .encodeProofOutputs([
+                    {
+                        inputNotes,
+                        outputNotes,
+                        publicOwner,
+                        publicValue,
+                        challenge: challengePlusGroupModulus,
+                    },
+                ])
+                .slice(0x42)}`;
+
+            const opts = {
+                from: accounts[0],
+                gas: 4000000,
+            };
+
+            const result = await privateRangeContract.validatePrivateRange(proofData, accounts[0], constants.CRS, opts);
+            expect(result).to.equal(expectedOutput);
         });
     });
 
-    describe('failure States', () => {
+    describe('Failure States', () => {
         beforeEach(async () => {
             privateRangeContract = await PrivateRange.new({
                 from: accounts[0],
@@ -203,6 +312,198 @@ contract('PrivateRange', (accounts) => {
 
             const proofData = inputCoder.privateRange(scalarZeroProofData, challenge, inputOwners, outputOwner, outputNotes);
 
+            const opts = {
+                from: accounts[0],
+                gas: 4000000,
+            };
+
+            await truffleAssert.reverts(privateRangeContract.validatePrivateRange(proofData, accounts[0], constants.CRS, opts));
+        });
+
+        it('validate failure if scalars are NOT mod(GROUP_MODULUS)', async () => {
+            const noteValues = [10, 4, 6];
+            const aztecAccounts = [...new Array(3)].map(() => secp256k1.generateAccount());
+            const notes = [...aztecAccounts.map(({ publicKey }, i) => note.create(publicKey, noteValues[i]))];
+
+            const originalNote = notes[0];
+            const comparisonNote = notes[1];
+            const utilityNote = notes[2];
+            const senderAddress = accounts[0];
+
+            const { proofData: proofDataRaw, challenge } = privateRange.constructProof(
+                [originalNote, comparisonNote, utilityNote],
+                senderAddress,
+            );
+
+            const inputNotes = [originalNote, comparisonNote];
+            const outputNotes = [utilityNote];
+            const inputOwners = inputNotes.map((m) => m.owner);
+            const outputOwner = [utilityNote.owner];
+
+            // Generate scalars that NOT mod r
+            const kBarBN = new BN(proofDataRaw[0][0].slice(2), 16);
+            const notModRKBar = `0x${kBarBN.add(constants.GROUP_MODULUS).toString(16)}`;
+
+            proofDataRaw[0][0] = notModRKBar;
+
+            const proofData = inputCoder.privateRange(proofDataRaw, challenge, inputOwners, outputOwner, outputNotes);
+
+            const opts = {
+                from: accounts[0],
+                gas: 4000000,
+            };
+
+            await truffleAssert.reverts(privateRangeContract.validatePrivateRange(proofData, accounts[0], constants.CRS, opts));
+        });
+
+        it('should fail if group element (blinding factor) resolves to the point at infinity', async () => {
+            const noteValues = [10, 4, 6];
+            const aztecAccounts = [...new Array(3)].map(() => secp256k1.generateAccount());
+            const notes = [...aztecAccounts.map(({ publicKey }, i) => note.create(publicKey, noteValues[i]))];
+
+            const originalNote = notes[0];
+            const comparisonNote = notes[1];
+            const utilityNote = notes[2];
+            const senderAddress = accounts[0];
+
+            const { proofData: proofDataRaw } = privateRange.constructProof(
+                [originalNote, comparisonNote, utilityNote],
+                senderAddress,
+            );
+
+            proofDataRaw[0][0] = `0x${padLeft('05', 64)}`;
+            proofDataRaw[0][1] = `0x${padLeft('05', 64)}`;
+            proofDataRaw[0][2] = `0x${padLeft(bn128.h.x.fromRed().toString(16), 64)}`;
+            proofDataRaw[0][3] = `0x${padLeft(bn128.h.y.fromRed().toString(16), 64)}`;
+            proofDataRaw[0][4] = `0x${padLeft(bn128.h.x.fromRed().toString(16), 64)}`;
+            proofDataRaw[0][5] = `0x${padLeft(bn128.h.y.fromRed().toString(16), 64)}`;
+            const challenge = `0x${padLeft('0a', 64)}`;
+
+            const inputNotes = [originalNote, comparisonNote];
+            const outputNotes = [utilityNote];
+            const inputOwners = inputNotes.map((m) => m.owner);
+            const outputOwner = [utilityNote.owner];
+
+            // Generate scalars that NOT mod r
+            const kBarBN = new BN(proofDataRaw[0][0].slice(2), 16);
+            const notModRKBar = `0x${kBarBN.add(constants.GROUP_MODULUS).toString(16)}`;
+
+            proofDataRaw[0][0] = notModRKBar;
+
+            const proofData = inputCoder.privateRange(proofDataRaw, challenge, inputOwners, outputOwner, outputNotes);
+
+            const opts = {
+                from: accounts[0],
+                gas: 4000000,
+            };
+
+            await truffleAssert.reverts(privateRangeContract.validatePrivateRange(proofData, accounts[0], constants.CRS, opts));
+        });
+
+        it('should fail if proofData NOT correctly encoded', async () => {
+            const noteValues = [10, 4, 6];
+            const aztecAccounts = [...new Array(3)].map(() => secp256k1.generateAccount());
+            const notes = [...aztecAccounts.map(({ publicKey }, i) => note.create(publicKey, noteValues[i]))];
+
+            const originalNote = notes[0];
+            const comparisonNote = notes[1];
+            const utilityNote = notes[2];
+            const senderAddress = accounts[0];
+
+            const { proofData: proofDataRaw, challenge } = privateRange.constructProof(
+                [originalNote, comparisonNote, utilityNote],
+                senderAddress,
+            );
+
+            const inputNotes = [originalNote, comparisonNote];
+            const outputNotes = [utilityNote];
+            const inputOwners = inputNotes.map((m) => m.owner);
+            const outputOwner = [utilityNote.owner];
+
+            const metadata = outputNotes;
+
+            const { length } = proofDataRaw;
+            const noteString = proofDataRaw.map((individualNotes) => encoderFactory.encodeNote(individualNotes));
+            // Incorrect encoding of proof data happens here: first two characters incorrectly sliced off
+            // noteString, and padLeft() increases from 64 to 66 to still recognise it as a valid bytes
+            // object. However. this is incorrect ABI encoding so will throw
+            const data = [padLeft(Number(length).toString(16), 66), ...noteString.slice(2)].join('');
+            const actualLength = Number(data.length / 2);
+
+            const configs = {
+                CHALLENGE: challenge.slice(2),
+                PROOF_DATA: { data, length: actualLength },
+                INPUT_OWNERS: encoderFactory.encodeInputOwners(inputOwners),
+                OUTPUT_OWNERS: encoderFactory.encodeOutputOwners(outputOwner),
+                METADATA: encoderFactory.encodeMetadata(metadata),
+            };
+
+            const abiParams = ['PROOF_DATA', 'INPUT_OWNERS', 'OUTPUT_OWNERS', 'METADATA'];
+
+            const incorrectEncoding = encoderFactory.encode(configs, abiParams, 'privateRange');
+
+            const opts = {
+                from: accounts[0],
+                gas: 4000000,
+            };
+
+            await truffleAssert.reverts(
+                privateRangeContract.validatePrivateRange(incorrectEncoding, accounts[0], constants.CRS, opts),
+            );
+        });
+
+        it('should fail for incorrect H_X, H_Y in CRS', async () => {
+            const noteValues = [10, 4, 6];
+            const aztecAccounts = [...new Array(3)].map(() => secp256k1.generateAccount());
+            const notes = [...aztecAccounts.map(({ publicKey }, i) => note.create(publicKey, noteValues[i]))];
+
+            const originalNote = notes[0];
+            const comparisonNote = notes[1];
+            const utilityNote = notes[2];
+
+            const { proofData } = privateRange.encodePrivateRangeTransaction({
+                originalNote,
+                comparisonNote,
+                utilityNote,
+                senderAddress: accounts[0],
+            });
+
+            const H_X = new BN('7673901602397024137095011250362199966051872585513276903826533215767972925880', 10);
+            const H_Y = new BN('8489654445897228341090914135473290831551238522473825886865492707826370766375', 10);
+            const t2 = [
+                `0x${padLeft('1cf7cc93bfbf7b2c5f04a3bc9cb8b72bbcf2defcabdceb09860c493bdf1588d', 64)}`,
+                `0x${padLeft('8d554bf59102bbb961ba81107ec71785ef9ce6638e5332b6c1a58b87447d181', 64)}`,
+                `0x${padLeft('204e5d81d86c561f9344ad5f122a625f259996b065b80cbbe74a9ad97b6d7cc2', 64)}`,
+                `0x${padLeft('2cb2a424885c9e412b94c40905b359e3043275cd29f5b557f008cd0a3e0c0dc', 64)}`,
+            ];
+
+            const fakeHx = H_X.add(new BN(1, 10));
+            const fakeHy = H_Y.add(new BN(1, 10));
+
+            const fakeCRS = [`0x${padLeft(fakeHx.toString(16), 64)}`, `0x${padLeft(fakeHy.toString(16), 64)}`, ...t2];
+
+            const opts = {
+                from: accounts[0],
+                gas: 4000000,
+            };
+
+            await truffleAssert.reverts(privateRangeContract.validatePrivateRange(proofData, accounts[0], fakeCRS, opts));
+        });
+
+        it('should fail for no notes', async () => {
+            // TODO
+            const originalNote = [];
+            const comparisonNote = [];
+            const utilityNote = [];
+
+            const { proofData } = privateRange.encodePrivateRangeTransaction({
+                originalNote,
+                comparisonNote,
+                utilityNote,
+                senderAddress: accounts[0],
+            });
+
+            console.log({ proofData });
             const opts = {
                 from: accounts[0],
                 gas: 4000000,
