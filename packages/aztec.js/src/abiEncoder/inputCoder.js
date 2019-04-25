5c28e115d79140c57f57e225aabc55ddf9501a31
thomas-waite <thomas.waite4@gmail.com>
2019-04-25 13:20:46 +0100

    refactor(aztec.js): call the 'privateRange' encoding rather than 'burn'

--- packages/aztec.js/src/abiEncoder/inputCoder.js
+++ packages/aztec.js/src/abiEncoder/inputCoder.js
@@ -153,8 +153,7 @@ inputCoder.privateRange = (proofData, challenge, inputOwners, outputOwners, meta
     };
 
     const abiParams = ['PROOF_DATA', 'INPUT_OWNERS', 'OUTPUT_OWNERS', 'METADATA'];
-
-    return encoderFactory.encode(configs, abiParams, 'burn');
+    return encoderFactory.encode(configs, abiParams, 'privateRange');
 };
 
 module.exports = inputCoder;
