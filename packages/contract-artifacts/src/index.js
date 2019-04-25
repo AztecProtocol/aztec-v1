0131cdd44f3a3f38cfa24b020f1dd93abcd4f390
Paul Berg <hello@paulrberg.com>
2019-04-25 17:48:04 +0100

    feat(contract-artifacts): make repo isomorphic
    that is, it works both in the browser and in node

--- packages/contract-artifacts/src/index.js
+++ packages/contract-artifacts/src/index.js
@@ -1,4 +1,57 @@
-const requireDir = require('require-dir');
+const ACE = require('../artifacts/ACE');
+const AdjustSupply = require('../artifacts/AdjustSupply');
+const AdjustSupplyABIEncoder = require('../artifacts/AdjustSupplyABIEncoder');
+const AdjustSupplyInterface = require('../artifacts/AdjustSupplyInterface');
+const BilateralSwap = require('../artifacts/BilateralSwap');
+const BilateralSwapABIEncoder = require('../artifacts/BilateralSwapABIEncoder');
+const BilateralSwapInterface = require('../artifacts/BilateralSwapInterface');
+const DividendComputation = require('../artifacts/DividendComputation');
+const DividendComputationABIEncoder = require('../artifacts/DividendComputationABIEncoder');
+const DividendComputationInterface = require('../artifacts/DividendComputationInterface');
+const ERC20Mintable = require('../artifacts/ERC20Mintable');
+const GenericABIEncoder = require('../artifacts/GenericABIEncoder');
+const IAZTEC = require('../artifacts/IAZTEC');
+const IZkAsset = require('../artifacts/IZkAsset');
+const JoinSplit = require('../artifacts/JoinSplit');
+const JoinSplitABIEncoder = require('../artifacts/JoinSplitABIEncoder');
+const JoinSplitInterface = require('../artifacts/JoinSplitInterface');
+const LibEIP712 = require('../artifacts/LibEIP712');
+const NoteRegistry = require('../artifacts/NoteRegistry');
+const NoteUtils = require('../artifacts/NoteUtils');
+const ProofUtils = require('../artifacts/ProofUtils');
+const SafeMath8 = require('../artifacts/SafeMath8');
+const ZkAsset = require('../artifacts/ZkAsset');
+const ZkAssetBurnable = require('../artifacts/ZkAssetBurnable');
+const ZkAssetDetailed = require('../artifacts/ZkAssetDetailed');
+const ZkAssetMintable = require('../artifacts/ZkAssetMintable');
+const ZkAssetOwnable = require('../artifacts/ZkAssetOwnable');
 
-const artifacts = requireDir('../artifacts');
-module.exports = artifacts;
+module.exports = {
+    ACE,
+    AdjustSupply,
+    AdjustSupplyABIEncoder,
+    AdjustSupplyInterface,
+    BilateralSwap,
+    BilateralSwapABIEncoder,
+    BilateralSwapInterface,
+    DividendComputation,
+    DividendComputationABIEncoder,
+    DividendComputationInterface,
+    ERC20Mintable,
+    GenericABIEncoder,
+    IAZTEC,
+    IZkAsset,
+    JoinSplit,
+    JoinSplitABIEncoder,
+    JoinSplitInterface,
+    LibEIP712,
+    NoteRegistry,
+    NoteUtils,
+    ProofUtils,
+    SafeMath8,
+    ZkAsset,
+    ZkAssetBurnable,
+    ZkAssetDetailed,
+    ZkAssetMintable,
+    ZkAssetOwnable,
+};
