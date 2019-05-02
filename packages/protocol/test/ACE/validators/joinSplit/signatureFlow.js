391e7c9970ea27203166d2090fbc2443ceceadac
thomas-waite <thomas.waite4@gmail.com>
2019-05-02 17:58:10 +0100

    test(protocol): tests for none and fake signatures

--- packages/protocol/test/ACE/validators/joinSplit/signatureFlow.js
+++ packages/protocol/test/ACE/validators/joinSplit/signatureFlow.js
@@ -18,7 +18,7 @@ const JoinSplitInterface = artifacts.require('./JoinSplitInterface');
 
 JoinSplit.abi = JoinSplitInterface.abi;
 
-contract.only('JoinSplit signature flow tests', (accounts) => {
+contract('JoinSplit signature flow tests', (accounts) => {
     // Tests to confirm no signature validation is performed in the JoinSplit.sol
     // validator
     let joinSplitContract;
@@ -83,6 +83,112 @@ contract.only('JoinSplit signature flow tests', (accounts) => {
             expect(decoded[0].publicOwner).to.equal(publicOwner.toLowerCase());
             expect(decoded[0].publicValue).to.equal(kPublic);
             expect(result).to.equal(expectedOutput);
+            expect(result).to.equal(expectedOutput);
+        });
+
+        it('succeeds for no inputNoteOwners (i.e. no signatures)', async () => {
+            const aztecAccounts = [...new Array(10)].map(() => secp256k1.generateAccount());
+            const { publicKey } = aztecAccounts[0];
+            const { address: address2 } = aztecAccounts[1];
+
+            const note1Value = 20;
+            const note2Value = 50;
+            const note3Value = 30;
+
+            const note1 = await note.create(publicKey, note1Value, address2);
+            const note2 = await note.create(publicKey, note2Value, address2);
+            const note3 = await note.create(publicKey, note3Value);
+
+            const kPublic = 0;
+
+            const randomAddress = proofUtils.randomAddress();
+            const { proofData, expectedOutput } = joinSplit.encodeJoinSplitTransaction({
+                inputNotes: [note2],
+                outputNotes: [note3, note1],
+                senderAddress: accounts[0],
+                inputNoteOwners: [], // incorrect account
+                publicOwner: accounts[3],
+                kPublic,
+                validatorAddress: randomAddress,
+            });
+
+            const opts = {
+                from: accounts[0],
+                gas: 4000000,
+            };
+
+            const result = await joinSplitContract.validateJoinSplit(proofData, accounts[0], constants.CRS, opts);
+            expect(result.signatures).to.equal(undefined);
+            expect(result).to.equal(expectedOutput);
+        });
+
+        it('succeeds for no validatorAddress', async () => {
+            const aztecAccounts = [...new Array(10)].map(() => secp256k1.generateAccount());
+            const { publicKey } = aztecAccounts[0];
+            const { address: address2 } = aztecAccounts[1];
+
+            const note1Value = 20;
+            const note2Value = 50;
+            const note3Value = 30;
+
+            const note1 = await note.create(publicKey, note1Value, address2);
+            const note2 = await note.create(publicKey, note2Value, address2);
+            const note3 = await note.create(publicKey, note3Value);
+
+            const kPublic = 0;
+
+            const { proofData, expectedOutput } = joinSplit.encodeJoinSplitTransaction({
+                inputNotes: [note2],
+                outputNotes: [note3, note1],
+                senderAddress: accounts[0],
+                inputNoteOwners: aztecAccounts.slice(1, 2), // incorrect account
+                publicOwner: accounts[3],
+                kPublic,
+                validatorAddress: [],
+            });
+
+            const opts = {
+                from: accounts[0],
+                gas: 4000000,
+            };
+
+            const result = await joinSplitContract.validateJoinSplit(proofData, accounts[0], constants.CRS, opts);
+            expect(result.signatures).to.equal(undefined);
+            expect(result).to.equal(expectedOutput);
+        });
+
+        it('succeeds for no inputNoteOwners and no validatorAddress', async () => {
+            const aztecAccounts = [...new Array(10)].map(() => secp256k1.generateAccount());
+            const { publicKey } = aztecAccounts[0];
+            const { address: address2 } = aztecAccounts[1];
+
+            const note1Value = 20;
+            const note2Value = 50;
+            const note3Value = 30;
+
+            const note1 = await note.create(publicKey, note1Value, address2);
+            const note2 = await note.create(publicKey, note2Value, address2);
+            const note3 = await note.create(publicKey, note3Value);
+
+            const kPublic = 0;
+
+            const { proofData, expectedOutput } = joinSplit.encodeJoinSplitTransaction({
+                inputNotes: [note2],
+                outputNotes: [note3, note1],
+                senderAddress: accounts[0],
+                inputNoteOwners: [], // incorrect account
+                publicOwner: accounts[3],
+                kPublic,
+                validatorAddress: [],
+            });
+
+            const opts = {
+                from: accounts[0],
+                gas: 4000000,
+            };
+
+            const result = await joinSplitContract.validateJoinSplit(proofData, accounts[0], constants.CRS, opts);
+            expect(result.signatures).to.equal(undefined);
             expect(result).to.equal(expectedOutput);
         });
     });
