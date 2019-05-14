c1f65d91b4817daa9ba5e34a5b51eb97a45957d8
thomas-waite <thomas.waite4@gmail.com>
2019-05-14 13:29:28 +0100

    refactor(aztec.js): split signNote() into two domain dependent functions

--- packages/aztec.js/src/signer/index.js
+++ packages/aztec.js/src/signer/index.js
@@ -8,6 +8,7 @@
 const { constants } = require('@aztec/dev-utils');
 const secp256k1 = require('@aztec/secp256k1');
 const typedData = require('@aztec/typed-data');
+const crypto = require('crypto');
 
 const signer = {};
 
@@ -37,25 +38,50 @@ signer.generateZKAssetDomainParams = (verifyingContract) => {
         verifyingContract,
     };
 };
+
 /**
- * Create an EIP712 ECDSA signature over an AZTEC note
- * @method signNote
+ * Create an EIP712 ECDSA signature over an AZTEC note, for a ZkAsset domain
+ * @method signNoteZkAssetDomain
  * @param {string} verifyingContract address of target contract
  * @param {string} spender address of the note spender
  * @param {string} privateKey the private key of message signer
  * @returns {string[]} ECDSA signature parameters [v, r, s], formatted as 32-byte wide hex-strings
  */
-signer.signNote = (verifyingContract, spender, privateKey) => {
+signer.signNoteZkAssetDomain = (validatorAddress, noteHash, spender, privateKey) => {
+    const domain = signer.generateZKAssetDomainParams(validatorAddress);
+    const schema = constants.eip712.NOTE_SIGNATURE;
+    const status = true;
+    const message = {
+        noteHash,
+        spender,
+        status,
+    };
+    return signer.signTypedData(domain, schema, message, privateKey);
+};
+
+/**
+ * Create an EIP712 ECDSA signature over an AZTEC note, 
+ * @method signNoteAZTECDomain
+ * @param {string} verifyingContract address of target contract
+ * @param {string} spender address of the note spender
+ * @param {string} privateKey the private key of message signer
+ * @returns {string[]} ECDSA signature parameters [v, r, s], formatted as 32-byte wide hex-strings
+ */
+
+signer.signNoteAZTECDomain = (verifyingContract, spender, privateKey) => {
     const domain = signer.generateAZTECDomainParams(verifyingContract, constants.eip712.ACE_DOMAIN_PARAMS);
     const schema = constants.eip712.NOTE_SIGNATURE;
+    const status = true;
 
     const message = {
         noteHash: `0x${crypto.randomBytes(32).toString('hex')}`,
         spender,
-        status: true,
+        status,
     };
     return signer.signTypedData(domain, schema, message, privateKey);
 };
+
+
 
 /**
  * Create an EIP712 ECDSA signature over structured data
