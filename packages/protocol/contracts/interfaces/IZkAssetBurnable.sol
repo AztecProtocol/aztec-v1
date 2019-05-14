be7678a5a738427e40f60e7ce428c54b6d6ad1cb
thomas-waite <thomas.waite4@gmail.com>
2019-05-14 16:25:04 +0100

    feat(protocol): implement ZkAssetBurnable interface

---
+++ packages/protocol/contracts/interfaces/IZkAssetBurnable.sol
@@ -0,0 +1,24 @@
+
+
+pragma solidity >=0.5.0 <0.6.0;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+import "../ERC1724/ZkAsset.sol";
+import "../ACE/ACE.sol";
+import "../libs/LibEIP712.sol";
+import "../libs/ProofUtils.sol";
+import "../ERC1724/ZkAssetOwnable.sol";
+
+/**
+ * @title ZkAssetBurnableInterface
+ * @author AZTEC
+ * @dev A standard defining the ZkAssetBurnable interfac e
+ * Copyright Spilbury Holdings Ltd 2019. All rights reserved.
+**/
+
+interface IZkAssetBurnable {
+    function confidentialBurn(uint24 _proof, bytes calldata _proofData) external;
+}
+
+
