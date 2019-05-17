9d106620129fedf99e46ed261e33f614b076042d
Paul Berg <hello@paulrberg.com>
2019-05-17 17:57:59 +0100

    refactor(aztec.js): switch to es6 classes for Keccak

--- packages/aztec.js/src/keccak/index.js
+++ packages/aztec.js/src/keccak/index.js
@@ -2,73 +2,62 @@ const { constants } = require('@aztec/dev-utils');
 const BN = require('bn.js');
 const { keccak256, padLeft } = require('web3-utils');
 
-const hashStrings = (inputArr) => {
-    const input = `${inputArr
-        .map((i) => {
-            const res = padLeft(i, 64);
-            return res;
-        })
-        .join('')}`;
-    return keccak256(`0x${input}`).slice(2);
-};
-
 /**
- *
  * @class
- * @classdesc Class to construct keccak256 hashes required by the AZTEC zero-knowledge proof,
+ * @classdesc Class to construct keccak256 hashes required by the AZTEC zero-knowledge proofs
  */
-function Keccak() {
-    /**
-     * array of data being hashed. Each element is a 32-bytes long hex-formatted string
-     * @member {string[]}
-     */
-    this.data = [];
-}
-
-/**
- * Append a BN.js instance {@link Keccak#data}
- *
- * @name Keccak#appendBN
- * @function
- * @param {scalar} scalar BN.js number
- */
-Keccak.prototype.appendBN = function append(scalar) {
-    this.data.push(padLeft(scalar.toString(16), 64));
-};
-
-/**
- * Append an elliptic.js group element to {@link Keccak#data}
- *
- * @name Keccak#append
- * @function
- * @param {Point} point elliptic.js point
- */
-Keccak.prototype.appendPoint = function appendPoint(point) {
-    this.data.push(padLeft(point.x.fromRed().toString(16), 64));
-    this.data.push(padLeft(point.y.fromRed().toString(16), 64));
-};
-
-/**
- * Compute keccak256 hash of {@link Keccak#data}, set {@link Keccak#data} to resulting hash
- *
- * @name Keccak#keccak
- * @function
- * @param {reductionContext} reductionContext BN.js reduction context for Montgomery modular multiplication
- */
-Keccak.prototype.keccak = function keccak(reductionContext = null) {
-    const result = hashStrings(this.data);
-    this.data = [result];
-    if (reductionContext) {
-        return new BN(this.data[0], 16).toRed(reductionContext);
+class Keccak {
+    constructor() {
+        /**
+         * array of data being hashed. Each element is a 32-bytes long hex-formatted string
+         * @member {string[]}
+         */
+        this.data = [];
     }
-    return this.data;
-};
 
-/**
- * Interface for the {@function keccak} with the reduction context set to the constant found in @aztec/dev-utils
- */
-Keccak.prototype.redKeccak = function redKeccak() {
-    return this.keccak(constants.BN128_GROUP_REDUCTION);
+    /**
+     * Append a BN.js instance {@link Keccak#data}
+     *
+     * @name Keccak#appendBN
+     * @function
+     * @param {scalar} scalar BN.js number
+     */
+    appendBN(scalar) {
+        this.data.push(padLeft(scalar.toString(16), 64));
+    }
+
+    /**
+     * Append an elliptic.js group element to {@link Keccak#data}
+     *
+     * @param {Point} point elliptic.js point
+     */
+    appendPoint(point) {
+        this.data.push(padLeft(point.x.fromRed().toString(16), 64));
+        this.data.push(padLeft(point.y.fromRed().toString(16), 64));
+    }
+
+    /**
+     * Compute keccak256 hash of {@link Keccak#data}, set {@link Keccak#data} to resulting hash
+     *
+     * @name Keccak#keccak
+     * @function
+     * @param {reductionContext} reductionContext BN.js reduction context for Montgomery modular multiplication
+     */
+    keccak(reductionContext = null) {
+        const paddedData = this.data.map((i) => padLeft(i, 64)).join('');
+        this.data = [keccak256(`0x${paddedData}`).slice(2)];
+        if (reductionContext) {
+            return new BN(this.data[0], 16).toRed(reductionContext);
+        }
+        return this.data;
+    }
+
+    /**
+     * Interface for the {@function keccak} with the reduction context set to the constant found in @aztec/dev-utils
+     */
+    redKeccak() {
+        return this.keccak(constants.BN128_GROUP_REDUCTION);
+    }
 }
 
 module.exports = Keccak;
