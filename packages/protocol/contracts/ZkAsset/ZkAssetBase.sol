5817c11317efe89831827dd6b2a49a925d80c9e8
Arnaud Schenk <arnaud@creditmint.io>
2019-05-22 15:23:36 +0100

    fix(protocol): make zkAsset templates enforce flags appropriate to type and derive _canConvert implicitly from linked token address

---
+++ packages/protocol/contracts/ZkAsset/ZkAssetBase.sol
@@ -0,0 +1,216 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+import "../ACE/NoteRegistry.sol";
+import "../ACE/ACE.sol";
+import "../interfaces/IAZTEC.sol";
+import "../interfaces/IZkAsset.sol";
+import "../libs/LibEIP712.sol";
+import "../libs/ProofUtils.sol";
+
+/**
+ * @title ZkAssetBase
+ * @author AZTEC
+ * @dev A contract defining the standard interface and behaviours of a confidential asset.
+ * The ownership values and transfer values are encrypted.
+ * Copyright Spilbury Holdings Ltd 2019. All rights reserved.
+ **/
+contract ZkAssetBase is IZkAsset, IAZTEC, LibEIP712 {
+    using NoteUtils for bytes;
+    using SafeMath for uint256;
+
+
+    // EIP712 Domain Name value
+    string constant internal EIP712_DOMAIN_NAME = "ZK_ASSET";
+
+    // EIP712 Domain Version value
+    string constant internal EIP712_DOMAIN_VERSION = "1";
+
+    bytes32 constant internal NOTE_SIGNATURE_TYPEHASH = keccak256(abi.encodePacked(
+        "NoteSignature(",
+            "bytes32 noteHash,",
+            "address spender,",
+            "bool status",
+        ")"
+    ));
+
+    ACE public ace;
+    IERC20 public linkedToken;
+
+    uint256 public scalingFactor;
+    mapping(bytes32 => mapping(address => bool)) public confidentialApproved;
+
+    constructor(
+        address _aceAddress,
+        address _linkedTokenAddress,
+        uint256 _scalingFactor,
+        bool _canAdjustSupply
+    ) public {
+        bool canConvert = (_linkedTokenAddress == address(0x0)) ? false : true;
+        EIP712_DOMAIN_HASH = keccak256(abi.encodePacked(
+            EIP712_DOMAIN_SEPARATOR_SCHEMA_HASH,
+            keccak256(bytes(EIP712_DOMAIN_NAME)),
+            keccak256(bytes(EIP712_DOMAIN_VERSION)),
+            bytes32(uint256(address(this)))
+        ));
+        ace = ACE(_aceAddress);
+        linkedToken = IERC20(_linkedTokenAddress);
+        scalingFactor = _scalingFactor;
+        ace.createNoteRegistry(
+            _linkedTokenAddress,
+            _scalingFactor,
+            _canAdjustSupply,
+            canConvert
+        );
+        emit CreateZkAsset(
+            _aceAddress,
+            _linkedTokenAddress,
+            _scalingFactor,
+            _canAdjustSupply,
+            canConvert
+        );
+    }
+    
+    /**
+    * @dev Executes a basic unilateral, confidential transfer of AZTEC notes
+    * Will submit _proofData to the validateProof() function of the Cryptography Engine.
+    *
+    * Upon successfull verification, it will update note registry state - creating output notes and
+    * destroying input notes.
+    *
+    * @param _proofData - bytes variable outputted from a proof verification contract, representing
+    * transfer instructions for the ACE
+    */
+    function confidentialTransfer(bytes memory _proofData) public {
+        bytes memory proofOutputs = ace.validateProof(JOIN_SPLIT_PROOF, msg.sender, _proofData);
+        confidentialTransferInternal(proofOutputs);
+    }
+
+    /**
+    * @dev Note owner approving a third party, another address, to spend the note on
+    * owner's behalf. This is necessary to allow the confidentialTransferFrom() method
+    * to be called
+    *
+    * @param _noteHash - keccak256 hash of the note coordinates (gamma and sigma)
+    * @param _spender - address being approved to spend the note
+    * @param _status - defines whether the _spender address is being approved to spend the
+    * note, or if permission is being revoked
+    * @param _signature - ECDSA signature from the note owner that validates the
+    * confidentialApprove() instruction
+    */
+    function confidentialApprove(
+        bytes32 _noteHash,
+        address _spender,
+        bool _status,
+        bytes memory _signature
+    ) public {
+        ( uint8 status, , , address noteOwner ) = ace.getNote(address(this), _noteHash);
+        require(status == 1, "only unspent notes can be approved");
+        address signer;
+        if (_signature.length != 0) {
+            // validate EIP712 signature
+            bytes32 hashStruct = keccak256(abi.encode(
+                NOTE_SIGNATURE_TYPEHASH,
+                _noteHash,
+                _spender,
+                status
+            ));
+            bytes32 msgHash = hashEIP712Message(hashStruct);
+            signer = recoverSignature(
+                msgHash,
+                _signature
+            );
+        } else {
+            signer = msg.sender;
+        }
+        require(signer == noteOwner, "the note owner did not sign this message");
+        confidentialApproved[_noteHash][_spender] = _status;
+    }
+
+    /**
+    * @dev Executes a value transfer mediated by smart contracts. The method is supplied with
+    * transfer instructions represented by a bytes _proofOutput argument that was outputted
+    * from a proof verification contract.
+    *
+    * @param _proof - uint24 variable which acts as a unique identifier for the proof which
+    * _proofOutput is being submitted. _proof contains three concatenated uint8 variables:
+    * 1) epoch number 2) category number 3) ID number for the proof
+    * @param _proofOutput - output of a zero-knowledge proof validation contract. Represents
+    * transfer instructions for the ACE
+    */
+    function confidentialTransferFrom(uint24 _proof, bytes memory _proofOutput) public {
+        (bytes memory inputNotes,
+        bytes memory outputNotes,
+        address publicOwner,
+        int256 publicValue) = _proofOutput.extractProofOutput();
+        
+        uint256 length = inputNotes.getLength();
+        for (uint i = 0; i < length; i += 1) {
+            (, bytes32 noteHash, ) = inputNotes.get(i).extractNote();
+            require(
+                confidentialApproved[noteHash][msg.sender] == true,
+                "sender does not have approval to spend input note"
+            );
+        }
+
+        ace.updateNoteRegistry(_proof, _proofOutput, msg.sender);
+
+        logInputNotes(inputNotes);
+        logOutputNotes(outputNotes);
+
+        if (publicValue < 0) {
+            emit ConvertTokens(publicOwner, uint256(-publicValue));
+        }
+        if (publicValue > 0) {
+            emit RedeemTokens(publicOwner, uint256(publicValue));
+        }
+    }
+
+    function confidentialTransferInternal(bytes memory proofOutputs) internal {
+        for (uint i = 0; i < proofOutputs.getLength(); i += 1) {
+            bytes memory proofOutput = proofOutputs.get(i);
+            ace.updateNoteRegistry(JOIN_SPLIT_PROOF, proofOutput, address(this));
+
+            (bytes memory inputNotes,
+            bytes memory outputNotes,
+            address publicOwner,
+            int256 publicValue) = proofOutput.extractProofOutput();
+
+            logInputNotes(inputNotes);
+            logOutputNotes(outputNotes);
+            if (publicValue < 0) {
+                emit ConvertTokens(publicOwner, uint256(-publicValue));
+            }
+            if (publicValue > 0) {
+                emit RedeemTokens(publicOwner, uint256(publicValue));
+            }
+        }
+    }
+
+    /**
+    * @dev Emit events for all input notes, which represent notes being destroyed
+    * and removed from the note registry
+    *
+    * @param inputNotes - input notes being destroyed and removed from note registry state
+    */
+    function logInputNotes(bytes memory inputNotes) internal {
+        for (uint i = 0; i < inputNotes.getLength(); i += 1) {
+            (address noteOwner, bytes32 noteHash, bytes memory metadata) = inputNotes.get(i).extractNote();
+            emit DestroyNote(noteOwner, noteHash, metadata);
+        }
+    }
+
+    /**
+    * @dev Emit events for all output notes, which represent notes being created and added
+    * to the note registry
+    *
+    * @param outputNotes - outputNotes being created and added to note registry state
+    */
+    function logOutputNotes(bytes memory outputNotes) internal {
+        for (uint i = 0; i < outputNotes.getLength(); i += 1) {
+            (address noteOwner, bytes32 noteHash, bytes memory metadata) = outputNotes.get(i).extractNote();
+            emit CreateNote(noteOwner, noteHash, metadata);
+        }
+    }
+}
