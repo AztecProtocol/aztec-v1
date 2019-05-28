2a7fb5747a287e8fa030b8e3066ea7ef615d9439
thomas-waite <thomas.waite4@gmail.com>
2019-05-28 16:00:42 +0100

    feat: add missing proofIDs

--- packages/protocol/contracts/interfaces/IAZTEC.sol
+++ packages/protocol/contracts/interfaces/IAZTEC.sol
@@ -18,6 +18,10 @@ contract IAZTEC {
     // 1 * 256**(2) + 1 * 256**(1) ++ 1 * 256**(0)
     uint24 public constant JOIN_SPLIT_PROOF = 65793;
 
+        // proofEpoch = 1 | proofCategory = 1 | proofId = 1
+    // 1 * 256**(2) + 1 * 256**(1) ++ 1 * 256**(0)
+    uint24 public constant BILATERAL_SWAP_PROOF = 65794;
+
     // proofEpoch = 1 | proofCategory = 2 | proofId = 1
     // (1 * 256**(2)) + (2 * 256**(1)) + (1 * 256**(0))
     uint24 public constant MINT_PROOF = 66049;
@@ -26,14 +30,17 @@ contract IAZTEC {
     // (1 * 256**(2)) + (3 * 256**(1)) + (1 * 256**(0))
     uint24 public constant BURN_PROOF = 66305;
 
-    // proofEpoch = 1 | proofCategory = 4 | proofId = 2
-    // (1 * 256**(2)) + (4 * 256**(1)) + (2 * 256**(0))
-    uint24 public constant PRIVATE_RANGE_PROOF = 66562;
-
     // proofEpoch = 1 | proofCategory = 4 | proofId = 1
     // (1 * 256**(2)) + (4 * 256**(1)) + (2 * 256**(0))
     uint24 public constant DIVIDEND_PROOF = 66561;
 
+    // proofEpoch = 1 | proofCategory = 4 | proofId = 2
+    // (1 * 256**(2)) + (4 * 256**(1)) + (2 * 256**(0))
+    uint24 public constant PRIVATE_RANGE_PROOF = 66562;
+
+    // proofEpoch = 1 | proofCategory = 4 | proofId = 3
+    // (1 * 256**(2)) + (4 * 256**(1)) + (3 * 256**(0))
+    uint24 public constant PUBLIC_RANGE_PROOF = 66563;
 
     // Hash of a dummy AZTEC note with k = 0 and a = 1
     bytes32 public constant ZERO_VALUE_NOTE_HASH = 0xcbc417524e52b95c42a4c42d357938497e3d199eb9b4a0139c92551d4000bc3c;
