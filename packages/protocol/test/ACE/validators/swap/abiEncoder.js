1628fd182e4cce04fdb5ba563af4ab3e52b4d301
Paul Berg <hello@paulrberg.com>
2019-05-28 21:56:59 +0100

    refactor(protocol): use new proof construction in join-split validators

--- packages/protocol/test/ACE/validators/swap/abiEncoder.js
+++ packages/protocol/test/ACE/validators/swap/abiEncoder.js
@@ -8,31 +8,53 @@ const { keccak256, padLeft } = require('web3-utils');
 
 const SwapABIEncoderTest = artifacts.require('./SwapABIEncoderTest');
 
-contract('Swap Validator ABI Encoder', (accounts) => {
+const maker = secp256k1.generateAccount();
+const publicOwner = constants.addresses.ZERO_ADDRESS;
+let swapAbiEncoderTest;
+const taker = secp256k1.generateAccount();
+
+const getNotes = async (asks = [], bids = []) => {
+    const inputNotes = [];
+    if (typeof bids[0] !== 'undefined') {
+        inputNotes.push(await note.create(maker.publicKey, bids[0]));
+    }
+    if (typeof bids[1] !== 'undefined') {
+        inputNotes.push(await note.create(taker.publicKey, bids[1]));
+    }
+    const outputNotes = [];
+    if (typeof asks[0] !== 'undefined') {
+        outputNotes.push(await note.create(maker.publicKey, asks[0]));
+    }
+    if (typeof asks[1] !== 'undefined') {
+        outputNotes.push(await note.create(taker.publicKey, asks[1]));
+    }
+    return { inputNotes, outputNotes };
+};
+
+const getDefaultNotes = async () => {
     const asks = [10, 20];
     const bids = [10, 20];
-    let inputNotes = [];
-    const maker = secp256k1.generateAccount();
-    let outputNotes = [];
-    const publicOwner = constants.addresses.ZERO_ADDRESS;
+    return getNotes(asks, bids);
+};
+
+contract.only('Swap Validator ABI Encoder', (accounts) => {
     const sender = accounts[0];
-    let swapAbiEncoderTest;
-    const taker = secp256k1.generateAccount();
+
+    before(async () => {
+        swapAbiEncoderTest = await SwapABIEncoderTest.new({ from: sender });
+    });
 
     describe('Success States', () => {
-        beforeEach(async () => {
-            // see https://github.com/AztecProtocol/specification#aztec-verifiers-bilateralswapsol
-            inputNotes = await Promise.all([note.create(maker.publicKey, bids[0]), note.create(taker.publicKey, bids[1])]);
-            outputNotes = await Promise.all([note.create(maker.publicKey, asks[0]), note.create(taker.publicKey, asks[1])]);
-            swapAbiEncoderTest = await SwapABIEncoderTest.new({ from: sender });
-        });
-
         it('should encode the output of a Swap proof', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
-            const data = swapProof.encodeABI();
-            const result = await swapAbiEncoderTest.validateSwap(data, sender, constants.CRS, { from: sender });
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            const data = proof.encodeABI();
 
-            const decoded = abiCoder.decoder.decodeProofOutputs(`0x${padLeft('0', 64)}${result.slice(2)}`);
+            const result = await swapAbiEncoderTest.validateSwap(data, sender, constants.CRS, { from: sender });
+            expect(result).to.equal(proof.eth.output);
+            expect(result.length).to.equal(proof.eth.output.length);
+
+            const decoded = abiCoder.outputCoder.decodeProofOutputs(`0x${padLeft('0', 64)}${result.slice(2)}`);
             expect(decoded[0].inputNotes[0].gamma.eq(inputNotes[0].gamma)).to.equal(true);
             expect(decoded[0].inputNotes[0].sigma.eq(inputNotes[0].sigma)).to.equal(true);
             expect(decoded[0].inputNotes[0].noteHash).to.equal(inputNotes[0].noteHash);
@@ -53,27 +75,27 @@ contract('Swap Validator ABI Encoder', (accounts) => {
 
             expect(decoded[0].publicValue).to.equal(0);
             expect(decoded[0].publicOwner).to.equal(publicOwner);
-            expect(decoded[0].challenge).to.equal(swapProof.challengeHex);
+            expect(decoded[0].challenge).to.equal(proof.challengeHex);
             expect(decoded[1].publicValue).to.equal(0);
             expect(decoded[1].publicOwner).to.equal(publicOwner);
-            expect(decoded[1].challenge).to.equal(keccak256(swapProof.challengeHex));
-
-            expect(result).to.equal(swapProof.eth.output);
-            expect(result.length).to.equal(swapProof.eth.output.length);
+            expect(decoded[1].challenge).to.equal(keccak256(proof.challengeHex));
         });
     });
 
     describe('Failure States', () => {
-        it('should revert if number of metadata entries != 2', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender, [outputNotes[0], outputNotes[1], inputNotes[1]]);
-            const data = swapProof.encodeABI();
+        it('should revert if number of output note owners != 4', async () => {
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            // See https://stackoverflow.com/questions/28569962/stubbing-a-get-method-using-sinon
+            sinon.stub(proof, 'outputNoteOwners').value([...proof.outputNoteOwners, ...proof.outputNoteOwners]);
+            const data = proof.encodeABI();
             await truffleAssert.reverts(swapAbiEncoderTest.validateSwap(data, sender, constants.CRS));
         });
 
-        it('should revert if number of output note owners != 4', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
-            sinon.stub(swapProof, 'outputNoteOwners').value([...swapProof.outputNoteOwners, ...swapProof.outputNoteOwners]);
-            const data = swapProof.encodeABI();
+        it('should revert if number of metadata entries != 2', async () => {
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender, [outputNotes[0], outputNotes[1], inputNotes[1]]);
+            const data = proof.encodeABI();
             await truffleAssert.reverts(swapAbiEncoderTest.validateSwap(data, sender, constants.CRS));
         });
     });
--- packages/protocol/test/ACE/validators/swap/index.js
+++ packages/protocol/test/ACE/validators/swap/index.js
@@ -1,4 +1,4 @@
-/* global artifacts, expect, contract, beforeEach, it:true */
+/* global artifacts, expect, contract, it:true */
 const { note, SwapProof } = require('aztec.js');
 const { constants } = require('@aztec/dev-utils');
 const secp256k1 = require('@aztec/secp256k1');
@@ -12,196 +12,211 @@ const Swap = artifacts.require('./Swap');
 const SwapInterface = artifacts.require('./SwapInterface');
 Swap.abi = SwapInterface.abi;
 
-contract.only('Swap Validator', (accounts) => {
+const maker = secp256k1.generateAccount();
+let swapValidator;
+const taker = secp256k1.generateAccount();
+
+const getNotes = async (asks = [], bids = []) => {
+    const inputNotes = [];
+    if (typeof bids[0] !== 'undefined') {
+        inputNotes.push(await note.create(maker.publicKey, bids[0]));
+    }
+    if (typeof bids[1] !== 'undefined') {
+        inputNotes.push(await note.create(taker.publicKey, bids[1]));
+    }
+    const outputNotes = [];
+    if (typeof asks[0] !== 'undefined') {
+        outputNotes.push(await note.create(maker.publicKey, asks[0]));
+    }
+    if (typeof asks[1] !== 'undefined') {
+        outputNotes.push(await note.create(taker.publicKey, asks[1]));
+    }
+    return { inputNotes, outputNotes };
+};
+
+const getDefaultNotes = async () => {
     const asks = [10, 20];
     const bids = [10, 20];
-    let inputNotes = [];
-    const maker = secp256k1.generateAccount();
-    let outputNotes = [];
-    const sender = accounts[0];
-    let swapValidator;
-    const taker = secp256k1.generateAccount();
+    return getNotes(asks, bids);
+};
 
-    beforeEach(async () => {
-        // see https://github.com/AztecProtocol/specification#aztec-verifiers-bilateralswapsol
-        inputNotes = await Promise.all([note.create(maker.publicKey, bids[0]), note.create(taker.publicKey, bids[1])]);
-        outputNotes = await Promise.all([note.create(maker.publicKey, asks[0]), note.create(taker.publicKey, asks[1])]);
+contract.only('Swap Validator', (accounts) => {
+    const sender = accounts[0];
+
+    before(async () => {
         swapValidator = await Swap.new({ from: accounts[0] });
     });
 
     describe('Success States', () => {
         it('should validate Swap proof', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender, [outputNotes[0], inputNotes[1]]);
-            const data = swapProof.encodeABI();
-
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            const data = proof.encodeABI();
             const result = await swapValidator.validateSwap(data, sender, constants.CRS, { from: sender });
-            expect(result).to.equal(swapProof.eth.output);
+            expect(result).to.equal(proof.eth.output);
         });
 
         it('should validate Swap proof that uses notes worth 0', async () => {
-            const testAsks = [0, 20];
-            const testBids = [0, 20];
-            const testInputNotes = await Promise.all([
-                note.create(maker.publicKey, testBids[0]),
-                note.create(taker.publicKey, testBids[1]),
-            ]);
-            const testOutputNotes = await Promise.all([
-                note.create(maker.publicKey, testAsks[0]),
-                note.create(taker.publicKey, testAsks[1]),
-            ]);
-            const swapProof = new SwapProof(testInputNotes, testOutputNotes, sender);
-            const data = swapProof.encodeABI();
+            const asks = [0, 20];
+            const bids = [0, 20];
+            const { inputNotes, outputNotes } = await getNotes(asks, bids);
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            const data = proof.encodeABI();
             const result = await swapValidator.validateSwap(data, sender, constants.CRS, { from: sender });
-            expect(result).to.equal(swapProof.eth.output);
+            expect(result).to.equal(proof.eth.output);
         });
 
-        it('should validate that challenge has GROUP_MODULUS added to it', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
-            swapProof.challenge = swapProof.challenge.add(constants.GROUP_MODULUS);
-            swapProof.constructOutput();
-            const data = swapProof.encodeABI();
+        it('should validate Swap proof with challenge that has GROUP_MODULUS added to it', async () => {
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            proof.challenge = proof.challenge.add(constants.GROUP_MODULUS);
+            proof.constructOutput();
+            const data = proof.encodeABI();
             const result = await swapValidator.validateSwap(data, sender, constants.CRS, { from: sender });
-            expect(result).to.equal(swapProof.eth.output);
+            expect(result).to.equal(proof.eth.output);
         });
     });
 
     describe('Failure States', () => {
         it('should fail if notes do NOT balance (k1 != k3, k2 != k4)', async () => {
-            const testAsks = [10, 50];
-            const testBids = [20, 20];
-            const testInputNotes = await Promise.all([
-                note.create(maker.publicKey, testBids[0]),
-                note.create(taker.publicKey, testBids[1]),
-            ]);
-            const testOutputNotes = await Promise.all([
-                note.create(maker.publicKey, testAsks[0]),
-                note.create(taker.publicKey, testAsks[1]),
-            ]);
-            const swapProof = new SwapProof(testInputNotes, testOutputNotes, sender);
-            const data = swapProof.encodeABI();
+            const asks = [10, 50];
+            const bids = [20, 20];
+            const { inputNotes, outputNotes } = await getNotes(asks, bids);
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            const data = proof.encodeABI();
             await truffleAssert.reverts(swapValidator.validateSwap(data, sender, constants.CRS), truffleAssert.ErrorType.REVERT);
         });
 
         it('should fail if using 2 notes instead of 4', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
-            const testAsks = [20];
-            const testBids = [20];
-            const testInputNotes = await Promise.all([note.create(maker.publicKey, testBids[0])]);
-            const testOutputNotes = await Promise.all([note.create(maker.publicKey, testAsks[0])]);
-            swapProof.inputNotes = testInputNotes;
-            swapProof.outputNotes = testOutputNotes;
-            swapProof.notes = [...testInputNotes, ...testOutputNotes];
-            const data = swapProof.encodeABI();
+            // We have to first use the correct number of input and output notes and later swap them
+            // because otherwise the proof construction fails
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            const asks = [20];
+            const bids = [20];
+            const { inputNotes: testInputNotes, outputNotes: testOutputNotes } = await getNotes(asks, bids);
+            proof.inputNotes = testInputNotes;
+            proof.outputNotes = testOutputNotes;
+            proof.notes = [...testInputNotes, ...testOutputNotes];
+            const data = proof.encodeABI();
             await truffleAssert.reverts(swapValidator.validateSwap(data, sender, constants.CRS), truffleAssert.ErrorType.REVERT);
         });
 
         it('should fail if using 3 notes instead of 4', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
-            const testAsks = [10, 20];
-            const testBids = [20];
-            const testInputNotes = await Promise.all([note.create(maker.publicKey, testBids[0])]);
-            const testOutputNotes = await Promise.all([note.create(maker.publicKey, testAsks[0])]);
-            swapProof.inputNotes = testInputNotes;
-            swapProof.outputNotes = testOutputNotes;
-            swapProof.notes = [...testInputNotes, ...testOutputNotes];
-            const data = swapProof.encodeABI();
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            const asks = [10, 20];
+            const bids = [20];
+            const { inputNotes: testInputNotes, outputNotes: testOutputNotes } = await getNotes(asks, bids);
+            proof.inputNotes = testInputNotes;
+            proof.outputNotes = testOutputNotes;
+            proof.notes = [...inputNotes, ...testOutputNotes];
+            const data = proof.encodeABI();
             await truffleAssert.reverts(swapValidator.validateSwap(data, sender, constants.CRS), truffleAssert.ErrorType.REVERT);
         });
 
         it('should fail if malformed proof data', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
-            swapProof.data = Array(4)
-                .fill()
-                .map(() => {
-                    return Array(6)
-                        .fill()
-                        .map(() => {
-                            return randomHex(32);
-                        });
-                });
-            const data = swapProof.encodeABI();
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            proof.data = [];
+            for (let i = 0; i < 4; i += 1) {
+                proof.data[i] = [];
+                for (let j = 0; j < 6; j += 1) {
+                    proof.data[i][j] = randomHex(32);
+                }
+            }
+            const data = proof.encodeABI();
             await truffleAssert.reverts(swapValidator.validateSwap(data, sender, constants.CRS), truffleAssert.ErrorType.REVERT);
         });
 
-        it('should fail if points NOT on curve', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
+        it('should fail if proof data points NOT on curve', async () => {
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
             const zeroSwapProof = await mockZeroSwapProof();
-            swapProof.data = zeroSwapProof.data;
-            const data = swapProof.encodeABI();
+            proof.data = zeroSwapProof.data;
+            const data = proof.encodeABI();
             await truffleAssert.reverts(swapValidator.validateSwap(data, sender, constants.CRS), truffleAssert.ErrorType.REVERT);
         });
 
-        it('should fail if scalars NOT modulo GROUP_MODULUS', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
-            const kBar = new BN(swapProof.data[0][0].slice(2), 16);
+        it('should fail if proof data scalars NOT modulo GROUP_MODULUS', async () => {
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            const kBar = new BN(proof.data[0][0].slice(2), 16);
             const notModKBar = `0x${kBar.add(constants.GROUP_MODULUS).toString(16)}`;
-            swapProof.data[0][0] = notModKBar;
-            const data = swapProof.encodeABI();
+            proof.data[0][0] = notModKBar;
+            const data = proof.encodeABI();
             await truffleAssert.reverts(swapValidator.validateSwap(data, sender, constants.CRS), truffleAssert.ErrorType.REVERT);
         });
 
-        it('should fail if scalars are zero', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
-            const zeroScalar = padLeft('0', 64);
-            swapProof.data[0][0] = zeroScalar;
-            swapProof.data[0][1] = zeroScalar;
-            swapProof.data[1][0] = zeroScalar;
-            swapProof.data[1][1] = zeroScalar;
-            swapProof.data[2][0] = zeroScalar;
-            swapProof.data[2][1] = zeroScalar;
-            swapProof.data[3][0] = zeroScalar;
-            swapProof.data[3][1] = zeroScalar;
-            const data = swapProof.encodeABI();
+        it('should fail if proof data scalars are zero', async () => {
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            const zeroScalar = padLeft('0x00', 64);
+            proof.data[0][0] = zeroScalar;
+            proof.data[0][1] = zeroScalar;
+            proof.data[1][0] = zeroScalar;
+            proof.data[1][1] = zeroScalar;
+            proof.data[2][0] = zeroScalar;
+            proof.data[2][1] = zeroScalar;
+            proof.data[3][0] = zeroScalar;
+            proof.data[3][1] = zeroScalar;
+            const data = proof.encodeABI();
             await truffleAssert.reverts(swapValidator.validateSwap(data, sender, constants.CRS), truffleAssert.ErrorType.REVERT);
         });
 
         it('should fail if blinding factors resolve to point at infinity', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
-            swapProof.data[0][0] = padLeft('0x05', 64);
-            swapProof.data[0][1] = padLeft('0x05', 64);
-            swapProof.data[0][2] = `0x${constants.H_X.toString(16)}`;
-            swapProof.data[0][3] = `0x${constants.H_Y.toString(16)}`;
-            swapProof.data[0][4] = `0x${constants.H_X.toString(16)}`;
-            swapProof.data[0][5] = `0x${constants.H_Y.toString(16)}`;
-            swapProof.challenge = new BN('0a', 16);
-            const data = swapProof.encodeABI();
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            proof.data[0][0] = padLeft('0x05', 64);
+            proof.data[0][1] = padLeft('0x05', 64);
+            proof.data[0][2] = `0x${constants.H_X.toString(16)}`;
+            proof.data[0][3] = `0x${constants.H_Y.toString(16)}`;
+            proof.data[0][4] = `0x${constants.H_X.toString(16)}`;
+            proof.data[0][5] = `0x${constants.H_Y.toString(16)}`;
+            proof.challenge = new BN('0a', 16);
+            const data = proof.encodeABI();
             await truffleAssert.reverts(swapValidator.validateSwap(data, sender, constants.CRS), truffleAssert.ErrorType.REVERT);
         });
 
         it('should fail if malformed challenge', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
-            swapProof.challenge = new BN(randomHex(32).slice(2), 16);
-            const data = swapProof.encodeABI();
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            proof.challenge = new BN('0');
+            const data = proof.encodeABI();
             await truffleAssert.reverts(swapValidator.validateSwap(data, sender, constants.CRS), truffleAssert.ErrorType.REVERT);
         });
 
         it('should fail if sender NOT integrated into challenge variable', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
-            swapProof.constructChallengeRecurse([[...inputNotes, ...outputNotes], swapProof.blindingFactors]);
-            swapProof.challenge = swapProof.challengeHash.redKeccak();
-            const data = swapProof.encodeABI();
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            proof.constructChallengeRecurse([[...inputNotes, ...outputNotes], proof.blindingFactors]);
+            proof.challenge = proof.challengeHash.redKeccak();
+            const data = proof.encodeABI();
             await truffleAssert.reverts(swapValidator.validateSwap(data, sender, constants.CRS), truffleAssert.ErrorType.REVERT);
         });
 
         it('should fail if notes NOT integrated into challenge variable', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
-            swapProof.constructChallengeRecurse([sender, swapProof.blindingFactors]);
-            swapProof.challenge = swapProof.challengeHash.redKeccak();
-            const data = swapProof.encodeABI();
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            proof.constructChallengeRecurse([sender, proof.blindingFactors]);
+            proof.challenge = proof.challengeHash.redKeccak();
+            const data = proof.encodeABI();
             await truffleAssert.reverts(swapValidator.validateSwap(data, sender, constants.CRS), truffleAssert.ErrorType.REVERT);
         });
 
         it('should fail if blinding factors NOT integrated into challenge variable', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
-            swapProof.constructChallengeRecurse([sender, [...inputNotes, ...outputNotes]]);
-            swapProof.challenge = swapProof.challengeHash.redKeccak();
-            const data = swapProof.encodeABI();
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            proof.constructChallengeRecurse([sender, [...inputNotes, ...outputNotes]]);
+            proof.challenge = proof.challengeHash.redKeccak();
+            const data = proof.encodeABI();
             await truffleAssert.reverts(swapValidator.validateSwap(data, sender, constants.CRS), truffleAssert.ErrorType.REVERT);
         });
 
-        it('should fail if incorrect H_X, H_Y in CRS)', async () => {
-            const swapProof = new SwapProof(inputNotes, outputNotes, sender);
-            const data = swapProof.encodeABI();
+        it('should fail if malformed H_X, H_Y in CRS', async () => {
+            const { inputNotes, outputNotes } = await getDefaultNotes();
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            const data = proof.encodeABI();
             const bogusHx = constants.H_X.add(new BN(1));
             const bogusHy = constants.H_Y.add(new BN(1));
             const bogusCRS = [`0x${bogusHx.toString(16)}`, `0x${bogusHy.toString(16)}`, ...constants.t2];
