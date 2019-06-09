dd7036f01e00266d12704409cc975bae9210a566
Arnaud Schenk <arnaud@creditmint.io>
2019-06-09 23:07:52 +0100

    feat(protocol): add ability to disable proofs without waiting on timelock

--- packages/protocol/contracts/ACE/ACEOwner.sol
+++ packages/protocol/contracts/ACE/ACEOwner.sol
@@ -23,4 +23,28 @@ contract ACEOwner is MultiSigWalletWithTimeLock {
     ) {
         ace = address(new ACE());
     }
+
+    function emergencyExecuteInvalidateProof(uint256 transactionId)
+        public
+        notExecuted(transactionId)
+        fullyConfirmed(transactionId)
+    {
+        Transaction storage txn = transactions[transactionId];
+        bytes memory txData = txn.data;
+
+        // Revert unless method signature in data is signature for invalidateProof(uint24 _proof)
+        assembly {
+            switch eq(shr(224, mload(add(txData, 0x20))), 0xcaaaa5d8)
+            case 0 {
+                revert(0x00, 0x00)
+            }
+        }
+        txn.executed = true;
+        if (external_call(txn.destination, txn.value, txn.data.length, txn.data)) {
+            emit Execution(transactionId);
+        } else {
+            emit ExecutionFailure(transactionId);
+            txn.executed = false;
+        }
+    }
 }
