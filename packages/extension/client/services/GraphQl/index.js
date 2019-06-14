ec7911e54cc2cffdfdef8670c119f4d8e2fc9fa9
Joe Andrews <joe@fuuzik.com>
2019-06-14 15:12:19 +0100

    feat(extension): intial project scafolding, folder structure

---
+++ packages/extension/client/services/GraphQl/index.js
@@ -0,0 +1,25 @@
+import ApolloClient from 'apollo-client';
+import { SchemaLink } from 'apollo-link-schema';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { makeExecutableSchema } from 'graphql-tools';
+import typeDefs from './schema.graphql';
+import resolvers from './resolvers';
+
+
+const initApollo = () => {
+
+    const schema = makeExecutableSchema({
+        typeDefs,
+        resolvers,
+    });
+
+    const client = new ApolloClient({
+        link: new SchemaLink({ schema }),
+        cache: new InMemoryCache({
+            addTypename: false,
+        }),
+    });
+    return client;
+};
+
+export default initApollo;
