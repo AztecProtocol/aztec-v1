e90bae21f6608a9e7a818af63897a724132fd56c
Paul Berg <hello@paulrberg.com>
2019-06-19 18:40:18 +0100

    refactor(dev-utils): move bn128 constants to the new eponymous package
    BREAKING CHANGE: bn128 related constants are no longer in this package

--- packages/dev-utils/src/constants.js
+++ packages/dev-utils/src/constants.js
@@ -1,19 +1,5 @@
 const BN = require('bn.js');
 
-const FIELD_MODULUS = new BN('21888242871839275222246405745257275088696311157297823662689037894645226208583', 10);
-const GROUP_MODULUS = new BN('21888242871839275222246405745257275088548364400416034343698204186575808495617', 10);
-const H_X = new BN('7673901602397024137095011250362199966051872585513276903826533215767972925880', 10);
-const H_Y = new BN('8489654445897228341090914135473290831551238522473825886865492707826370766375', 10);
-const t2 = [
-    '0x01cf7cc93bfbf7b2c5f04a3bc9cb8b72bbcf2defcabdceb09860c493bdf1588d',
-    '0x08d554bf59102bbb961ba81107ec71785ef9ce6638e5332b6c1a58b87447d181',
-    '0x204e5d81d86c561f9344ad5f122a625f259996b065b80cbbe74a9ad97b6d7cc2',
-    '0x02cb2a424885c9e412b94c40905b359e3043275cd29f5b557f008cd0a3e0c0dc',
-];
-
-const BN128_COMPRESSION_MASK = new BN('8000000000000000000000000000000000000000000000000000000000000000', 16);
-const BN128_GROUP_REDUCTION = BN.red(GROUP_MODULUS);
-
 /**
  * Helper module that contains key constants for our zero-knowledge proving system
  *
@@ -21,54 +7,10 @@ const BN128_GROUP_REDUCTION = BN.red(GROUP_MODULUS);
  */
 const constants = {
     /**
-     * BN128 compression mask
-     */
-    BN128_COMPRESSION_MASK,
-    /**
-     * BN.js reduction context for bn128 curve group's prime modulus
-     */
-    BN128_GROUP_REDUCTION,
-    /**
-     * Common reference string
-     */
-    CRS: [`0x${H_X.toString(16)}`, `0x${H_Y.toString(16)}`, ...t2],
-    /**
      * Generic scaling factor that maps between AZTEC note values and ERC20 token balances. When used for DAI,
      * 1 note value = 0.1 DAI
      */
     ERC20_SCALING_FACTOR: new BN('100000000000000000', 10),
-    /** modulus of bn128 curve's finite field (p)
-     *  @constant FIELD_MODULUS
-     *  @type {BN}
-     *  @default
-     *  21888242871839275222246405745257275088696311157297823662689037894645226208583
-     */
-    FIELD_MODULUS,
-    /** modulus of bn128's elliptic curve group (n)
-     *  @constant GROUP_MODULUS
-     *  @type {BN}
-     *  @default
-     *  21888242871839275222246405745257275088548364400416034343698204186575808495617
-     */
-    GROUP_MODULUS,
-    /**
-     * X-Coordinate of AZTEC's second generator point 'h'. Created by taking the keccak256 hash of the ascii string
-     *      'just read the instructions', right-padded to 32 bytes. i.e:
-     *      0x6A75737420726561642074686520696E737472756374696F6E73000000000000. H_X is the result of this hash, modulo
-     *      the elliptic curve group modulus n.
-     *  @constant H_X
-     *  @type {BN}
-     *  @default
-     *  7673901602397024137095011250362199966051872585513276903826533215767972925880
-     */
-    H_X,
-    /** Y-Coordinate of AZTEC's second generator point 'h'. Created from odd-valued root of (H_X^{3} + 3)
-     *  @constant H_Y
-     *  @type {BN}
-     *  @default
-     *  8489654445897228341090914135473290831551238522473825886865492707826370766375
-     */
-    H_Y,
     /** Maximum value that can be held in an AZTEC Note
      *  @constant K_MAX
      *  @type {string}
@@ -97,7 +39,7 @@ const constants = {
      *  @constant t2
      *  @type {BN}
      */
-    t2,
+    // t2,
     /** Number of signatures per file in trusted setup database
      *  @constant SIGNATURES_PER_FILE
      *  @type {number}
@@ -108,10 +50,6 @@ const constants = {
      * BN value equal to 0
      */
     ZERO_BN: new BN(0),
-    /**
-     * BN value equal to 0 in reduction context
-     */
-    ZERO_BN_RED: new BN(0).toRed(BN128_GROUP_REDUCTION),
     /**
      * Hash of a dummy AZTEC note with k = 0 and a = 1
      * @constant ZERO_VALUE_NOTE_HASH
