4478618f4a9eba46a1053dc55571e13fce52bb9d
Leila Wang <leizciw@gmail.com>
2019-06-28 15:27:26 +0100

    feat(extension): add more tests for StorageServiceAsync

--- packages/extension/src/services/StorageServiceAsync/assetStorage.spec.js
+++ packages/extension/src/services/StorageServiceAsync/assetStorage.spec.js
@@ -6,7 +6,7 @@ import assetStorage from './assetStorage';
 
 jest.mock('~utils/storage');
 
-describe('AssetStorage', () => {
+describe('createOrUpdate', () => {
     let set;
 
     beforeEach(() => {
@@ -18,17 +18,51 @@ describe('AssetStorage', () => {
         storage.reset();
     });
 
-    const asset = {
-        id: '123',
-        address: '0xabc',
-    };
+    const assets = [
+        {
+            id: '123',
+            address: '0xabc',
+        },
+        {
+            id: '456',
+            address: '0xdef',
+        },
+    ];
 
-    it('will set asset to storage', async () => {
-        await assetStorage.createOrUpdate(asset);
+    it('set asset to storage', async () => {
+        await assetStorage.createOrUpdate(assets[0]);
         expect(set.callCount).toBe(1);
         expect(set.args[0]).toEqual([{
-            [asset.id]: 'a:0',
+            [assets[0].id]: 'a:0',
             assetCount: 1,
         }]);
+    });
+
+    it('increase count when adding different assets to storage', async () => {
+        await assetStorage.createOrUpdate(assets[0]);
+        expect(set.callCount).toBe(1);
+        expect(set.args[0]).toEqual([{
+            [assets[0].id]: 'a:0',
+            assetCount: 1,
+        }]);
+
+        await assetStorage.createOrUpdate(assets[1]);
+        expect(set.callCount).toBe(2);
+        expect(set.args[1]).toEqual([{
+            [assets[1].id]: 'a:1',
+            assetCount: 2,
+        }]);
+    });
+
+    it('will not call set when adding existing asset to storage', async () => {
+        await assetStorage.createOrUpdate(assets[0]);
+        expect(set.callCount).toBe(1);
+        expect(set.args[0]).toEqual([{
+            [assets[0].id]: 'a:0',
+            assetCount: 1,
+        }]);
+
+        await assetStorage.createOrUpdate(assets[0]);
+        expect(set.callCount).toBe(1);
     });
 });
