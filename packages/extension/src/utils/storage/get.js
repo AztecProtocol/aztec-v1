b93ad0b461eee78785475e3ba75a9af9652f44c7
Leila Wang <leizciw@gmail.com>
2019-07-01 16:49:52 +0100

    feat(extension): replace chrome with standardized browser object

--- packages/extension/src/utils/storage/get.js
+++ packages/extension/src/utils/storage/get.js
@@ -1,16 +1,18 @@
-/* global chrome */
+import browser from 'webextension-polyfill';
 
 export default function get(keys, {
     sync = false,
 } = {}) {
     return new Promise((resolve) => {
-        chrome.storage[sync ? 'sync' : 'local']
-            .get(keys, (values) => {
+        browser.storage[sync ? 'sync' : 'local']
+            .get(keys)
+            .then((values) => {
                 const result = values && typeof keys === 'string'
                     ? values[keys]
                     : values;
 
                 resolve(result);
-            });
+            })
+            .catch(error => console.error(error));
     });
 }
