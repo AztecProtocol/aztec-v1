bb9af20dac8fdc57905f58b3545fe67f0b32770c
Leila Wang <leizciw@gmail.com>
2019-07-03 17:30:59 +0100

    feat(extension): remove less optimised StorageService

--- packages/extension/src/background/tasks/sync.js
+++ packages/extension/src/background/tasks/sync.js
@@ -1,6 +1,40 @@
-import syncNotes from '../helpers/syncNotes';
+/* global chrome */
+import {
+    onIdle,
+} from '~utils/storage';
+import StorageService from '../services/StorageService';
+import fetchNoteFromServer from '../utils/fetchNoteFromServer';
 
-export default async function sync() {
-    // TODO - sync by accounts
-    await syncNotes();
+export default async function sync({
+    notesPerRequest = 15,
+    account = '__account_id_0',
+    lastId = '',
+} = {}) {
+    onIdle(
+        () => console.log('--- database idle ---'),
+        {
+            persisting: true,
+        },
+    );
+
+    const newNotes = await fetchNoteFromServer({
+        numberOfNotes: notesPerRequest,
+        account,
+        lastId,
+    });
+    console.log('syncNotes', newNotes);
+
+    chrome.storage.local.clear();
+    chrome.storage.local.getBytesInUse(null, (bytes) => {
+        console.log('getBytesInUse before optimized async', bytes);
+    });
+
+    const startTimeAsync = Date.now();
+    await Promise.all(newNotes.map(note => StorageService.addNote(note)));
+    console.log(`(Optimized Async) Done in ${Date.now() - startTimeAsync} ms`);
+
+    chrome.storage.local.getBytesInUse(null, (bytes) => {
+        console.log('getBytesInUse after optimized async', bytes);
+    });
+    chrome.storage.local.get(null, data => console.info(data));
 }
