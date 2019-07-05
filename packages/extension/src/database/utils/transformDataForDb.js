f652b85305a4543e27d09c45f2f0c49c583c8e64
Leila Wang <leizciw@gmail.com>
2019-07-05 11:22:25 +0100

    feat(extension): use -0 as undefined value placeholder in storage

--- packages/extension/src/database/utils/transformDataForDb.js
+++ packages/extension/src/database/utils/transformDataForDb.js
@@ -1,3 +1,18 @@
+const undefinedField = -0; // it has less bytes than '' or null or undefined
+
 export default function transformDataForDb(fields, rawData) {
-    return fields.map(field => rawData[field]);
+    const data = [];
+    fields.forEach((field, i) => {
+        if (field in rawData) {
+            data[i] = rawData[field];
+        } else {
+            data[i] = undefinedField;
+        }
+    });
+
+    return data;
 }
+
+export {
+    undefinedField,
+};
