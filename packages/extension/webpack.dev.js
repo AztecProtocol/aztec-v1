fc29572e9ad81b735ad46593a007ebcda17cefaa
Leila Wang <leizciw@gmail.com>
2019-07-09 15:11:57 +0100

    feat(extension): allow to inspect graphQL in background.html

--- packages/extension/webpack.dev.js
+++ packages/extension/webpack.dev.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
     devtool: 'cheap-module-source-map',
     entry: {
         background: './src/background',
+        'graphql-inspector': './src/background/services/GraphQLService/inspector/index.jsx',
         content: './src/content',
         client: './src/client',
     },
@@ -15,13 +17,31 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.js/, // assuming the files are named .js.flow
+                enforce: 'pre',
+                use: ['remove-flow-types-loader'],
+            },
+            {
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: ['babel-loader'],
             },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                ],
+            },
         ],
     },
-    plugins: [],
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
     watchOptions: {
         aggregateTimeout: 300,
         poll: 1000,
