5f2f223d474db4942804f8a54b4b0f4d741fe44b
thomas-waite <thomas.waite4@gmail.com>
2019-07-17 19:09:38 +0100

    feat(aztec.js): implement privateRange proof JS code JS code for proof construction and verification

--- packages/aztec.js/src/proof/privateRange/index.js
+++ packages/aztec.js/src/proof/privateRange/index.js
@@ -1,9 +1,9 @@
+const bn128 = require('@aztec/bn128');
 const { constants, proofs } = require('@aztec/dev-utils');
 const BN = require('bn.js');
 const { AbiCoder } = require('web3-eth-abi');
 const { keccak256, padLeft, randomHex } = require('web3-utils');
 
-const bn128 = require('../../bn128');
 const { inputCoder, outputCoder } = require('../../encoder');
 const { Proof, ProofType } = require('../proof');
 const ProofUtils = require('../utils');
@@ -36,72 +36,43 @@ class PrivateRangeProof extends Proof {
             });
 
         let B;
-        // let reducer = this.rollingHash.redKeccak(); // "x" in the white paper
-        // console.log('Construction');
-        let reducer;
 
-        const baSelection = [new BN(74598, 16).toRed(constants.BN128_GROUP_REDUCTION), new BN(85944, 16).toRed(constants.BN128_GROUP_REDUCTION), new BN(95548, 16).toRed(constants.BN128_GROUP_REDUCTION)];
-        const bkSelection = [new BN(98584, 16).toRed(constants.BN128_GROUP_REDUCTION), new BN(83498, 16).toRed(constants.BN128_GROUP_REDUCTION), new BN(93741, 16).toRed(constants.BN128_GROUP_REDUCTION)];
+        let reducer;
         this.blindingFactors = this.notes.map((note, i) => {
             let { bk } = blindingScalars[i];
             const { ba } = blindingScalars[i];
 
-            // const baSelection = [new BN(74598).toRed(constants.BN128_GROUP_REDUCTION), new BN(85944).toRed(constants.BN128_GROUP_REDUCTION), new BN(95548).toRed(constants.BN128_GROUP_REDUCTION)];
-            // const bkSelection = [new BN(98584).toRed(constants.BN128_GROUP_REDUCTION), new BN(83498).toRed(constants.BN128_GROUP_REDUCTION), new BN(93741).toRed(constants.BN128_GROUP_REDUCTION)];
             if (i === 0) {
-                B = note.gamma.mul(bkSelection[0]).add(bn128.h.mul(baSelection[0]));
-                console.log('i: ', i, 'B.x: ', B.x.toString());
-            };
+                B = note.gamma.mul(bk).add(bn128.h.mul(ba));
+            }
 
             if (i === 1) {
                 reducer = this.rollingHash.redKeccak();
-                const xbk = bkSelection[1].redMul(reducer);
-                const xba = baSelection[1].redMul(reducer);
+                const xbk = bk.redMul(reducer);
+                const xba = ba.redMul(reducer);
                 B = note.gamma.mul(xbk).add(bn128.h.mul(xba));
-                console.log('i: ', i, 'B.x: ', B.x.toString());
-            };
+            }
 
             if (i === 2) {
                 reducer = this.rollingHash.redKeccak();
-                bkSelection[2] = bkSelection[0].redAdd(bkSelection[1]);
-                // bk = (blindingScalars[0].bk).redSub(blindingScalars[1].bk);
-                // console.log('bk: ', bk.fromRed().toString());
-                // console.log('reducer: ', reducer);
+                bk = blindingScalars[0].bk.redSub(blindingScalars[1].bk);
 
-                const xbk = bkSelection[2].redMul(reducer);
-                const xba = baSelection[2].redMul(reducer);
-                // console.log('gamma: ', note.gamma);
+                const xbk = bk.redMul(reducer);
+                const xba = ba.redMul(reducer);
 
                 B = note.gamma.mul(xbk).add(bn128.h.mul(xba));
-                console.log('i: ', i, 'B.x: ', B.x.toString());
-                console.log('i: ', i, 'B.x: ', B.x.fromRed().toString((16), 64));
-                console.log('i: ', i, 'B.y: ', B.y.fromRed().toString((16), 64));
-
             }
             return {
                 B,
-                bk: bkSelection[i], // bkSelection[i],
-                ba: baSelection[i], // baSelection[i],
+                bk,
+                ba,
             };
         });
-        console.log('bk: ', this.blindingFactors[2].bk.fromRed().toString());
-
     }
 
     constructChallenge() {
-        // console.log('this.sender: ', this.sender);
-        // console.log('this.publicValue: ', this.publicValue);
-        // console.log('this.publicOwner: ', this.publicOwner);
-        // console.log('this.notes: ', this.notes);
-        // console.log('this.blindingFactors: ', this.blindingFactors);
-
-        // console.log('intial blinding factors: ', this.blindingFactors);
         this.constructChallengeRecurse([this.sender, this.publicValue, this.publicOwner, this.notes, this.blindingFactors]);
-        // console.log('proof construction challenge hash: ', this.challengeHash);
         this.challenge = this.challengeHash.redKeccak();
-        console.log('original challenge: ', this.challenge.fromRed().toString());
-
-        // console.log('original challenge: ', `0x${padLeft(this.challenge.toString(16), 64)}`);
     }
 
     constructData() {
@@ -114,10 +85,10 @@ class PrivateRangeProof extends Proof {
                     .redMul(this.challenge)
                     .redAdd(bk)
                     .fromRed();
-                // console.log('kBar: ', kBar.toString(), 'i: ', i);
             } else {
                 kBar = new BN(randomHex(32), 16).umod(bn128.curve.n).toString(16);
             }
+
             const aBar = note.a
                 .redMul(this.challenge)
                 .redAdd(ba)
@@ -133,8 +104,6 @@ class PrivateRangeProof extends Proof {
             ];
             return items.map((item) => `0x${padLeft(item.toString(16), 64)}`);
         });
-
-        // console.log('this.data in proof construction: ', this.data);
     }
 
     // TODO: normalise proof output encoding. In some places it's expected to use `encodeProofOutputs`
@@ -151,24 +120,19 @@ class PrivateRangeProof extends Proof {
         this.outputs = outputCoder.encodeProofOutputs([proofOutput]);
         this.hash = outputCoder.hashProofOutput(this.output);
 
-        // need to add this constant to proofs in dev-utils once Paul's done
-        // the 'uint24' needs to become proofs.PRIVATE_RANGE_PROOF = 66562
-        const PRIVATE_RANGE_PROOF = 66562;
         this.validatedProofHash = keccak256(
-            new AbiCoder().encodeParameters(['bytes32', 'uint24', 'address'], [this.hash, PRIVATE_RANGE_PROOF, this.sender]),
+            new AbiCoder().encodeParameters(
+                ['bytes32', 'uint24', 'address'],
+                [this.hash, proofs.PRIVATE_RANGE_PROOF, this.sender],
+            ),
         );
     }
 
     /**
-     * Encode the join-split proof as data for an Ethereum transaction
-     * @param {string} validator Ethereum address of the join-split validator contract
+     * Encode the privateRange proof as data for an Ethereum transaction
      * @returns {Object} proof data and expected output
      */
-    encodeABI(validator) {
-        if (!ProofUtils.isEthereumAddress(validator)) {
-            throw new Error('validator is not an Ethereum address');
-        }
-
+    encodeABI() {
         const encodedParams = [
             inputCoder.encodeProofData(this.data),
             inputCoder.encodeOwners(this.inputNoteOwners),
@@ -176,15 +140,11 @@ class PrivateRangeProof extends Proof {
             inputCoder.encodeMetadata(this.metadata),
         ];
 
-        const length = 3 + encodedParams.length + 1;
+        // First hardcoded value in length calc is num of
+        // elements prepending ...offsets in abiEncodedParams
+        const length = 1 + encodedParams.length + 1;
         const offsets = ProofUtils.getOffsets(length, encodedParams);
-        const abiEncodedParams = [
-            padLeft(this.m.toString(16), 64),
-            this.challengeHex.slice(2),
-            padLeft(this.publicOwner.slice(2), 64),
-            ...offsets,
-            ...encodedParams,
-        ];
+        const abiEncodedParams = [this.challengeHex.slice(2), ...offsets, ...encodedParams];
         return `0x${abiEncodedParams.join('').toLowerCase()}`;
     }
 }
--- packages/aztec.js/src/proof/privateRange/verifier.js
+++ packages/aztec.js/src/proof/privateRange/verifier.js
@@ -1,8 +1,7 @@
 /* eslint-disable prefer-destructuring */
+const bn128 = require('@aztec/bn128');
 const { constants, errors } = require('@aztec/dev-utils');
-const { padLeft } = require('web3-utils');
 
-const bn128 = require('../../bn128');
 const Keccak = require('../../keccak');
 const Verifier = require('../verifier');
 
@@ -10,8 +9,6 @@ const { BN128_GROUP_REDUCTION, K_MAX, ZERO_BN, ZERO_BN_RED } = constants;
 
 class PrivateRangeVerifier extends Verifier {
     verifyProof() {
-
-        // console.log('this.data in verifier: ', this.data);
         const dataLength = this.proof.data.length;
         if (dataLength < 3) {
             this.errors.push(errors.codes.INCORRECT_NOTE_NUMBER);
@@ -33,7 +30,7 @@ class PrivateRangeVerifier extends Verifier {
         let challengeX;
         this.data.forEach((item, i) => {
             const { gamma, sigma } = item;
-            let { kBar, aBar } = item;
+            const { kBar, aBar } = item;
             let B;
 
             if (i === 0) {
@@ -41,7 +38,6 @@ class PrivateRangeVerifier extends Verifier {
                     .mul(kBar)
                     .add(bn128.h.mul(aBar))
                     .add(sigma.mul(this.challenge).neg());
-                console.log('i: ', i, 'B.x: ', B.x.toString());
             }
 
             if (i === 1) {
@@ -54,27 +50,18 @@ class PrivateRangeVerifier extends Verifier {
                     .mul(kBarX)
                     .add(bn128.h.mul(aBarX))
                     .add(sigma.mul(challengeX).neg());
-                console.log('i: ', i, 'B.x: ', B.x.toString());
             }
 
             if (i === 2) {
                 reducer = rollingHash.redKeccak();
-                // console.log('reducer: ', reducer);
-                const kBarX = ((this.data[0]).kBar).redAdd((this.data[1]).kBar).redMul(reducer);
+                const kBarX = this.data[0].kBar.redSub(this.data[1].kBar).redMul(reducer);
                 const aBarX = aBar.redMul(reducer);
-                challengeX = challengeX.redMul(reducer);
-                // console.log('gamma: ', gamma);
-
+                challengeX = this.challenge.redMul(reducer);
 
                 B = gamma
                     .mul(kBarX)
                     .add(bn128.h.mul(aBarX))
                     .add(sigma.mul(challengeX).neg());
-                console.log('i: ', i, 'B.x: ', B.x.toString());
-                console.log('i: ', i, 'B.x: ', B.x.fromRed().toString((16), 64));
-                console.log('i: ', i, 'B.y: ', B.y.fromRed().toString((16), 64));
-
-                // console.log('B: ', B);
             }
 
             if (B.isInfinity()) {
@@ -89,10 +76,6 @@ class PrivateRangeVerifier extends Verifier {
             }
         });
 
-        // console.log('proof verification challenge hash: ', challengeResponse);
-        // console.log('challenge response hash: ', challengeResponse);
-        console.log('recovered challenge: ', challengeResponse.redKeccak().fromRed().toString());
-        console.log('original challenge: ', this.challenge.fromRed().toString());
         if (
             !challengeResponse
                 .redKeccak()
