2878750103e98ada43ea0d813fba2101a0803db9
Leila Wang <leizciw@gmail.com>
2019-07-18 10:56:19 +0100

    feat(extension): update mock server to be consistent with graph node

--- packages/extension/mockServer/utils/filterByWhere.js
+++ packages/extension/mockServer/utils/filterByWhere.js
@@ -8,14 +8,29 @@ export default function filterByWhere(where, prefixes, items) {
             filteredItems = filteredItems.filter(item => toMatch.indexOf(item[prefix]) >= 0);
         }
 
+        let toNotMatch = [];
+        if (where[`${prefix}_not_in`]) {
+            toNotMatch = [...where[`${prefix}_not_in`]];
+        }
+        if (where[`${prefix}_not`]) {
+            toNotMatch.push(where[`${prefix}_not`]);
+        }
+
         const gt = where[`${prefix}_gt`];
         if (gt !== undefined) {
             filteredItems = filteredItems.filter(item => item[prefix] > gt);
         }
-
+        const gte = where[`${prefix}_gte`];
+        if (gte !== undefined) {
+            filteredItems = filteredItems.filter(item => item[prefix] >= gte);
+        }
         const lt = where[`${prefix}_lt`];
         if (lt !== undefined) {
             filteredItems = filteredItems.filter(item => item[prefix] < lt);
+        }
+        const lte = where[`${prefix}_lte`];
+        if (lte !== undefined) {
+            filteredItems = filteredItems.filter(item => item[prefix] <= lte);
         }
     });
 
