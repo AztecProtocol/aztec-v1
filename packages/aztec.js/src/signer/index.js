5935823fe4b43595176f061c8407ba47569ec77b
thomas-waite <thomas.waite4@gmail.com>
2019-07-21 21:15:03 +0100

    refactor(aztec.js): create signNotesForConfidentialTransfer() function break out the joinSplit proof constructSignatures() code into a seperate function in signer module name it signNotesForConfidentialTransfer() to indicate the context in which this note signing function is used rename signer.signNote() method to signer.signNoteForConfidentialApprove() to better indicate the context in which this signing function should be used update docs, to be more clear about intended use case of signing functions BREAKING CHANGE: rename signer.signNote() to signer.signNoteForConfidentialApprove()

--- packages/aztec.js/src/signer/index.js
+++ packages/aztec.js/src/signer/index.js
@@ -5,10 +5,10 @@
  * @module signer
  */
 
-const { constants } = require('@aztec/dev-utils');
+const { constants, proofs } = require('@aztec/dev-utils');
 const secp256k1 = require('@aztec/secp256k1');
 const typedData = require('@aztec/typed-data');
-const { randomHex } = require('web3-utils');
+const { randomHex, padRight } = require('web3-utils');
 
 const signer = {};
 
@@ -40,19 +40,21 @@ signer.generateZKAssetDomainParams = (verifyingContract) => {
 };
 
 /**
- * Create an EIP712 ECDSA signature over an AZTEC note, for a ZkAsset.sol domain
+ * Create an EIP712 ECDSA signature over an AZTEC note, suited for the confidentialApprove() method of a 
+ * ZkAsset. The ZkAsset.confidentialApprove() method must be called when granting note spending permission
+ * to a third party and is required in order for ZkAsset.confidentialTransferFrom() to be successful.
  *
- * This is expected to be the default signNote() function. However for use cases, such as
+ * This is expected to be the most commonly used note signing() function. However for use cases, such as
  * testing, where ACE domain params are required then the signNoteACEDomain() function is
  * available.
- * @method signNote
+ * @method signNoteForConfidentialApprove
  * @param {string} verifyingContract address of target contract
  * @param {string} noteHash noteHash of the note being signed
  * @param {string} spender address of the note spender
  * @param {string} privateKey the private key of message signer
- * @returns {string} ECDSA signature parameters [r, s, v]
+ * @returns {string} ECDSA signature parameters [r, s, v], formatted as 32-byte wide hex-strings
  */
-signer.signNote = (verifyingContract, noteHash, spender, privateKey) => {
+signer.signNoteForConfidentialApprove = (verifyingContract, noteHash, spender, privateKey) => {
     const domain = signer.generateZKAssetDomainParams(verifyingContract);
     const schema = constants.eip712.NOTE_SIGNATURE;
     const status = true;
@@ -62,7 +64,43 @@ signer.signNote = (verifyingContract, noteHash, spender, privateKey) => {
         status,
     };
     const { signature } = signer.signTypedData(domain, schema, message, privateKey);
-    return signature[1] + signature[2].slice(2) + signature[0].slice(-2);
+    const r = signature[1];
+    const s = signature[2].slice(2);
+    const v = signature[0].slice(-2);
+    return r + s + v;
+};
+
+/**
+ * Create an EIP712 ECDSA signature over an AZTEC note, to be used to give permission for
+ * note expenditure during a zkAsset confidentialTransfer() method call
+ *
+ * @method signNoteForConfidentialTransfer
+ * @param {string} verifyingContract address of target contract
+ * @param {string} noteOwnerAccount Ethereum account (privateKey, publicKey and address) of owner of the note
+ * being signed
+ * @param {string} noteHash hash of the note being signed
+ * @param {string} challenge hexadecimal representation of the challenge variable
+ * @param {string} sender address of the transaction sender
+ * @returns {string} ECDSA signature parameters [r, s, v] 
+ */
+signer.signNotesForConfidentialTransfer = (verifyingContract, noteOwnerAccount, noteHash, challenge, sender) => {
+    const domain = signer.generateZKAssetDomainParams(verifyingContract);
+    const schema = constants.eip712.JOIN_SPLIT_SIGNATURE;
+    const message = {
+        proof: proofs.JOIN_SPLIT_PROOF,
+        noteHash,
+        challenge,
+        sender,
+    };
+
+    const { privateKey } = noteOwnerAccount;
+    const { signature } = signer.signTypedData(domain, schema, message, privateKey);
+
+    const r = signature[1].slice(2);
+    const s = signature[2].slice(2);
+    const v = padRight(signature[0].slice(-2), 64);
+
+    return r + s + v;
 };
 
 /**
