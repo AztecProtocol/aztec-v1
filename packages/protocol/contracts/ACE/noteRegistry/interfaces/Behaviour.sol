bfd2b39d1a8ca0c84f65e3410d0ec50d8b2846f9
Arnaud Schenk <arnaud@creditmint.io>
2019-07-23 13:36:05 +0100

    feat(protocol): change directory structure, make storage unstructured

--- packages/protocol/contracts/ACE/noteRegistry/interfaces/Behaviour.sol
+++ packages/protocol/contracts/ACE/noteRegistry/interfaces/Behaviour.sol
@@ -4,8 +4,8 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 import "../../../interfaces/IAZTEC.sol";
-import "../../../libs/NoteUtils.sol";
-import "./Data.sol";
+import "../proxies/BaseAdminUpgradeabilityProxy.sol";
+import "../Manager.sol";
 
 /**
  * @title NoteRegistryBehaviour contract which contains the storage variables that define the set of valid
@@ -15,71 +15,30 @@ import "./Data.sol";
  * Copyright Spilbury Holdings Ltd 2019. All rights reserved.
  **/
 contract NoteRegistryBehaviour is Ownable, IAZTEC {
-    using NoteUtils for bytes;
     using SafeMath for uint256;
 
-    NoteRegistryData public noteRegistryData;
     bool public isActiveBehaviour;
+    bool public initialised;
+    address public dataLocation;
 
-    function transferDataContract(address _newOwner) public;
+    constructor () Ownable() public {
+        isActiveBehaviour = true;
+    }
 
-    /**
-    * @dev Call transferFrom on a linked ERC20 token. Used in cases where the ACE's mint
-    * function is called but the token balance of the note registry in question is
-    * insufficient
-    *
-    * @param _value the value to be transferred
-    */
-    function supplementTokens(uint256 _value) external;
+    function initialise(
+        address _newOwner,
+        address _linkedTokenAddress,
+        uint256 _scalingFactor,
+        bool _canAdjustSupply,
+        bool _canConvert
+    ) public {
+        require(initialised != true, "registry already initialised");
+        _transferOwnership(_newOwner);
 
-    /**
-    * @dev Burn AZTEC notes
-    * TODO
-    */
-    function burn(bytes calldata _proofOutputs) external;
+        dataLocation = msg.sender;
+        initialised = true;
+    }
 
-    /**
-    * @dev Mint AZTEC notes
-    * TODO
-    */
-    function mint(bytes calldata _proofOutputs) external;
-
-    /**
-    * @dev Update the state of the note registry according to transfer instructions issued by a
-    * zero-knowledge proof
-    *
-    * @param _proof - unique identifier for a proof
-    * @param _proofOutput - transfer instructions issued by a zero-knowledge proof
-    * @param _proofSender - address of the entity sending the proof
-    */
-    function updateNoteRegistry(
-        uint24 _proof,
-        bytes memory _proofOutput,
-        address _proofSender
-    ) public;
-
-    /**
-    * @dev This should be called from an asset contract.
-    */
-    function publicApprove(address _publicOwner, bytes32 _proofHash, uint256 _value) public;
-
-    function setConfidentialTotalMinted(bytes32 newTotalNoteHash) internal returns (bytes32);
-
-    function setConfidentialTotalBurned(bytes32 newTotalNoteHash) internal returns (bytes32);
-
-    /**
-     * @dev Returns the registry for a given address.
-     *
-     * @return linkedTokenAddress - public ERC20 token that is linked to the NoteRegistry. This is used to
-     * transfer public value into and out of the system
-     * @return scalingFactor - defines how many ERC20 tokens are represented by one AZTEC note
-     * @return totalSupply - TODO
-     * @return confidentialTotalMinted - keccak256 hash of the note representing the total minted supply
-     * @return confidentialTotalBurned - keccak256 hash of the note representing the total burned supply
-     * @return canConvert - flag set by the owner to decide whether the registry has public to private, and
-     * vice versa, conversion privilege
-     * @return canAdjustSupply - determines whether the registry has minting and burning privileges
-     */
     function getRegistry() public view returns (
         address linkedToken,
         uint256 scalingFactor,
@@ -90,16 +49,24 @@ contract NoteRegistryBehaviour is Ownable, IAZTEC {
         bool canAdjustSupply
     );
 
-    /**
-     * @dev Returns the note for a given address and note hash.
-     *
-     * @param _noteHash - keccak256 hash of the note coordiantes (gamma and sigma)
-     * @return status - status of the note, details whether the note is in a note registry
-     * or has been destroyed
-     * @return createdOn - time the note was created
-     * @return destroyedOn - time the note was destroyed
-     * @return noteOwner - address of the note owner
-     */
+    function supplementTokens(uint256 _value) external;
+
+    function burn(bytes calldata _proofOutputs) external;
+
+    function mint(bytes calldata _proofOutputs) external;
+
+    function updateNoteRegistry(
+        uint24 _proof,
+        bytes memory _proofOutput,
+        address _proofSender
+    ) public;
+
+    function publicApprove(address _publicOwner, bytes32 _proofHash, uint256 _value) public;
+
+    function setConfidentialTotalMinted(bytes32 newTotalNoteHash) internal returns (bytes32);
+
+    function setConfidentialTotalBurned(bytes32 newTotalNoteHash) internal returns (bytes32);
+
     function getNote(bytes32 _noteHash) public view returns (
         uint8 status,
         uint40 createdOn,
@@ -107,19 +74,11 @@ contract NoteRegistryBehaviour is Ownable, IAZTEC {
         address noteOwner
     );
 
-    /**
-     * @dev Removes input notes from the note registry
-     *
-     * @param inputNotes - an array of input notes from a zero-knowledge proof, that are to be
-     * removed and destroyed from a note registry
-     */
     function updateInputNotes(bytes memory inputNotes) internal;
 
-    /**
-     * @dev Adds output notes to the note registry
-     *
-     * @param outputNotes - an array of output notes from a zero-knowledge proof, that are to be
-     * added to the note registry
-     */
     function updateOutputNotes(bytes memory outputNotes) internal;
+
+    function createNote(bytes32 _noteHash, address _noteOwner) internal;
+
+    function deleteNote(bytes32 _noteHash, address _noteOwner) internal;
 }
--- packages/protocol/test/ACE/NoteRegistry/NoteRegistryFactory.js
+++ packages/protocol/test/ACE/NoteRegistry/NoteRegistryFactory.js
@@ -1,14 +1,13 @@
 /* eslint-disable prefer-destructuring */
 /* global artifacts, expect, contract, beforeEach, it, web3:true */
 // ### External Dependencies
-const BN = require('bn.js');
 
 // ### Internal Dependencies
 /* eslint-disable-next-line object-curly-newline */
-const { constants: { addresses }, proofs } = require('@aztec/dev-utils');
+const { proofs } = require('@aztec/dev-utils');
+const truffleAssert = require('truffle-assertions');
 
 // ### Artifacts
-const NoteRegistryData = artifacts.require('./noteRegistry/epochs/201907/Data201907');
 const NoteRegistryBehaviour = artifacts.require('./noteRegistry/epochs/201907/convertible/BehaviourConvert201907');
 const NoteRegistryFactory = artifacts.require('./noteRegistry/epochs/201907/convertible/FactoryConvertible201907');
 
@@ -16,58 +15,34 @@ const NoteRegistryFactory = artifacts.require('./noteRegistry/epochs/201907/conv
 const { BOGUS_PROOF, JOIN_SPLIT_PROOF } = proofs;
 
 contract('NoteRegistryFactory', (accounts) => {
-    const [owner] = accounts;
-    const scalingFactor = new BN(1);
+    const [owner, notOwner] = accounts;
 
     let factoryContract;
 
     beforeEach(async () => {
-        factoryContract = await NoteRegistryFactory.new();
+        factoryContract = await NoteRegistryFactory.new(owner);
     });
 
     describe('Success States', async () => {
-        it('should successfully deploy note registry and assign ownership', async () => {
-            const receipt = await factoryContract.deployNewNoteRegistry(
-                addresses.ZERO_ADDRESS,
-                scalingFactor,
-                false,
-                true,
+        it('should successfully deploy note registry', async () => {
+            const receipt = await factoryContract.deployNewBehaviourInstance(
                 {
                     from: owner,
                 }
             );
             const event = receipt.logs.find(l => l.event === 'NoteRegistryDeployed');
             const behaviourContract = await NoteRegistryBehaviour.at(event.args.behaviourContract);
-            const behaviourContractOwner = await behaviourContract.owner();
-            expect(behaviourContractOwner).to.equal(owner);
-
-            const dataContractAddress = await behaviourContract.noteRegistryData();
-            const dataContract = await NoteRegistryData.at(dataContractAddress);
-            const dataContractOwner = await dataContract.owner();
-            expect(dataContractOwner).to.equal(behaviourContract.address);
+            expect(behaviourContract).to.not.equal(undefined);
         });
+    });
 
-        it('should successfully deploy new behaviour contract and assign ownership', async () => {
-            const receipt = await factoryContract.deployNewNoteRegistry(
-                addresses.ZERO_ADDRESS,
-                scalingFactor,
-                false,
-                true,
+    describe('Failure States', async () => {
+        it('should fail to deploy note registry if not owner', async () => {
+            await truffleAssert.reverts(factoryContract.deployNewBehaviourInstance(
                 {
-                    from: owner,
+                    from: notOwner,
                 }
-            );
-            const event = receipt.logs.find(l => l.event === 'NoteRegistryDeployed');
-            const behaviourContract = await NoteRegistryBehaviour.at(event.args.behaviourContract);
-
-            const dataContractAddress = await behaviourContract.noteRegistryData();
-            const updateReceipt = await factoryContract.deployNewBehaviourInstance(dataContractAddress);
-
-            const newEvent = updateReceipt.logs.find(l => l.event === 'NoteRegistryDeployed');
-            const newBehaviourContract = await NoteRegistryBehaviour.at(newEvent.args.behaviourContract);
-
-            const newBehaviourOwner = await newBehaviourContract.owner();
-            expect(newBehaviourOwner).to.equal(owner);
+            ));
         });
     });
 });
