1272d80083c9c2ef3e4b7156ff62ac52373bf29d
Arnaud Schenk <arnaud@creditmint.io>
2019-07-24 13:56:41 +0100

    fix(protocol): add basic 201908 folder

---
+++ packages/protocol/contracts/ACE/noteRegistry/epochs/201908/mixed/Behaviour.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "../../201907/mixed/Behaviour.sol";
+
+/**
+ * @title NoteRegistryBehaviour contract which contains the storage variables that define the set of valid
+ * AZTEC notes for a particular address
+ * @author AZTEC
+ * @dev TODO
+ * Copyright Spilbury Holdings Ltd 2019. All rights reserved.
+ **/
+contract BehaviourMixed201908 is BehaviourMixed201907 {
+    constructor () BehaviourMixed201907() public {}
+}
