0bfdd42379acda9560efec50da11b9ec1095e616
Arnaud Schenk <arnaud@creditmint.io>
2019-07-24 11:36:25 +0100

    fix(protocol): add migrations properly

---
+++ packages/protocol/contracts/ACE/noteRegistry/interfaces/ProxyAdmin.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.0;
+
+/**
+ * @title BaseAdminUpgradeabilityProxy
+ * @dev This contract combines an upgradeability proxy with an authorization
+ * mechanism for administrative tasks.
+ * All external functions in this contract must be guarded by the
+ * `ifAdmin` modifier. See ethereum/solidity#3864 for a Solidity
+ * feature proposal that would enable this to be done automatically.
+ */
+contract ProxyAdmin {
+  function admin() external returns (address);
+
+  function upgradeTo(address newImplementation) external;
+}
