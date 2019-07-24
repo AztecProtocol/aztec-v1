27e67efe455ac6afec5c9c36f38555709bbdb980
Arnaud Schenk <arnaud@creditmint.io>
2019-07-24 11:36:25 +0100

    fix(protocol): add migrations properly

---
+++ packages/protocol/contracts/ACE/noteRegistry/epochs/201907/adjustable/Factory.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "../Factory.sol";
+import "./Behaviour.sol";
+
+/**
+ * @title NoteRegistryFactory contract which contains the storage variables that define the set of valid
+ * AZTEC notes for a particular address
+ * @author AZTEC
+ * @dev todo
+ **/
+contract FactoryAdjustable201907 is Factory201907 {
+    constructor(address _aceAddress) public Factory201907(_aceAddress) {}
+
+    function deployNewBehaviourInstance() public
+      onlyOwner
+      returns (address)
+    {
+        BehaviourAdjustable201907 behaviourContract = new BehaviourAdjustable201907();
+        emit NoteRegistryDeployed(address(behaviourContract));
+        return address(behaviourContract);
+    }
+}
