4e8fb85c7a7462757269f25512d60b97ce0c8a24
Arnaud Schenk <arnaud@creditmint.io>
2019-07-29 15:01:40 +0100

    style(protocol): fix minor typos, unused variables according to review

--- packages/protocol/migrations/8_deploy_convertibleFactory.js
+++ packages/protocol/migrations/8_deploy_convertibleFactory.js
@@ -6,8 +6,6 @@ const { isUndefined } = require('lodash');
 
 const ACE = artifacts.require('./ACE.sol');
 const ConvertibleFactory = artifacts.require('./noteRegistry/epochs/201907/convertible/FactoryConvertible201907');
-const AdjustableFactory = artifacts.require('./noteRegistry/epochs/201907/adjustable/FactoryAdjustable201907');
-const MixedFactory = artifacts.require('./noteRegistry/epochs/201907/mixed/FactoryMixed201907');
 
 module.exports = (deployer) => {
     if (isUndefined(ACE) || isUndefined(ACE.address)) {
@@ -19,12 +17,8 @@ module.exports = (deployer) => {
     new Promise(() => {
         return deployer.deploy(ConvertibleFactory, ACE.address).then(async ({ address }) => {
             const ace = await ACE.at(ACE.address);
-            // const adjustableFactoryAddress = await AdjustableFactory.new(ACE.address);
-            // const mixedFactoryAddress = await MixedFactory.new(ACE.address);
 
             await ace.setFactory(1 * 256**(2) + 1 * 256**(1) + 1 * 256**(0), address);
-            // await ace.setFactory(1 * 256**(2) + 1 * 256**(1) + 2 * 256**(0), adjustableFactoryAddress);
-            // await ace.setFactory(1 * 256**(2) + 1 * 256**(1) + 3 * 256**(0), mixedFactoryAddress);
         });
     });
 };
