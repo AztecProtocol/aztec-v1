c3082f99a06f0208fea93e3783252bb4e9b3eb0f
Leila Wang <leizciw@gmail.com>
2019-07-29 13:06:52 +0100

    feat(extension): create utils to select random ints in a range

--- packages/extension/src/utils/random/index.js
+++ packages/extension/src/utils/random/index.js
@@ -1,9 +1,11 @@
 import randomInt from './randomInt';
+import randomInts from './randomInts';
 import randomId from './randomId';
 import randomSumArray from './randomSumArray';
 
 export {
     randomInt,
+    randomInts,
     randomId,
     randomSumArray,
 };
