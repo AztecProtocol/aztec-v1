6d479c53d27da6259c46dcc99dfb5a6ac7c53154
Leila Wang <leizciw@gmail.com>
2019-07-30 19:02:15 +0100

    feat(extension): send empty signature for withdrawal

--- packages/extension/src/client/apis/withdraw/approve.js
+++ packages/extension/src/client/apis/withdraw/approve.js
@@ -1,4 +1,3 @@
-import aztec from 'aztec.js';
 import asyncForEach from '~utils/asyncForEach';
 import Web3Service from '~client/services/Web3Service';
 import ContractError from '~client/utils/ContractError';
@@ -12,17 +11,10 @@ export default async function approveWithdraw({
         inputNotesOwner,
     },
 }) {
-    const privateKey = '0xb8a23114e720d45005b608f8741639464a341c32c61920bf341b5cbddae7651d';
     try {
         await asyncForEach(inputNotes, async ({
             noteHash,
         }) => {
-            const signature = aztec.signer.signNote(
-                assetAddress,
-                noteHash,
-                inputNotesOwner.address,
-                privateKey,
-            );
             await Web3Service
                 .useContract('ZkAsset')
                 .at(assetAddress)
@@ -31,7 +23,7 @@ export default async function approveWithdraw({
                     noteHash,
                     inputNotesOwner.address,
                     true,
-                    signature,
+                    '0x',
                 );
         });
     } catch (error) {
