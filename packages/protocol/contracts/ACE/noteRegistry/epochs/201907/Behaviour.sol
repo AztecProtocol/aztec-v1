0675704e277d5f86d3b3d3fa3a282c8a02910559
Arnaud Schenk <arnaud@creditmint.io>
2019-07-30 11:16:50 +0100

    docs(protocol): make docs + interface cleaner and better

--- packages/protocol/contracts/ACE/noteRegistry/epochs/201907/Behaviour.sol
+++ packages/protocol/contracts/ACE/noteRegistry/epochs/201907/Behaviour.sol
@@ -5,10 +5,12 @@ import "../../../../libs/NoteUtils.sol";
 import "../../interfaces/Behaviour.sol";
 
 /**
- * @title NoteRegistryBehaviour contract which contains the storage variables that define the set of valid
- * AZTEC notes for a particular address
+ * @title Behaviour201907
  * @author AZTEC
- * @dev TODO
+ * @dev Details the methods and the storage schema of a note registry. Is an ownable contract, and should always inherrit from the previous
+ * epoch of the behaviour contract. This contract defines the shared methods between all asset types for the 201907 generation (epoch 1).
+ * Methods are documented in interface.
+ *
  * Copyright Spilsbury Holdings Ltd 2019. All rights reserved.
  **/
 contract Behaviour201907 is NoteRegistryBehaviour {
@@ -86,19 +88,6 @@ contract Behaviour201907 is NoteRegistryBehaviour {
         initialised = true;
     }
 
-     /**
-        * @dev Returns the registry for a given address.
-        *
-        * @return linkedTokenAddress - public ERC20 token that is linked to the NoteRegistry. This is used to
-        * transfer public value into and out of the system
-        * @return scalingFactor - defines how many ERC20 tokens are represented by one AZTEC note
-        * @return totalSupply - TODO
-        * @return confidentialTotalMinted - keccak256 hash of the note representing the total minted supply
-        * @return confidentialTotalBurned - keccak256 hash of the note representing the total burned supply
-        * @return canConvert - flag set by the owner to decide whether the registry has public to private, and
-        * vice versa, conversion privilege
-        * @return canAdjustSupply - determines whether the registry has minting and burning privileges
-     */
     function getRegistry() public view returns (
         address linkedToken,
         uint256 scalingFactor,
@@ -118,40 +107,18 @@ contract Behaviour201907 is NoteRegistryBehaviour {
         canAdjustSupply = registry.canAdjustSupply;
     }
 
-    /**
-    * @dev Call transferFrom on a linked ERC20 token. Used in cases where the ACE's mint
-    * function is called but the token balance of the note registry in question is
-    * insufficient
-    *
-    * @param _value the value to be transferred
-    */
-    function supplementTokens(uint256 _value) external onlyOwner {
+    function incrementTotalSupply(uint256 _value) external onlyOwner {
         registry.totalSupply = registry.totalSupply.add(_value);
     }
 
-    /**
-        * @dev Burn AZTEC notes
-        * TODO
-    */
     function burn(bytes calldata /* _proofOutputs */) external onlyOwner {
         require(registry.canAdjustSupply == true, "this asset is not burnable");
     }
 
-    /**
-        * @dev Mint AZTEC notes
-        * TODO
-    */
     function mint(bytes calldata  /* _proofOutputs */) external onlyOwner {
         require(registry.canAdjustSupply == true, "this asset is not mintable");
     }
 
-    /**
-        * @dev Update the state of the note registry according to transfer instructions issued by a
-        * zero-knowledge proof
-        *
-        * @param _proof - unique identifier for a proof
-        * @param _proofOutput - transfer instructions issued by a zero-knowledge proof
-    */
     function updateNoteRegistry(
         uint24 _proof,
         bytes memory _proofOutput
@@ -174,9 +141,6 @@ contract Behaviour201907 is NoteRegistryBehaviour {
         }
     }
 
-    /**
-    * @dev This should be called from an asset contract.
-    */
     function publicApprove(address /* _publicOwner */, bytes32 /* _proofHash */, uint256 /* _value */) public onlyOwner {
         require(registry.canConvert == true, "this asset is not convertible");
     }
@@ -190,16 +154,6 @@ contract Behaviour201907 is NoteRegistryBehaviour {
         require(registry.canAdjustSupply == true, "this asset is not burnable");
     }
 
-    /**
-     * @dev Returns the note for a given address and note hash.
-     *
-     * @param _noteHash - keccak256 hash of the note coordiantes (gamma and sigma)
-     * @return status - status of the note, details whether the note is in a note registry
-     * or has been destroyed
-     * @return createdOn - time the note was created
-     * @return destroyedOn - time the note was destroyed
-     * @return noteOwner - address of the note owner
-     */
     function getNote(bytes32 _noteHash) public view returns (
         uint8 status,
         uint40 createdOn,
@@ -220,12 +174,6 @@ contract Behaviour201907 is NoteRegistryBehaviour {
         }
     }
 
-    /**
-     * @dev Removes input notes from the note registry
-     *
-     * @param inputNotes - an array of input notes from a zero-knowledge proof, that are to be
-     * removed and destroyed from a note registry
-     */
     function updateInputNotes(bytes memory inputNotes) internal {
         uint256 length = inputNotes.getLength();
 
@@ -236,12 +184,6 @@ contract Behaviour201907 is NoteRegistryBehaviour {
         }
     }
 
-    /**
-     * @dev Adds output notes to the note registry
-     *
-     * @param outputNotes - an array of output notes from a zero-knowledge proof, that are to be
-     * added to the note registry
-     */
     function updateOutputNotes(bytes memory outputNotes) internal {
         uint256 length = outputNotes.getLength();
 
