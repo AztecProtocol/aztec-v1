c2ceba42fe9990fe18ed0d2af4cd3c397fed33c3
Leila Wang <leizciw@gmail.com>
2019-07-30 10:53:39 +0100

    feat(extension): allow to set owner when recovering note from viewingKey

--- packages/extension/src/utils/note/fromViewingKey.js
+++ packages/extension/src/utils/note/fromViewingKey.js
@@ -1,5 +1,9 @@
 import aztec from 'aztec.js';
 
-export default async function fromViewingKey(viewingKey) {
-    return aztec.note.fromViewKey(viewingKey);
+export default async function fromViewingKey(viewingKey, owner) {
+    const note = await aztec.note.fromViewKey(viewingKey);
+    if (note && owner) {
+        note.owner = owner;
+    }
+    return note;
 }
