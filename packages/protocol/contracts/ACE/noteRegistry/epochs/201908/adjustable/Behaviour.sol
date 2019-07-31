cf0be99ca363f29511f2288bfdfee27f066413fc
Arnaud Schenk <arnaud@creditmint.io>
2019-07-31 22:11:50 +0530

    docs(protocol): add more basic doc-strings

--- packages/protocol/contracts/ACE/noteRegistry/epochs/201908/adjustable/Behaviour.sol
+++ packages/protocol/contracts/ACE/noteRegistry/epochs/201908/adjustable/Behaviour.sol
@@ -3,10 +3,10 @@ pragma solidity >=0.5.0 <0.6.0;
 import "../../201907/adjustable/Behaviour.sol";
 
 /**
- * @title NoteRegistryBehaviour contract which contains the storage variables that define the set of valid
- * AZTEC notes for a particular address
+ * @title BehaviourAdjustable201908
  * @author AZTEC
- * @dev TODO
+ * @dev This contract extends BehaviourAdjustable201907.
+ *
  * Copyright Spilsbury Holdings Ltd 2019. All rights reserved.
  **/
 contract BehaviourAdjustable201908 is BehaviourAdjustable201907 {
