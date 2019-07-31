19b26f7203a2e2b6857713d350571403a1415ddc
Arnaud Schenk <arnaud@creditmint.io>
2019-07-31 22:09:33 +0530

    docs(protocol): fix copyright notice + add basic docstring

--- packages/protocol/contracts/ACE/noteRegistry/epochs/201908/mixed/Factory.sol
+++ packages/protocol/contracts/ACE/noteRegistry/epochs/201908/mixed/Factory.sol
@@ -4,10 +4,10 @@ import "../../../interfaces/Factory.sol";
 import "./Behaviour.sol";
 
 /**
- * @title NoteRegistryFactory contract which contains the storage variables that define the set of valid
- * AZTEC notes for a particular address
- * @author AZTEC
- * @dev todo
+  * @title FactoryMixed201908
+  * @author AZTEC
+  * @dev Deploys a BehaviourMixed201908
+  * Copyright Spilsbury Holdings Ltd 2019. All rights reserved.
  **/
 contract FactoryMixed201908 is NoteRegistryFactory {
     constructor(address _aceAddress) public NoteRegistryFactory(_aceAddress) {}
