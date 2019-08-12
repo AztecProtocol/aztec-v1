20c019518e055b3640bac3dc8608fe458c949fa2
Arnaud Schenk <arnaud@creditmint.io>
2019-08-12 16:47:06 +0100

    feat(protocol): add ability to mint on initialisation

--- packages/protocol/contracts/ERC1724/base/ZkAssetMintableBase.sol
+++ packages/protocol/contracts/ERC1724/base/ZkAssetMintableBase.sol
@@ -30,9 +30,27 @@ contract ZkAssetMintableBase is ZkAssetOwnableBase {
     * @param _proofData - bytes array of proof data, outputted from a proof construction
     */
     function confidentialMint(uint24 _proof, bytes calldata _proofData) external onlyOwner {
+        _confidentialMint(_proof, _proofData, address(this));
+    }
+
+    /**
+    * @dev Internal function executing a minting procedure. Takes in a _proofSender argument.
+    *   If called from the constructor, that _proofSender will be the owner of the zkAsset, otherwise
+    *   will be address(this)
+    *
+    * @param _proof - uint24 variable which acts as a unique identifier for the proof which
+    * _proofOutput is being submitted. _proof contains three concatenated uint8 variables:
+    * 1) epoch number 2) category number 3) ID number for the proof
+    * @param _proofData - bytes array of proof data, outputted from a proof construction
+    * @param _proofSender - address of the proof sender
+    */
+    function _confidentialMint(uint24 _proof, bytes memory _proofData, address _proofSender)
+        internal
+        onlyOwner
+    {
         require(_proofData.length != 0, "proof invalid");
 
-        (bytes memory _proofOutputs) = ace.mint(_proof, _proofData, address(this));
+        (bytes memory _proofOutputs) = ace.mint(_proof, _proofData, _proofSender);
 
         (, bytes memory newTotal, ,) = _proofOutputs.get(0).extractProofOutput();
 
@@ -45,6 +63,7 @@ contract ZkAssetMintableBase is ZkAssetOwnableBase {
         logOutputNotes(mintedNotes);
         emit UpdateTotalMinted(noteHash, metadata);
     }
+
     /**
     * @dev Executes a basic unilateral, confidential transfer of AZTEC notes adapted for use with
     * a mintable ZkAsset.
