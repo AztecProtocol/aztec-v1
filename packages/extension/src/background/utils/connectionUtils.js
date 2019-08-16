f0eb976d6bb97088d06a4ffe6bd45c62ca20a0bf
Joe Andrews <joe@fuuzik.com>
2019-08-16 16:02:42 +0100

    feat(extension): refactor rxjs so we can await metamask

---
+++ packages/extension/src/background/utils/connectionUtils.js
@@ -0,0 +1,112 @@
+import browser from 'webextension-polyfill';
+import psl from 'psl';
+import gql from 'graphql-tag';
+import insertVariablesToGql from '~utils/insertVariablesToGql';
+import actionModel from '~database/models/action';
+import GraphQLService from '../services/GraphQLService';
+import {
+    errorToActionMap,
+} from '~config/action';
+
+export const updateActionState = async (action) => {
+    await actionModel.set(action);
+    return action;
+};
+export const openPopup = ({ timestamp }) => {
+    const popupURL = browser.extension.getURL('pages/popup.html');
+    const { width, height } = window.screen;
+    browser.windows.create({
+        url: `${popupURL}?id=${timestamp}`,
+        width: 340, // approximate golden ratio
+        top: (height - 550) / 2,
+        left: (width - 340) / 2,
+        height: 550,
+        type: 'popup',
+        focused: true,
+    });
+};
+
+export const addDomainData = ({ data, sender }) => {
+    const {
+        url,
+    } = sender;
+    let domain;
+    if (url.match(/^https?:\/\/(127.0.0.1|localhost)(:[0-9+]|\/)/)) {
+        domain = 'localhost';
+    } else {
+        ({
+            domain,
+        } = psl.parse(url.replace(/^https?:\/\//, '').split('/')[0]));
+    }
+
+    return {
+        domain,
+        ...data,
+    };
+};
+
+export const handleQuery = async ({
+    mutation,
+    query,
+    domain,
+    variables,
+    requestId,
+}) => {
+    let type = 'query';
+
+    if (mutation) {
+        type = 'mutation';
+    }
+    // we always want the domain to be from the sender so the user can't fake this
+    const graphQuery = insertVariablesToGql(
+        mutation || query,
+        {
+            domain,
+        },
+    );
+
+    const response = await GraphQLService[type]({
+        [type]: gql(`${type} {${graphQuery}}`),
+        variables,
+    });
+
+    return {
+        response,
+        requestId,
+    };
+};
+
+export const generateResponseCode = ({ response }, uiError, timeout) => {
+    const queryName = Object.keys(response)
+        .find(name => !!response[name].error);
+    const errorData = queryName ? response[queryName].error : false;
+    if (response && !errorData) {
+        return 200;
+    }
+    if (errorData && errorToActionMap[errorData.key]) {
+        // this is an auth error so need to show UI
+        return 401;
+    }
+
+    if (uiError) {
+        return 403;
+    }
+    if (timeout) {
+        return 408;
+    }
+    return 500;
+};
+
+export const generateResponseError = ({ response, requestId }, uiError, timeout) => {
+    const queryName = Object.keys(response)
+        .find(name => !!response[name].error);
+    const errorData = queryName ? response[queryName].error : false;
+    return {
+        type: errorToActionMap[errorData.key],
+        timestamp: Date.now(),
+        data: {
+            requestId,
+            response: JSON.parse(errorData.response),
+        },
+    };
+};
--- packages/extension/src/client/apis/swap/prove.js
+++ packages/extension/src/client/apis/swap/prove.js
@@ -1,32 +1,16 @@
 import aztec from 'aztec.js';
-import {
-    createNotes,
-} from '~utils/note';
-import {
-    randomSumArray,
-} from '~utils/random';
-import address from '~utils/address';
 import ApiError from '~client/utils/ApiError';
 import validateAccount from '../utils/validateAccount';
 
 export default async function proveSwap({
-    makerBid,
-    takerBid,
-    takerAsk: {
-        value: takerAskValue,
-        noteAccess: takerAskNoteAccess,
-        owner: takerAskOwner,
-    },
-    makerAsk: {
-        value: makerAskValue,
-        noteAccess: makerAskNoteAccess,
-        owner: makerAskOwner,
+    swap: {
+        makerBid,
+        takerBid,
+        takerAsk,
+        makerAsk,
     },
     sender,
 }) {
-    // in the swap proof we have two existing notes that are getting destroyed in return for the creation of two
-    // output notes
-    // The outputNotes should be created here so we are sure they are created correctly.
     const {
         SwapProof,
         ProofUtils,
@@ -34,19 +18,12 @@ export default async function proveSwap({
         note,
     } = aztec;
 
-    const taker = await validateAccount(takerAskOwner, true);
-    const maker = await validateAccount(makerAskOwner, true);
+    const taker = await validateAccount(takerBid.owner, true);
+    const maker = await validateAccount(takerAsk.owner, true);
 
-    const makerBidNote = await window.aztec.note(makerBid);
-    const makerAskNote = await note.create(maker.spendingPublicKey, makerAskValue, makerAskOwner, maker.linkedPublicKey);
-    const takerBidNote = await window.aztec.note(takerBid);
-    const takerAskNote = await note.create(taker.spendingPublicKey, takerAskValue, takerAskOwner, maker.linkedPublicKe);
+    const inputNotes = [makerBid, takerBid];
+    const outputNotes = [makerAsk, takerAsk];
 
-
-    const inputNotes = [makerBidNote, takerBidNote];
-    const outputNotes = [makerAskNote, takerAskNote];
-
-    const metadata = metaData.extractNoteMetadata(notes);
     const proof = new SwapProof(
         inputNotes,
         outputNotes,
