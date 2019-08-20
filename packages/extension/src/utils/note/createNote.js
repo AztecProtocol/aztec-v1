2c4ebcad4f01143f401c692e40bd12ca6cd80652
Leila Wang <leizciw@gmail.com>
2019-08-20 12:08:53 +0100

    fix(extension): set metadata for new note only when publicKey exists

--- packages/extension/src/utils/note/createNote.js
+++ packages/extension/src/utils/note/createNote.js
@@ -6,11 +6,15 @@ import {
 
 export default async function createNote(value, publicKey, owner, linkedPublicKey) {
     const note = await aztec.note.create(publicKey, value, owner);
-    const viewingKey = encryptedViewingKey(linkedPublicKey, note.getView());
-    const newMetaData = addAccess('', {
-        address: owner,
-        viewingKey: viewingKey.toHexString(),
-    });
-    note.setMetaData(newMetaData);
+
+    if (linkedPublicKey) {
+        const viewingKey = encryptedViewingKey(linkedPublicKey, note.getView());
+        const newMetaData = addAccess('', {
+            address: owner,
+            viewingKey: viewingKey.toHexString(),
+        });
+        note.setMetaData(newMetaData);
+    }
+
     return note;
 }
