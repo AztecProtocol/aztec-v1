bca7e4a237623af75d128d623448babf9564fc18
Leila Wang <leizciw@gmail.com>
2019-08-20 12:11:31 +0100

    refactor(extension): adjust error response the add helper to test it

---
+++ packages/extension/tests/helpers/expectErrorResponse.js
@@ -0,0 +1,20 @@
+const triggerCallback = async (cb) => {
+    let response;
+    try {
+        response = await cb();
+    } catch (error) {
+        response = error;
+    }
+
+    return response;
+};
+
+export default function expectErrorResponse(cb) {
+    return {
+        toBe: async (key) => {
+            const response = await triggerCallback(cb);
+            expect(Object.keys(response)).toEqual(['error']);
+            expect(response.error.key).toBe(key);
+        },
+    };
+}
