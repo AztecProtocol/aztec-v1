b49f3eeb0e8f3da9a03134cdbb73d63cc61b6e58
Leila Wang <leizciw@gmail.com>
2019-08-20 13:15:31 +0100

    refactor(extension): adjust decorators for background endpoints

--- packages/extension/src/background/services/GraphQLService/decorators/ensureDomainPermission.js
+++ packages/extension/src/background/services/GraphQLService/decorators/ensureDomainPermission.js
@@ -4,7 +4,7 @@ import validateAccount from '../validators/validateAccount';
 import validateSession from '../validators/validateSession';
 import validateDomain from '../validators/validateDomain';
 
-export default function ensureUserPermission(func) {
+export default function ensureDomainPermission(func) {
     return pipe([
         validateExtension,
         validateSession,
