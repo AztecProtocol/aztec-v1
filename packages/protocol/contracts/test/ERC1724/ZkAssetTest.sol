4f2473e7ae458d0a7cf8a56475b6b8d372cb90c2
thomas-waite <thomas.waite4@gmail.com>
2019-09-05 12:37:40 +0100

    feat(protocol): implement blockTimeStamp based permissioning system

--- packages/protocol/contracts/test/ERC1724/ZkAssetTest.sol
+++ packages/protocol/contracts/test/ERC1724/ZkAssetTest.sol
@@ -22,8 +22,7 @@ contract ZkAssetTest is ZkAsset {
     } 
 
     /**
-    * @dev Modified version of the ZkAsset updateNoteMetaData() function. It removes the permissioning safety guard
-    * around who can call the function - for truffle testing purposes
+    * @dev Update the metadata of a note that already exists in storage. 
     * @param noteHash - hash of a note, used as a unique identifier for the note
     * @param metaData - metadata to update the note with
     */
@@ -31,10 +30,15 @@ contract ZkAssetTest is ZkAsset {
         ( uint8 status, , , address noteOwner ) = ace.getNote(address(this), noteHash);
         (, , bytes32 confidentialTotalMinted, bytes32 confidentialTotalBurned, , , , bool canAdjustSupply) = ace.getRegistry(address(this));
 
-        // Safety guard removed from here
+        // Permissioning check
+        bytes32 addressID = keccak256(abi.encodePacked(msg.sender, noteHash));
 
-        disapproveAddresses(noteHash);
+
+        // Approve the addresses in the note metaData
         approveAddresses(metaData, noteHash);
+
+        // Set the metaDatatTimeLog to the latest block time
+        setMetaDataTimeLog(noteHash);
 
         emit UpdateNoteMetaData(noteOwner, noteHash, metaData);
     }
