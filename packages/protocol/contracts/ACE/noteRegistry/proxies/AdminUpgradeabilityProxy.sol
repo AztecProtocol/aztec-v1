bda490020adf96e574aacc4ce8cd1dff5ebd50ad
Arnaud Schenk <arnaud@creditmint.io>
2019-09-06 16:50:44 +0100

    fix(protocol): add extra 0 address check

--- packages/protocol/contracts/ACE/noteRegistry/proxies/AdminUpgradeabilityProxy.sol
+++ packages/protocol/contracts/ACE/noteRegistry/proxies/AdminUpgradeabilityProxy.sol
@@ -19,6 +19,7 @@ contract AdminUpgradeabilityProxy is BaseAdminUpgradeabilityProxy, Upgradeabilit
     */
     constructor(address _logic, address _admin, bytes memory _data) UpgradeabilityProxy(_logic, _data) public payable {
         assert(ADMIN_SLOT == bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1));
+        require(_admin != address(0x0), "Cannot set the admin address to address(0x0)");
         _setAdmin(_admin);
     }
 }
