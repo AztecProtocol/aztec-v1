04d333c79ccff948bbc3521693c2a2e98201ab97
Malkevych Bohdan <bomalkevych@gmail.com>
2019-09-09 10:14:28 +0300

    refactor(extension): refactored account saving with batches

---
+++ packages/extension/src/background/services/eventsSyncServices/AccountSyncService/utils/account/index.js
@@ -0,0 +1,5 @@
+import performCreateBulkAccounts from './createBulkAccounts';
+
+export const createBulkAccounts = (accounts) => {
+    return performCreateBulkAccounts(accounts);
+}
--- packages/extension/src/background/services/eventsSyncServices/AccountSyncService/utils/addAccount/index.js
+++
@@ -1,5 +0,0 @@
-import createOrUpdateAccount from './createOrUpdateAccount';
-
-export default async function addAccount(account) {
-    return createOrUpdateAccount(account);
-}
