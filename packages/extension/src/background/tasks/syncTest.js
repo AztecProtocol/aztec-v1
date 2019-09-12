210e647666f3dd650a96c40dff555f75614a54ac
Malkevych Bohdan <bomalkevych@gmail.com>
2019-09-12 15:29:40 +0300

    fix(extension): added config providersfor infura

--- packages/extension/src/background/tasks/syncTest.js
+++ packages/extension/src/background/tasks/syncTest.js
@@ -1,16 +1,36 @@
-import Web3 from 'web3';
 import Web3Service from '~background/services/Web3Service';
-import decodeNoteLogs from '../services/eventsSyncServices/NoteSyncService/utils/decodeNoteLogs';
-import associatedNotesWithOwner from '../services/eventsSyncServices/NoteSyncService/utils/associatedNotesWithOwner';
-import saveNotes from '../services/eventsSyncServices/NoteSyncService/utils/saveNotes';
+import decodeNoteLogs from '../services/EventService/utils/helpers/decodeNoteLogs';
+import { 
+    saveNotes 
+} from '../services/EventService/utils/saveNotes';
 import {
     IZkAssetConfig,
 } from '~background/config/contracts';
 import { infuraHttpProviderURI } from '~background/helpers/InfuraTestCreds';
+import Web3ServiceFactory from '~background/services/Web3Service/factory';
+import {
+    AZTECAccountRegistryConfig,
+    ACEConfig,
+} from '../config/contracts';
+import {
+    clearDB
+ } from '~background/database';
 
+
+const contractsConfigs = [
+    AZTECAccountRegistryConfig.config,
+    ACEConfig.config,
+];
+
+const networksConfig = {
+    title: 'rinkeby',
+    networkId: 9999,
+    providerUrl: infuraHttpProviderURI,
+    contractsConfigs: contractsConfigs,
+};
 
 const optionsForAllEvents = (address) => {
-    const { abi } = Web3Service.eth;
+    const { abi } = Web3Service(networksConfig.networkId).eth;
 
     const eventsTopics = [
         IZkAssetConfig.events.createNote,
@@ -34,25 +54,20 @@ const optionsForAllEvents = (address) => {
 export const runLoadingEventsTest = async () => {
     console.log(`This is only for testing purpuse (measure speed of loading, storing items into real IndexedDB). Please disable it if you dont know what it is`)
 
-    const provider = new Web3.providers.HttpProvider(infuraHttpProviderURI)
-    Web3Service.init({
-        provider,
-    })
+    clearDB({networkId: networksConfig.networkId});
 
-    const access = {
-        address: '0x27b60ccecad263fd6ba595c68ad0e4c968ad9c67',
-    }
+    Web3ServiceFactory.setConfigs([networksConfig]);
+
     const owner = {
         address: '0x27b60ccecad263fd6ba595c68ad0e4c968ad9c67',
     }
 
     let assetEventsEmitterAddress = '0x99b8385e3d95c13e7239ef2382f71d810b1c623a';
-    const filterByAddress = access.address;
 
     // given
     const options = optionsForAllEvents(assetEventsEmitterAddress);
     const eventsTopics = options.topics[0];
-    const { getPastLogs } = Web3Service.eth;
+    const { getPastLogs } = Web3Service(networksConfig.networkId).eth;
 
     // action
     const tStart = performance.now();
@@ -63,19 +78,16 @@ export const runLoadingEventsTest = async () => {
     console.log(`Load past logs ${rawLogs.length} took: ${((t1 - t0) / 1000)} seconds.`);
 
     t0 = performance.now();
-    const decodedLogs = decodeNoteLogs(eventsTopics, rawLogs);
+    const decodedLogs = decodeNoteLogs(eventsTopics, rawLogs, networksConfig.networkId);
     t1 = performance.now();
     console.log(`Decode raw logs ${decodedLogs.createNotes.length} took: ${((t1 - t0) / 1000)} seconds.`)
-    
-    t0 = performance.now();
-    const notes = associatedNotesWithOwner(decodedLogs, filterByAddress);
-    t1 = performance.now();
-    console.log(`Filtering logs ${notes.createNotes.length} took: ${((t1 - t0) / 1000)} seconds.`)
+
+    console.log(`${JSON.stringify(decodedLogs)}`)
 
     t0 = performance.now();
-    await saveNotes(notes);
+    await saveNotes(decodedLogs, networksConfig.networkId);
     t1 = performance.now();
-    console.log(`Saving logs ${notes.createNotes.length} into indexedDB db took: ${((t1 - t0) / 1000)} seconds.`)
+    console.log(`Saving logs ${decodedLogs.createNotes.length} into indexedDB db took: ${((t1 - t0) / 1000)} seconds.`)
 
     const tEnd = performance.now();
 
