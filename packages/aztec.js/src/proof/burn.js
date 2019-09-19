38606dcb2c1d1ed05c1042aba053512aac8aa426
thomas-waite <thomas.waite4@gmail.com>
2019-09-19 09:58:11 +0100

    refactor(aztec.js): update proof export scripts with proofHandler

--- packages/aztec.js/src/proof/burn.js
+++ packages/aztec.js/src/proof/burn.js
@@ -1,12 +1,35 @@
 /* eslint-disable func-names */
-const BurnProof66305 = require('./epoch0/BURN/burn');
-const proofExport = require('./proofExport');
+const helpers = require('./helpers');
+const proofHandler = require('./proofHandler');
+const { ProofType } = require('./epoch0/proof');
 
+/**
+ * Export the BurnProof for a default epoch
+ *
+ * @method BurnProof
+ * @param  {...any} args - rest parameter representing all
+ * @returns An instance of the default BurnProof with the passed parameters
+ */
 function BurnProof(...args) {
-    return proofExport.exportProof(BurnProof66305, ...args);
+    return proofHandler.exportProof.bind({ epochNum: this.epochNum })(ProofType.BURN.name, ...args);
 }
 
-BurnProof.epoch = function(epochNum) {
+/**
+ * Export a BurnProof for a given epoch number
+ *
+ * @method epoch
+ * @param {Number} epochNum - epoch number for which a BurnProof is to be returned
+ * @param {bool} setDefaultEpoch - if true, sets the inputted epochNum to be the default. If false, does
+ * not set the inputted epoch number to be the default
+ * @returns A BurnProof construction for the given epoch number
+ */
+BurnProof.epoch = function(epochNum, setDefaultEpoch = false) {
+    helpers.validateEpochNum(epochNum);
+
+    if (setDefaultEpoch) {
+        proofHandler.setDefaultEpoch(epochNum);
+    }
+
     return (...args) => {
         return BurnProof.bind({ epochNum })(...args);
     };
--- packages/aztec.js/src/proof/swap.js
+++ packages/aztec.js/src/proof/swap.js
@@ -1,12 +1,35 @@
 /* eslint-disable func-names */
-const proofExport = require('./proofExport');
-const SwapProof65794 = require('./epoch0/BALANCED/swap');
+const helpers = require('./helpers');
+const proofHandler = require('./proofHandler');
+const { ProofType } = require('./epoch0/proof');
 
+/**
+ * Export the SwapProof for a default epoch
+ *
+ * @method SwapProof
+ * @param  {...any} args - rest parameter representing all
+ * @returns An instance of the default SwapProof with the passed parameters
+ */
 function SwapProof(...args) {
-    return proofExport.exportProof(SwapProof65794, ...args);
+    return proofHandler.exportProof.bind({ epochNum: this.epochNum })(ProofType.SWAP.name, ...args);
 }
 
-SwapProof.epoch = function(epochNum) {
+/**
+ * Export a SwapProof for a given epoch number
+ *
+ * @method epoch
+ * @param {Number} epochNum - epoch number for which a SwapProof is to be returned
+ * @param {bool} setDefaultEpoch - if true, sets the inputted epochNum to be the default. If false, does
+ * not set the inputted epoch number to be the default
+ * @returns A SwapProof construction for the given epoch number
+ */
+SwapProof.epoch = function(epochNum, setDefaultEpoch = false) {
+    helpers.validateEpochNum(epochNum);
+
+    if (setDefaultEpoch) {
+        proofHandler.setDefaultEpoch(epochNum);
+    }
+
     return (...args) => {
         return SwapProof.bind({ epochNum })(...args);
     };
