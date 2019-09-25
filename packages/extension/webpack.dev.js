8fd13356d15b20bca31f4f8c3e7e91fc59b7d8a2
Joe Andrews <joe@fuuzik.com>
2019-09-25 11:41:29 +0100

    feat(extension): add eslint errors to babel

--- packages/extension/webpack.dev.js
+++ packages/extension/webpack.dev.js
@@ -28,7 +28,7 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                use: ['babel-loader'],
+                use: ['babel-loader', 'eslint-loader'],
                 // options: {
                 //     presets: ['@babel/env'],
                 // },
