644cd13ac52864fd749d4beab900f9084586b119
Joe Andrews <joe@fuuzik.com>
2019-10-02 13:27:31 +0100

    feat(extension): fix send proof task

--- packages/extension/src/client/demo/ownable.js
+++ packages/extension/src/client/demo/ownable.js
@@ -24,7 +24,7 @@ export default async function demoOwnable({
         address: userAddress,
     } = Web3Service.account;
 
-    let zkAssetAddress = '0x6B54197d159d71FaafE01B35b697932AC1d4D292'; // ADD EXISTING ASSET ADDRESS HERE
+    let zkAssetAddress = '0x2baEFf6946CD6904Da9387FcF56169058fFE5Bf6'; // ADD EXISTING ASSET ADDRESS HERE
     if (!zkAssetAddress) {
         log('Creating new asset...');
         const {
