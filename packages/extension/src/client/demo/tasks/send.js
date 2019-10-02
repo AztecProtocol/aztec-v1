1e16724227dc7df240269d8ebf67bf69df9653d3
Joe Andrews <joe@fuuzik.com>
2019-10-02 12:41:35 +0100

    feat(extension): update send proof task

--- packages/extension/src/client/demo/tasks/send.js
+++ packages/extension/src/client/demo/tasks/send.js
@@ -7,11 +7,5 @@ export default async function send(asset, transactions, options) {
     const sendProof = await asset.send(transactions, options);
     log(sendProof.export());
 
-    log('Approving send proof...');
-    await sendProof.approve();
-    log('Approved!');
-
-    log('Sending...');
-    await sendProof.send();
     log(`Successfully sent ${sendAmount} to account '${receiver}'.`);
 }
