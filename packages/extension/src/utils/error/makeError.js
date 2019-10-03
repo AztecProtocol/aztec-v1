c0f8882e5dbb54571f958ab97be9800f4ab04fa3
Leila Wang <leizciw@gmail.com>
2019-10-03 18:43:13 +0100

    fix(extension): use messageOptions and data as i18n variables

--- packages/extension/src/utils/error/makeError.js
+++ packages/extension/src/utils/error/makeError.js
@@ -13,7 +13,10 @@ export default function makeError(type, errorI18n) {
             warnLog(`Error '${key}' is not defined.`);
             message = key;
         } else {
-            message = errorI18n.t(key, data);
+            message = errorI18n.t(key, {
+                ...data,
+                ...messageOptions,
+            });
         }
         return {
             error: {
