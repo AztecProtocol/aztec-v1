859edb8d696b8aa4bbb32a8b52f97c220a46aa1a
Leila Wang <leizciw@gmail.com>
2019-10-03 15:23:00 +0100

    refactor(extension): separate resolvers for ui and background

--- packages/extension/src/background/services/GraphQLService/resolvers/background.js
+++ packages/extension/src/background/services/GraphQLService/resolvers/background.js
@@ -1,14 +1,47 @@
 import ClientSubscriptionService from '~background/services/ClientSubscriptionService';
+import userModel from '~database/models/user';
 import {
     ensureKeyvault, // TODO rename this also checks session
     ensureDomainPermission,
 } from '../decorators';
 import mergeResolvers from './utils/mergeResolvers';
 import syncUserInfo from './utils/syncUserInfo';
+import getAccounts from './utils/getAccounts';
+import requestGrantAccess from './utils/requestGrantAccess';
+import pickNotesFromBalance from './utils/pickNotesFromBalance';
+import syncAssetInfo from './utils/syncAssetInfo';
+import syncNoteInfo from './utils/syncNoteInfo';
+import syncUtilityNoteInfo from './utils/syncUtilityNoteInfo';
 import base from './base';
 
 const backgroundResolvers = {
     Query: {
+        user: ensureDomainPermission(async (_, args) => ({
+            account: await userModel.get({
+                id: (args.id || args.currentAddress),
+            }),
+        })),
+        asset: ensureDomainPermission(async (_, args) => ({
+            asset: await syncAssetInfo(args),
+        })),
+        note: ensureDomainPermission(async (_, args, ctx) => ({
+            note: await syncNoteInfo(args, ctx),
+        })),
+        utilityNote: ensureDomainPermission(async (_, args, ctx) => ({
+            note: await syncUtilityNoteInfo(args, ctx),
+        })),
+        grantNoteAccessPermission: ensureDomainPermission(async (_, args, ctx) => ({
+            permission: await requestGrantAccess(args, ctx),
+        })),
+        pickNotesFromBalance: ensureDomainPermission(async (_, args, ctx) => ({
+            notes: await pickNotesFromBalance(args, ctx),
+        })),
+        account: ensureDomainPermission(async (_, args) => ({
+            account: await userModel.get({ address: args.currentAddress }),
+        })),
+        accounts: ensureDomainPermission(async (_, args) => ({
+            accounts: await getAccounts(args),
+        })),
         subscribe: ensureDomainPermission(async (_, args) => ({
             success: ClientSubscriptionService.grantSubscription(args),
         })),
--- packages/extension/src/background/services/GraphQLService/resolvers/base.js
+++ packages/extension/src/background/services/GraphQLService/resolvers/base.js
@@ -1,24 +1,14 @@
 import BigInt from 'apollo-type-bigint';
 import assetModel from '~database/models/asset';
-import userModel from '~database/models/user';
 import accountModel from '~database/models/account';
 import {
     fromCode,
 } from '~utils/noteStatus';
-import {
-    ensureDomainPermission,
-} from '../decorators';
 import getUserSpendingPublicKey from './utils/getUserSpendingPublicKey';
-import getAccounts from './utils/getAccounts';
 import decryptViewingKey from './utils/decryptViewingKey';
 import getViewingKeyFromMetadata from './utils/getViewingKeyFromMetadata';
 import getDecryptedViewingKeyFromMetadata from './utils/getDecryptedViewingKeyFromMetadata';
 import getAssetBalance from './utils/getAssetBalance';
-import requestGrantAccess from './utils/requestGrantAccess';
-import pickNotesFromBalance from './utils/pickNotesFromBalance';
-import syncAssetInfo from './utils/syncAssetInfo';
-import syncNoteInfo from './utils/syncNoteInfo';
-import syncUtilityNoteInfo from './utils/syncUtilityNoteInfo';
 
 export default {
     BigInt: new BigInt('bigInt'),
@@ -42,33 +32,6 @@ export default {
     GrantNoteAccessPermission: {
         asset: ({ asset }) => asset && assetModel.get({ id: asset }),
     },
-    Query: {
-        user: ensureDomainPermission(async (_, args, ...rest) => ({
-            account: await userModel.get({
-                id: (args.id || args.currentAddress),
-            }),
-        })),
-        asset: ensureDomainPermission(async (_, args) => ({
-            asset: await syncAssetInfo(args),
-        })),
-        note: ensureDomainPermission(async (_, args, ctx) => ({
-            note: await syncNoteInfo(args, ctx),
-        })),
-        utilityNote: ensureDomainPermission(async (_, args, ctx) => ({
-            note: await syncUtilityNoteInfo(args, ctx),
-        })),
-        grantNoteAccessPermission: ensureDomainPermission(async (_, args, ctx) => ({
-            permission: await requestGrantAccess(args, ctx),
-        })),
-        pickNotesFromBalance: ensureDomainPermission(async (_, args, ctx) => ({
-            notes: await pickNotesFromBalance(args, ctx),
-        })),
-        account: ensureDomainPermission(async (_, args) => ({
-            account: await userModel.get({ address: args.currentAddress }),
-        })),
-        accounts: ensureDomainPermission(async (_, args) => ({
-            accounts: await getAccounts(args),
-        })),
-    },
+    Query: {},
     Mutation: {},
 };
