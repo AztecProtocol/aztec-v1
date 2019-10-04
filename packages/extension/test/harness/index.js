0c95442e62db6016666cb59dcf12928f24ad63ed
Arnaud Schenk <arnaud@creditmint.io>
2019-10-04 10:12:23 +0100

    test(extension): fix multi-instantiation impacting shared state

--- packages/extension/test/harness/index.js
+++ packages/extension/test/harness/index.js
@@ -7,44 +7,51 @@ const steps = require('./steps');
 
 const wait = ms => new Promise(r => setTimeout(r, ms));
 
-const Environment = {
-    extension: undefined,
-    metamask: undefined,
-    metadata: {},
-    openPages: [],
-    init: async function(extensionPath, {
-        metamaskPath = path.resolve(__dirname + '/../../../../node_modules/dappeteer/metamask/5.3.0/'),
-        extensionName = "AZTEC",
-        network = 'localhost',
-        extensionHomePage = 'pages/defaultPopup.html',
-    } = {}) {
-        this.extensionName = extensionName;
-        this.browser = await dappeteer.launch(puppeteer, {
-            headless: false, // extension are allowed only in head-full mode
-            args: [
-                `--disable-extensions-except=${extensionPath},${metamaskPath}`,
-                `--load-extension=${extensionPath}`
-            ]
-        });
+async function init(extensionPath, {
+    metamaskPath = path.resolve(__dirname + '/../../../../node_modules/dappeteer/metamask/5.3.0/'),
+    extensionName = "AZTEC",
+    network = 'localhost',
+    extensionHomePage = 'pages/defaultPopup.html',
+} = {}) {
+    const environment = {
+        extension: undefined,
+        metamask: undefined,
+        metadata: {},
+        openPages: [],
+        ...steps,
+        ...scenarios,
+        clean: async function() {
+            return Promise.all(this.openPages.map(async page => page.close()))
+        },
+    };
 
-        this.metamask = await dappeteer.getMetamask(this.browser);
-        await this.metamask.switchNetwork(network);
+    environment.extensionName = extensionName;
+    environment.browser = await dappeteer.launch(puppeteer, {
+        headless: false, // extension are allowed only in head-full mode
+        args: [
+            `--disable-extensions-except=${extensionPath},${metamaskPath}`,
+            `--load-extension=${extensionPath}`
+        ]
+    });
 
-        const extensionBackgroundTarget = await this.getExtensionBackground();
-        const extensionUrl = extensionBackgroundTarget._targetInfo.url || '';
-        const [,, extensionID] = extensionUrl.split('/');
-    
-        const extensionLink = `chrome-extension://${extensionID}/${extensionHomePage}`;
-        this.metadata = {
-            url: extensionUrl,
-            id: extensionID,
-            link: extensionLink
-        };
+    environment.metamask = await dappeteer.getMetamask(environment.browser);
+    await environment.metamask.switchNetwork(network);
 
-        return this;
-    },
-    ...steps,
-    ...scenarios
+    const extensionBackgroundTarget = await environment.getExtensionBackground();
+    const extensionUrl = extensionBackgroundTarget._targetInfo.url || '';
+    const [,, extensionID] = extensionUrl.split('/');
+
+    const extensionLink = `chrome-extension://${extensionID}/${extensionHomePage}`;
+    environment.metadata = {
+        url: extensionUrl,
+        id: extensionID,
+        link: extensionLink,
+        network,
+    };
+
+    return environment;
 }
 
-module.exports = Environment;
+module.exports = {
+    init
+};
