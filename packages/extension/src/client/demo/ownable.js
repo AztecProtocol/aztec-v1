af0d1bb85181bd2838962759feb2c09544c87184
Arnaud Schenk <arnaud@creditmint.io>
2019-10-04 22:45:30 +0100

    test(extension): remove dappeter; implement own metamask signup flow

--- packages/extension/src/client/demo/ownable.js
+++ packages/extension/src/client/demo/ownable.js
@@ -17,115 +17,115 @@ export default async function demoOwnable({
     initialERC20Balance = 200,
     scalingFactor = 1,
 } = {}) {
-    const { aztec } = window;
-    await aztec.enable();
+    // const { aztec } = window;
+    // await aztec.enable();
 
-    const {
-        address: userAddress,
-    } = Web3Service.account;
+    // const {
+    //     address: userAddress,
+    // } = Web3Service.account;
 
-    let zkAssetAddress = ''; // ADD EXISTING ASSET ADDRESS HERE
-    if (!zkAssetAddress) {
-        log('Creating new asset...');
-        const {
-            erc20Address,
-            zkAssetAddress: newZkAssetAddress,
-        } = await createNewAsset({
-            zkAssetType: 'ZkAssetOwnable',
-            scalingFactor,
-        });
+    // // let zkAssetAddress = '0x2baEFf6946CD6904Da9387FcF56169058fFE5Bf6'; // ADD EXISTING ASSET ADDRESS HERE
+    // // if (!zkAssetAddress) {
+    // //     log('Creating new asset...');
+    // //     const {
+    // //         erc20Address,
+    // //         zkAssetAddress: newZkAssetAddress,
+    // //     } = await createNewAsset({
+    // //         zkAssetType: 'ZkAssetOwnable',
+    // //         scalingFactor,
+    // //     });
 
-        await depositToERC20({
-            userAddress,
-            amount: initialERC20Balance,
-            erc20Address,
-        });
+    // //     await depositToERC20({
+    // //         userAddress,
+    // //         amount: initialERC20Balance,
+    // //         erc20Address,
+    // //     });
 
-        zkAssetAddress = newZkAssetAddress;
-        log(
-            'New zk asset created!',
-            'Asset balance = 0',
-            `Linked ERC20 account balance = ${initialERC20Balance}.`,
-        );
+    // //     zkAssetAddress = newZkAssetAddress;
+    // //     log(
+    // //         'New zk asset created!',
+    // //         'Asset balance = 0',
+    // //         `Linked ERC20 account balance = ${initialERC20Balance}.`,
+    // //     );
 
-        warnLog(
-            'Add this address to demo file to prevent creating new asset:',
-            zkAssetAddress,
-        );
-    }
+    // //     warnLog(
+    // //         'Add this address to demo file to prevent creating new asset:',
+    // //         zkAssetAddress,
+    // //     );
+    // // }
 
-    const asset = await aztec.asset(zkAssetAddress);
+    // const asset = await aztec.asset(zkAssetAddress);
 
-    log(asset);
-    if (!asset.isValid()) {
-        log('Asset is not valid.');
-        return;
-    }
+    // log(asset);
+    // if (!asset.isValid()) {
+    //     log('Asset is not valid.');
+    //     return;
+    // }
 
-    const logBalance = async () => {
-        await sleep(1000);
-        log(`Asset balance = ${await asset.balance()}`);
-    };
+    // const logBalance = async () => {
+    //     await sleep(1000);
+    //     log(`Asset balance = ${await asset.balance()}`);
+    // };
 
-    await logBalance();
+    // await logBalance();
 
-    const depositAmount = randomInt(1, 50);
+    // const depositAmount = randomInt(1, 50);
 
-    let erc20Balance = await asset.balanceOfLinkedToken();
-    if (erc20Balance >= depositAmount) {
-        log(`ERC20 account balance = ${erc20Balance}`);
-    } else {
-        log(`ERC20 balance (${erc20Balance}) is not enough to make a deposit of ${depositAmount}.`);
+    // let erc20Balance = await asset.balanceOfLinkedToken();
+    // if (erc20Balance >= depositAmount) {
+    //     log(`ERC20 account balance = ${erc20Balance}`);
+    // } else {
+    //     log(`ERC20 balance (${erc20Balance}) is not enough to make a deposit of ${depositAmount}.`);
 
-        log('Sending free token...', {
-            userAddress,
-            amount: depositAmount,
-        });
+    //     log('Sending free token...', {
+    //         userAddress,
+    //         amount: depositAmount,
+    //     });
 
-        await depositToERC20({
-            userAddress,
-            amount: depositAmount,
-            erc20Address: asset.linkedTokenAddress,
-        });
+    //     await depositToERC20({
+    //         userAddress,
+    //         amount: depositAmount,
+    //         erc20Address: asset.linkedTokenAddress,
+    //     });
 
-        erc20Balance = await asset.balanceOfLinkedToken();
-        log(`Your new ERC20 account balance is ${erc20Balance}.`);
-    }
-
-
-    await deposit(asset, [{
-        amount: depositAmount,
-        to: userAddress,
-    }], {
-        from: userAddress,
-        sender: userAddress,
-    });
+    //     erc20Balance = await asset.balanceOfLinkedToken();
+    //     log(`Your new ERC20 account balance is ${erc20Balance}.`);
+    // }
 
 
-    await logBalance();
+    // await deposit(asset, [{
+    //     amount: depositAmount,
+    //     to: userAddress,
+    // }], {
+    //     from: userAddress,
+    //     sender: userAddress,
+    // });
 
 
-    const withdrawAmount = randomInt(1, 10);
-    await withdraw(asset, withdrawAmount, {
-        sender: userAddress,
-        from: userAddress,
-        to: userAddress,
-    });
+    // await logBalance();
 
 
-    await logBalance();
+    // const withdrawAmount = randomInt(1, 10);
+    // await withdraw(asset, withdrawAmount, {
+    //     sender: userAddress,
+    //     from: userAddress,
+    //     to: userAddress,
+    // });
 
 
-    const sendAmount = randomInt(1, 10);
-    await send(asset, [{
-        amount: sendAmount,
-        to: userAddress,
-    }], {
-        from: userAddress,
-        sender: userAddress,
-
-    });
+    // await logBalance();
 
 
-    await logBalance();
+    // const sendAmount = randomInt(1, 10);
+    // await send(asset, [{
+    //     amount: sendAmount,
+    //     to: userAddress,
+    // }], {
+    //     from: userAddress,
+    //     sender: userAddress,
+
+    // });
+
+
+    // await logBalance();
 }
