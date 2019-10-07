a14a4a9d968cdd47069126a32ab3675d0010f79b
Arnaud Schenk <arnaud@creditmint.io>
2019-10-07 18:40:09 +0100

    test(extension): fix uncaught exception, start debugging erc20

--- packages/extension/test/ExtensionUI.js
+++ packages/extension/test/ExtensionUI.js
@@ -20,10 +20,15 @@ dotenv.config();
 contract.only('Extension', (accounts) => {
     const [user] = accounts;
     let environment;
+    let zkAsset;
+    let erc20;
     before(async () => {
         const ace = await ACE.deployed();
-        const erc20 = await ERC20Mintable.deployed();
-        const zkAsset = await ZkAssetOwnable.new(ace.address, erc20.address, 1);
+        erc20 = await ERC20Mintable.deployed();
+        zkAsset = await ZkAssetOwnable.new(ace.address, erc20.address, 1);
+        console.log(zkAsset.transactionHash);
+        console.log(zkAsset.address);
+        console.log(erc20.address)
         await zkAsset.setProofs(1, 17);
         await erc20.mint(user, 10000);
         await erc20.approve(ace.address, 10000);
@@ -36,20 +41,23 @@ contract.only('Extension', (accounts) => {
 
     it('should successfully create an AZTEC account', async () => {
         await environment.createAccount();
-        console.log('here!');
 
         const extensionPage = await environment.openExtension();
         const header = await extensionPage.api.waitForXPath("//div[contains(., 'My Assets')]");
         expect(header).to.not.equal(undefined);
+        await extensionPage.close();
     });
 
-    // it('should run demo script', async () => {
-    //     const homepage = await environment.openPage('https://www.aztecprotocol.com');
-    //     await homepage.api.evaluate(() => window.aztec.enable());
-    //     // const authorizePage = await environment.getPage(target => target.url().match(/register\/domain/));
-    //     // await authorizePage.clickMain();
-    //     // await homepage.api.reload();
-    //     // await environment.metamask.sign();
-    //     await environment.wait(100000);
-    // });
+    it('should set an AZTEC asset', async () => {
+        const homepage = Object.values(environment.openPages)
+            .find(p => p.aztecContext === true);
+
+        const asset = await homepage.api.evaluate(async (address) => await window.aztec.asset(address), zkAsset.address);
+        expect(asset.address).to.equal(zkAsset.address);
+        expect(asset.linkedTokenAddress).to.equal(erc20.address);
+        const isValid = await homepage.api.evaluate(async (address) => (await window.aztec.asset(address)).isValid(), zkAsset.address);
+        expect(isValid).to.equal(true);
+        const erc20Balance = await homepage.api.evaluate(async (address) => (await window.aztec.asset(address)).balanceOfLinkedToken(), zkAsset.address);
+        console.log(asset);
+    });
 });
