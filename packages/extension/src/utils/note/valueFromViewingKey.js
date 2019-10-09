507b2f0b7419ca8184edf6ca5b0c0a50bd499ee8
Joe Andrews <joe@fuuzik.com>
2019-10-09 11:40:58 +0100

    feat(extension): fix value from viewing key

--- packages/extension/src/utils/note/valueFromViewingKey.js
+++ packages/extension/src/utils/note/valueFromViewingKey.js
@@ -1,4 +1,4 @@
 import BN from 'bn.js';
 import bn128 from '@aztec/bn128';
 
-export default viewingKey => new BN(viewingKey.slice(66, 74), 16).toRed(bn128.groupReduction);
+export default viewingKey => new BN(viewingKey.slice(66, 74), 16).toRed(bn128.groupReduction).toNumber();
