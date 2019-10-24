86c45e841c62856f125bf79f2463f0c0cf29fcd8
Leila Wang <leizciw@gmail.com>
2019-10-24 23:23:38 +0100

    feat(extension): improve RawNoteManager for syncing notes from indexedDB

--- packages/extension/src/background/services/NoteService/config.js
+++ packages/extension/src/background/services/NoteService/config.js
@@ -6,5 +6,6 @@ export const defaultMaxRawNotes = 2 ** 18;
 export const defaultMaxCallbacks = UNLIMITED;
 export const defaultMaxProcesses = 2 ** 3;
 export const defaultNotesPerBatch = 200;
+export const defaultNotesPerSyncBatch = 1000;
 export const defaultNotesPerDecryptionBatch = 100;
 export const defaultSyncInterval = 1000;
