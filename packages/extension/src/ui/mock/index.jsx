a9c330b7092ba8fd8258243a5149d013309fe5dc
Leila Wang <leizciw@gmail.com>
2019-10-25 14:21:34 +0100

    fix(extension): add required data for mock ui

--- packages/extension/src/ui/mock/index.jsx
+++ packages/extension/src/ui/mock/index.jsx
@@ -3,44 +3,15 @@ import ReactDOM from 'react-dom';
 import {
     HashRouter as Router,
 } from 'react-router-dom';
-import ApolloClient from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
-import { SchemaLink } from 'apollo-link-schema';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { makeExecutableSchema } from 'graphql-tools';
-import typeDefs from '~background/services/GraphQLService/typeDefs/ui';
-import resolvers from '~background/services/GraphQLService/resolvers/ui';
 import App from '../App';
 import ControlPanel from './ControlPanel';
-
-const schema = makeExecutableSchema({
-    typeDefs,
-    resolvers,
-});
-
-const apollo = new ApolloClient({
-    link: new SchemaLink({ schema }),
-    cache: new InMemoryCache({
-        addTypename: false,
-    }),
-    defaultOptions: {
-        query: {
-            fetchPolicy: 'no-cache',
-        },
-        watchQuery: {
-            fetchPolicy: 'no-cache',
-        },
-    },
-    connectToDevTools: true,
-});
+import './window';
 
 ReactDOM.render(
-    <ApolloProvider client={apollo}>
-        <Router>
-            <ControlPanel>
-                <App mock />
-            </ControlPanel>
-        </Router>
-    </ApolloProvider>,
+    <Router>
+        <ControlPanel>
+            <App mock />
+        </ControlPanel>
+    </Router>,
     document.getElementById('app'),
 );
