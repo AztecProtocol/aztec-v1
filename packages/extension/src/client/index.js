c0e8ba9f43d94e18f1aa5fe31d4ce7557d57139f
Leila Wang <leizciw@gmail.com>
2019-10-25 11:50:11 +0100

    fix(extension): only run scripts in aztecprotocol.com

--- packages/extension/src/client/index.js
+++ packages/extension/src/client/index.js
@@ -3,8 +3,7 @@ import Aztec from './Aztec';
 window.aztec = new Aztec();
 
 if (process.env.NODE_ENV === 'development') {
-    if (window.location.hostname.match(/aztecprotocol/)) {
-        console.log('____run demo');
+    if (window.location.hostname.match(/^(www.)?aztecprotocol.com$/)) {
         const demo = require('./demo/ownable.js').default; // eslint-disable-line global-require
         demo();
     }
