4ab2a4b90f4857348f71b00c706fdabc60cbd679
Leila Wang <leizciw@gmail.com>
2019-10-31 18:42:15 +0000

    refactor(extension): remove deprcated code in NoteService

--- packages/extension/src/background/services/NoteService/utils/getBalanceFromNoteValues.js
+++
@@ -1,7 +0,0 @@
-export default function getBalanceFromNoteValues(noteValues = {}) {
-    return Object.keys(noteValues)
-        .reduce((accum, value) => {
-            const numberOfNotes = noteValues[value].length;
-            return accum + (value * numberOfNotes);
-        }, 0);
-}
--- packages/extension/src/background/services/NoteService/utils/newAssetNoteData.js
+++
@@ -1,7 +0,0 @@
-export default function newAssetNoteData() {
-    return {
-        balance: 0,
-        noteValues: {},
-        lastSynced: '',
-    };
-}
