de004a10754725e4abcf9bd8df064dba54b90ddd
Leila Wang <leizciw@gmail.com>
2019-11-04 16:10:34 +0000

    feat(extension): show loading page before ui iframe is loaded

--- packages/extension/src/ui/background.jsx
+++ packages/extension/src/ui/background.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import i18n from '~ui/helpers/i18n';
+import Popup from '~/ui/components/Popup';
+import Loading from '~/ui/views/Loading';
+import PoweredBy from '~/ui/components/PoweredBy';
 import './styles/guacamole.css';
-import './styles/background.scss';
+import styles from './styles/background.scss';
+
+const UiPlaceholder = () => {
+    const locale = 'en';
+    const theme = {
+        name: 'light',
+    };
+    const phrases = require(`./locales/${locale}`).default; // eslint-disable-line global-require, import/no-dynamic-require
+    i18n.setLocale(locale);
+    i18n.register(phrases);
+
+    return (
+        <div
+            id="aztec-popup"
+            className={styles['ui-placeholder']}
+        >
+            <div id="aztec-popup-ui" />
+            <div id="aztec-popup-placeholder">
+                <Popup>
+                    <Loading />
+                </Popup>
+            </div>
+            <PoweredBy
+                className={styles.footer}
+                theme={theme.name}
+            />
+        </div>
+    );
+};
 
 ReactDOM.render(
-    <div />,
-    document.getElementById('app'),
+    <UiPlaceholder />,
+    document.getElementById('background-app'),
 );
