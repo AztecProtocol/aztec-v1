1c0e4415651242ff0300f8f1f3ebbdb01ded3bff
Joe Andrews <joe@fuuzik.com>
2019-11-07 23:42:30 +0000

    feat(extension): add url config

--- packages/extension/src/config/urls.js
+++ packages/extension/src/config/urls.js
@@ -1,6 +1,17 @@
 // TODO - add another config file for production
-
-export default {
+const production = {
+    background: 'http://sdk.aztecprotocol.com/public/background.html',
+    ui: 'http://sdk.aztecprotocol.com/public/ui.html',
+};
+const development = {
     background: 'http://localhost:5555/sdk/public/background.html',
     ui: 'http://localhost:5555/sdk/public/ui.html',
+    // background: 'http://sdk.aztecprotocol.com/public/background.html',
+    // ui: 'http://sdk.aztecprotocol.com/public/ui.html',
 };
+const urls = {
+    PRODUCTION: production,
+    development,
+};
+const config = urls[process.env.NODE_ENV] || urls.development;
+export default config;
