5090db96c155bd997e28c1f68614f16727a3e826
Malkevych Bohdan <bomalkevych@gmail.com>
2019-11-08 16:52:31 +0200

    fix(extension): deposit with gsn

---
+++ packages/extension/contracts/AZTECAccountRegistryGSN.sol
@@ -0,0 +1,28 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "@openzeppelin/contracts-ethereum-package/contracts/GSN/GSNRecipient.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/GSN/bouncers/GSNBouncerSignature.sol";
+
+import "@aztec/protocol/contracts/interfaces/IZkAsset.sol";
+import "@aztec/protocol/contracts/libs/LibEIP712.sol";
+import "./AZTECAccountRegistry.sol";
+
+/**
+ * @title AZTECAccountRegistryGSN implementation
+ * @author AZTEC
+ * Copyright Spilbury Holdings Ltd 2019. All rights reserved.
+ **/
+
+contract AZTECAccountRegistryGSN is LibEIP712, AZTECAccountRegistry, GSNRecipient, GSNBouncerSignature {
+
+    constructor(
+        address _trustedAddress
+    ) public {
+        GSNRecipient.initialize();
+        GSNBouncerSignature.initialize(_trustedAddress);
+    }
+
+    function confidentialTransfer(address _registryOwner, bytes memory _proofData, bytes memory _signatures) public {
+        IZkAsset(_registryOwner).confidentialTransfer(_proofData, _signatures);
+    }
+}
--- packages/extension/src/client/apis/deposit/prove.js
+++ packages/extension/src/client/apis/deposit/prove.js
@@ -1,4 +1,5 @@
 import Web3Service from '~/client/services/Web3Service';
+import GSNService from '~/client/services/GSNService';
 import query from '~client/utils/query';
 
 export default async function proveDeposit({
@@ -12,6 +13,13 @@ export default async function proveDeposit({
         address,
     } = Web3Service.account;
 
+    const {
+        useGSN,
+        proxyContractAddress,
+    } = GSNService.getGSNConfig();;
+
+    const senderValue = useGSN ? proxyContractAddress : sender || address;
+    
     const data = await query({
         type: 'constructProof',
         args: {
@@ -19,8 +27,9 @@ export default async function proveDeposit({
             assetAddress,
             transactions,
             from: from || address,
-            sender: sender || address,
+            sender: senderValue,
             numberOfOutputNotes,
+            useGSN,
         },
     }) || {};
 
