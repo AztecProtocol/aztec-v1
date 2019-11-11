759804f52992f567fe8fec10f257ceae15c43b3f
thomas-waite <thomas.waite4@gmail.com>
2019-11-11 17:32:57 +0000

    test(protocol): add integration tests for key flows

--- packages/protocol/test/integration/AZTEC.js
+++ packages/protocol/test/integration/AZTEC.js
@@ -1,20 +1,24 @@
 /* global artifacts, expect, contract, it:true */
-const { JoinSplitProof, signer, note } = require('aztec.js');
+const {
+    BurnProof,
+    DividendProof,
+    JoinSplitProof,
+    MintProof,
+    note,
+    PrivateRangeProof,
+    PublicRangeProof,
+    signer,
+    SwapProof,
+} = require('aztec.js');
 const bn128 = require('@aztec/bn128');
 const {
-    constants,
-    proofs: {
-        DIVIDEND_PROOF,
-        JOIN_SPLIT_PROOF,
-        SWAP_PROOF,
-        PRIVATE_RANGE_PROOF,
-        PUBLIC_RANGE_PROOF,
-    },
+    proofs: { BURN_PROOF, DIVIDEND_PROOF, JOIN_SPLIT_PROOF, SWAP_PROOF, MINT_PROOF, PRIVATE_RANGE_PROOF, PUBLIC_RANGE_PROOF },
 } = require('@aztec/dev-utils');
 const secp256k1 = require('@aztec/secp256k1');
 
 const BN = require('bn.js');
-const { getNotesForAccount } = require('../helpers/ERC1724')
+const { getNotesForAccount } = require('../helpers/ERC1724');
+const factoryHelpers = require('../helpers/Factory');
 
 const ACE = artifacts.require('./ACE');
 const DividendValidator = artifacts.require('./Dividend');
@@ -22,14 +26,15 @@ const ERC20Mintable = artifacts.require('./ERC20Mintable');
 const JoinSplitValidator = artifacts.require('./Joinsplit');
 const PublicRangeValidator = artifacts.require('./PublicRange');
 const PrivateRangeValidator = artifacts.require('./PrivateRange');
-const SwapValidator = artifacts.require('./Swap')
-const ZkAsset = artifacts.require('./ZkAsset');
-
+const SwapValidator = artifacts.require('./Swap');
+const TestFactory = artifacts.require('./test/TestFactory');
+const ZkAsset = artifacts.require('./zkAssetAdjustable');
 
 contract('AZTEC integration', (accounts) => {
     let ace;
     let dividendValidator;
     let erc20;
+    let factoryContract;
     let joinSplitValidator;
     let privateRangeValidator;
     let publicRangeValidator;
@@ -37,7 +42,7 @@ contract('AZTEC integration', (accounts) => {
     let zkAsset;
     let aztecAccount;
     const scalingFactor = new BN(1);
-
+    const publicOwner = accounts[0];
 
     before(async () => {
         // instantiate all deployed contracts
@@ -48,13 +53,18 @@ contract('AZTEC integration', (accounts) => {
         publicRangeValidator = await PublicRangeValidator.at(PublicRangeValidator.address);
         swapValidator = await SwapValidator.at(SwapValidator.address);
 
-
         erc20 = await ERC20Mintable.at(ERC20Mintable.address);
-        zkAsset = await ZkAsset.new(ace.address, erc20.address, scalingFactor);
+        factoryContract = await TestFactory.new(ace.address);
+        zkAsset = await ZkAsset.new(ace.address, erc20.address, scalingFactor, 0, []);
         aztecAccount = secp256k1.generateAccount();
+
+        // Fund account with ERC20s
+        const tokensTransferred = new BN(500);
+        await erc20.mint(publicOwner, scalingFactor.mul(tokensTransferred));
+        await erc20.approve(ace.address, scalingFactor.mul(tokensTransferred));
     });
 
-    describe.only('Initialisation', async () => {
+    describe('Initialisation', async () => {
         it('should have set ACE owner', async () => {
             const owner = await ace.owner();
             expect(owner).to.equal(accounts[0]);
@@ -82,23 +92,79 @@ contract('AZTEC integration', (accounts) => {
             expect(swapValidatorAddress).to.equal(swapValidator.address);
         });
 
-        it('should have set ZkAsset linkedToken', async () => {
+        it('should have set zkAsset linkedToken', async () => {
             const linkedTokenAddress = await zkAsset.linkedToken();
             expect(linkedTokenAddress).to.equal(erc20.address);
         });
 
-        it('should have set ZkAsset owner', async () => {
-            // const zkAssetOwner = await zkAsset.owner();
-            // expect(zkAssetOwner).to.equal(accounts[1])
+        it('should have set zkAsset owner', async () => {
+            const zkAssetOwner = await zkAsset.owner();
+            expect(zkAssetOwner).to.equal(accounts[0]);
         });
-
     });
 
-    describe('Key flows', async () => {
+    describe.only('Key flows', async () => {
         const sender = accounts[0];
-        const publicOwner = accounts[2];
 
-        it.only('should perform a confidentialTransfer(), with a deposit proof', async () => {
+        it('should verify a dividend proof', async () => {
+            const za = 100;
+            const zb = 5;
+            const notionalNote = await note.create(aztecAccount.publicKey, 90);
+            const residualNote = await note.create(aztecAccount.publicKey, 4);
+            const targetNote = await note.create(aztecAccount.publicKey, 50);
+
+            const proof = new DividendProof(notionalNote, residualNote, targetNote, sender, za, zb);
+            const data = proof.encodeABI();
+            const { receipt } = await ace.validateProof(DIVIDEND_PROOF, sender, data);
+            expect(receipt.status).to.equal(true);
+        });
+
+        it('should verify a private range proof', async () => {
+            const originalNote = await note.create(aztecAccount.publicKey, 10);
+            const comparisonNote = await note.create(aztecAccount.publicKey, 4);
+            const utilityNote = await note.create(aztecAccount.publicKey, 6);
+
+            const proof = new PrivateRangeProof(originalNote, comparisonNote, utilityNote, sender);
+            const data = proof.encodeABI();
+            const { receipt } = await ace.validateProof(PRIVATE_RANGE_PROOF, sender, data);
+            expect(receipt.status).to.equal(true);
+        });
+
+        it('should verify a public range range proof', async () => {
+            const originalNote = await note.create(aztecAccount.publicKey, 50);
+            const utilityNote = await note.create(aztecAccount.publicKey, 40);
+            const publicComparison = 10;
+            const isGreaterOrEqual = true;
+
+            const proof = new PublicRangeProof(originalNote, publicComparison, sender, isGreaterOrEqual, utilityNote);
+            const data = proof.encodeABI();
+            const { receipt } = await ace.validateProof(PUBLIC_RANGE_PROOF, sender, data);
+            expect(receipt.status).to.equal(true);
+        });
+
+        it.only('should verify a swap proof ', async () => {
+            const asks = [10, 20];
+            const bids = [10, 20];
+            const maker = secp256k1.generateAccount();
+            const taker = secp256k1.generateAccount();
+
+            const inputNotes = [
+                await note.create(maker.publicKey, bids[0]),
+                await note.create(taker.publicKey, bids[1]),
+            ];
+
+            const outputNotes = [
+                await note.create(maker.publicKey, asks[0]),
+                await note.create(taker.publicKey, asks[1]),
+            ];
+
+            const proof = new SwapProof(inputNotes, outputNotes, sender);
+            const data = proof.encodeABI();
+            const { receipt } = await ace.validateProof(SWAP_PROOF, sender, data);
+            expect(receipt.status).to.equal(true);
+        });
+
+        it('should perform a confidentialTransfer(), with a deposit proof', async () => {
             // Convert 100 tokens into two output notes
             const depositInputNotes = [];
             const depositInputOwnerAccounts = [];
@@ -108,38 +174,193 @@ contract('AZTEC integration', (accounts) => {
 
             const proof = new JoinSplitProof(depositInputNotes, depositOutputNotes, sender, publicValue, publicOwner);
             const data = proof.encodeABI(zkAsset.address);
-            console.log('before construct signatures');
             const signatures = proof.constructSignatures(zkAsset.address, depositInputOwnerAccounts);
-            console.log('constructed signatures')
 
-            const balancePreTransfer = await erc20.balanceOf(accounts[0]);
+            const balancePreTransfer = await erc20.balanceOf(publicOwner);
             const transferAmountBN = new BN(depositPublicValue);
             const expectedBalancePostTransfer = balancePreTransfer.sub(transferAmountBN.mul(scalingFactor));
 
-            console.log('constructed public approve')
-            await ace.publicApprove(zkAsset.address, proof.hash, depositPublicValue, { from: accounts[0] });
+            await ace.publicApprove(zkAsset.address, proof.hash, depositPublicValue, { from: publicOwner });
             const { receipt } = await zkAsset.methods['confidentialTransfer(bytes,bytes)'](data, signatures, {
-                from: accounts[0],
+                from: sender,
             });
             expect(receipt.status).to.equal(true);
 
-            const balancePostTransfer = await erc20.balanceOf(accounts[0]);
+            const balancePostTransfer = await erc20.balanceOf(publicOwner);
             expect(balancePostTransfer.toString()).to.equal(expectedBalancePostTransfer.toString());
         });
 
         it('should perform a confidentialTransfer(), with a withdraw proof', async () => {
-            // Convert 100 output notes into 100 ERC20 tokens
+            // Convert tokens into notes, then withdraw those notes
+
+            // convert tokens into notes
+            const depositInputNotes = [];
+            const depositInputOwnerAccounts = [];
+            const depositOutputNotes = await getNotesForAccount(aztecAccount, [50, 50]);
+            const depositPublicValue = 100;
+            const publicValue = depositPublicValue * -1;
+
+            const depositProof = new JoinSplitProof(depositInputNotes, depositOutputNotes, sender, publicValue, publicOwner);
+            const depositData = depositProof.encodeABI(zkAsset.address);
+            const signatures = depositProof.constructSignatures(zkAsset.address, depositInputOwnerAccounts);
+
+            await ace.publicApprove(zkAsset.address, depositProof.hash, depositPublicValue, { from: publicOwner });
+            await zkAsset.methods['confidentialTransfer(bytes,bytes)'](depositData, signatures, {
+                from: sender,
+            });
+
+            const balancePostDeposit = await erc20.balanceOf(publicOwner);
+
+            // withdraw notes into tokens
+            const withdrawInputNotes = depositOutputNotes;
+            const withdrawInputOwnerAccounts = [aztecAccount, aztecAccount];
+            const withdrawOutputNotes = [];
+            const withdrawPublicValue = 100;
+
+            const withdrawAmountBN = new BN(withdrawPublicValue);
+            const expectedBalancePostWithdraw = balancePostDeposit.add(withdrawAmountBN.mul(scalingFactor));
+
+            const withdrawProof = new JoinSplitProof(
+                withdrawInputNotes,
+                withdrawOutputNotes,
+                sender,
+                withdrawPublicValue,
+                publicOwner,
+            );
+            const withdrawData = withdrawProof.encodeABI(zkAsset.address);
+            const withdrawSignatures = withdrawProof.constructSignatures(zkAsset.address, withdrawInputOwnerAccounts);
+
+            await zkAsset.methods['confidentialTransfer(bytes,bytes)'](withdrawData, withdrawSignatures, {
+                from: sender,
+            });
+
+            const balancePostWithdraw = await erc20.balanceOf(publicOwner);
+            expect(balancePostWithdraw.toString()).to.equal(expectedBalancePostWithdraw.toString());
         });
 
         it('should perform a mint operation', async () => {
             // Mint 3 AZTEC notes, worth a total of 300 tokens
+            const newMintCounter = 50;
+            const mintedNoteValues = [20, 30];
+
+            const zeroMintCounterNote = await note.createZeroValueNote();
+            const newMintCounterNote = await note.create(aztecAccount.publicKey, newMintCounter);
+            const mintedNotes = await getNotesForAccount(aztecAccount, mintedNoteValues);
+
+            const proof = new MintProof(zeroMintCounterNote, newMintCounterNote, mintedNotes, sender);
+            const data = proof.encodeABI();
+            const { receipt } = await zkAsset.confidentialMint(MINT_PROOF, data, { from: accounts[0] });
+            expect(receipt.status).to.equal(true);
         });
 
-        it('should perform a confidentialApprove() and allow a confidentialTransferFrom()', async () => {
-            // Call confidentialApprove() on two notes to approve the ZkAsset to spend on user's behalf
+        it('should be able to burn minted value', async () => {
+            // Mint 3 AZTEC notes, worth a total of 300 tokens
+            const newMintCounter = 100;
+            const mintedNoteValues = [80, 20];
+
+            const zeroMintCounterNote = await note.createZeroValueNote();
+            const newMintCounterNote = await note.create(aztecAccount.publicKey, newMintCounter);
+            const mintedNotes = await getNotesForAccount(aztecAccount, mintedNoteValues);
+
+            const proof = new MintProof(zeroMintCounterNote, newMintCounterNote, mintedNotes, sender);
+            const data = proof.encodeABI();
+            await zkAsset.confidentialMint(MINT_PROOF, data, { from: accounts[0] });
+
+            const newBurnCounterNote = await note.create(aztecAccount.publicKey, newMintCounter);
+            const zeroBurnCounterNote = await note.createZeroValueNote();
+
+            const burnProof = new BurnProof(zeroBurnCounterNote, newBurnCounterNote, mintedNotes, sender);
+            const burnData = burnProof.encodeABI(zkAsset.address);
+
+            const { receipt: burnReceipt } = await zkAsset.confidentialBurn(BURN_PROOF, burnData);
+            expect(burnReceipt.status).to.equal(true);
+        });
+
+        it('should delegate note spending control using a confidentialApprove() and allow a confidentialTransferFrom()', async () => {
+            // Call confidentialApprove() on two notes to approve the zkAsset to spend on user's behalf
+            const delegatedAddress = accounts[3];
+
+            const depositInputNotes = [];
+            const depositOutputNotes = await getNotesForAccount(aztecAccount, [50, 10]);
+            const depositPublicValue = -60;
+            const depositInputOwnerAccounts = [];
+
+            const depositProof = new JoinSplitProof(
+                depositInputNotes,
+                depositOutputNotes,
+                sender,
+                depositPublicValue,
+                publicOwner,
+            );
+            const depositData = depositProof.encodeABI(zkAsset.address);
+            const depositSignatures = depositProof.constructSignatures(zkAsset.address, depositInputOwnerAccounts);
+
+            await ace.publicApprove(zkAsset.address, depositProof.hash, depositPublicValue, {
+                from: accounts[0],
+            });
+
+            await zkAsset.methods['confidentialTransfer(bytes,bytes)'](depositData, depositSignatures, {
+                from: accounts[0],
+            });
+
+            const transferInputNotes = depositOutputNotes;
+            const transferOutputNotes = await getNotesForAccount(aztecAccount, [20, 30]);
+            const withdrawalPublicValue = 10;
+
+            const transferProof = new JoinSplitProof(
+                transferInputNotes,
+                transferOutputNotes,
+                delegatedAddress,
+                withdrawalPublicValue,
+                publicOwner,
+            );
+            const transferData = transferProof.encodeABI(zkAsset.address);
+
+            const signature1 = signer.signNoteForConfidentialApprove(
+                zkAsset.address,
+                transferInputNotes[0].noteHash,
+                delegatedAddress,
+                true,
+                aztecAccount.privateKey,
+            );
+            await zkAsset.confidentialApprove(transferInputNotes[0].noteHash, delegatedAddress, true, signature1);
+
+            const signature2 = signer.signNoteForConfidentialApprove(
+                zkAsset.address,
+                transferInputNotes[1].noteHash,
+                delegatedAddress,
+                true,
+                aztecAccount.privateKey,
+            );
+            await zkAsset.confidentialApprove(transferInputNotes[1].noteHash, delegatedAddress, true, signature2);
+
+            await ace.validateProof(JOIN_SPLIT_PROOF, delegatedAddress, transferData, { from: delegatedAddress });
+            const { receipt } = await zkAsset.confidentialTransferFrom(JOIN_SPLIT_PROOF, transferProof.eth.output, {
+                from: delegatedAddress,
+            });
+            expect(receipt.status).to.equal(true);
         });
 
         it('should perform a note registry upgrade', async () => {
+            const zkAssetOwner = accounts[0];
+
+            const existingProxy = await ace.registries(zkAssetOwner);
+            const newFactoryId = factoryHelpers.generateFactoryId(1, 3, 3);
+            const newFactoryContract = await TestFactory.new(ace.address);
+
+            await ace.setFactory(newFactoryId, newFactoryContract.address, { from: sender });
+            console.log('set factory');
+
+            const preUpgradeBehaviour = await factoryContract.getImplementation.call(existingProxy.behaviour);
+
+            await zkAsset.upgradeRegistryVersion(newFactoryId, { from: zkAssetOwner });
+            console.log('performed upgrade registry version');
+
+            const postUpgradeProxy = await ace.registries(zkAssetOwner);
+            expect(postUpgradeProxy.behaviour).to.equal(existingProxy.behaviour);
+
+            const newBehaviourAddress = await newFactoryContract.getImplementation.call(existingProxy.behaviour);
+            expect(newBehaviourAddress).to.not.equal(preUpgradeBehaviour);
         });
     });
 });
