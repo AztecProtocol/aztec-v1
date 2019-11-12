adee5776c4f734e7ff8f053b68ea8114a6bdd893
Malkevych Bohdan <bomalkevych@gmail.com>
2019-11-12 16:23:38 +0200

    fix(extension): added isRequired apiKey

--- packages/extension/src/utils/approveGSNFunction.js
+++ packages/extension/src/utils/approveGSNFunction.js
@@ -3,25 +3,25 @@ import axios from 'axios';
 const SIGNING_PROVIDER = 'https://bv9t4hwozi.execute-api.us-east-1.amazonaws.com';
 
 export default async ({
-        from, to, encodedFunctionCall, txFee, gasPrice, gas, nonce, relayerAddress, relayHubAddress,
+    from,
+    to,
+    encodedFunctionCall,
+    txFee,
+    gasPrice,
+    gas,
+    nonce,
+    relayerAddress,
+    relayHubAddress,
 }) => {
-    let response;
     const data = {
         from, to, encodedFunctionCall, txFee, gasPrice, gas, nonce, relayerAddress, relayHubAddress,
     };
 
-    console.log(`Lambda payload: ${JSON.stringify(data)}`);
+    const response = await axios.post(`${SIGNING_PROVIDER}/Stage/sign-data`, {
+        data,
+        apiKey: 'aztecprotocol_api_key',
+    });
 
-    try {
-        response = await axios.post(`${SIGNING_PROVIDER}/Stage/sign-data`, {
-            data,
-            apiKey: '1234567API_KEY',
-        });
-
-        console.log(`Lambda response: ${JSON.stringify(response)}`);
-    } catch (error) {
-        console.error(error);
-    }
-    
+    console.log(`Lambda response: ${JSON.stringify(response)}`);
     return response.data.data.dataSignature;
 };
