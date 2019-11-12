d111383becb4719757b3a7eb5eae6f62435c1f47
Leila Wang <leizciw@gmail.com>
2019-11-12 12:09:55 +0000

    refactor(extension): replace CombinedView with AnimatedTransaction

--- packages/extension/src/ui/pages/Login.jsx
+++ packages/extension/src/ui/pages/Login.jsx
@@ -1,39 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import returnAndClose from '~ui/helpers/returnAndClose';
-import CombinedViews from '~ui/views/handlers/CombinedViews';
+import AnimatedTransaction from '~ui/views/handlers/AnimatedTransaction';
 import LoginTransaction from '~ui/views/LoginTransaction';
 
-const Steps = [
-    LoginTransaction,
+const steps = [
+    {
+        content: LoginTransaction,
+    },
 ];
 
 const Login = ({
-    actionId,
     currentAccount,
-    goToPage,
 }) => (
-    <CombinedViews
-        Steps={Steps}
+    <AnimatedTransaction
+        steps={steps}
         initialData={{
             currentAccount,
         }}
-        onExit={actionId
-            ? returnAndClose
-            : ({ loggedIn }) => goToPage(loggedIn ? 'account' : 'account.restore')}
+        onExit={returnAndClose}
     />
 );
 
 Login.propTypes = {
-    actionId: PropTypes.string,
     currentAccount: PropTypes.shape({
         address: PropTypes.string.isRequired,
     }).isRequired,
-    goToPage: PropTypes.func.isRequired,
-};
-
-Login.defaultProps = {
-    actionId: '',
 };
 
 export default Login;
