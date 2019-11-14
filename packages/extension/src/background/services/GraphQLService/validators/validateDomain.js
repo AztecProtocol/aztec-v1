e7c2ba413374effca339935b88d2ae320a82c191
Leila Wang <leizciw@gmail.com>
2019-11-14 17:50:09 +0000

    fix(extension): don't block requests from ui

--- packages/extension/src/background/services/GraphQLService/validators/validateDomain.js
+++ packages/extension/src/background/services/GraphQLService/validators/validateDomain.js
@@ -1,12 +1,19 @@
 import {
     permissionError,
 } from '~utils/error';
+import urls from '~/config/urls';
 import AuthService from '~background/services/AuthService';
+
+const {
+    origin: uiSourceOrigin,
+} = new URL(urls.ui);
 
 export default async function validateDomain(_, args) {
     const { domain } = args;
 
-    const registeredDomain = await AuthService.getRegisteredDomain(domain);
+    const registeredDomain = domain === uiSourceOrigin
+        ? domain
+        : await AuthService.getRegisteredDomain(domain);
 
     if (!registeredDomain) {
         return permissionError('domain.not.register', {
