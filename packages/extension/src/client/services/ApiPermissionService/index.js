03c582c65c3eeed861c3ec9380bd5952eada528b
Leila Wang <leizciw@gmail.com>
2019-11-18 17:56:53 +0000

    fix(extension): init contracts before checking permission

--- packages/extension/src/client/services/ApiPermissionService/index.js
+++ packages/extension/src/client/services/ApiPermissionService/index.js
@@ -1,7 +1,9 @@
 import ensurePermission from './utils/ensurePermission';
+import setContractConfigs from './utils/setContractConfigs';
 import generateApis from './utils/generateApis';
 
 export default {
     ensurePermission,
+    setContractConfigs,
     generateApis,
 };
--- packages/extension/src/client/services/ApiPermissionService/utils/generateApis.js
+++ packages/extension/src/client/services/ApiPermissionService/utils/generateApis.js
@@ -1,14 +1,6 @@
-import ApiError from '~client/utils/ApiError';
 import Web3Service from '~/client/services/Web3Service';
-import ZkAsset from '~contracts/ZkAsset';
-import ERC20Mintable from '~contracts/ERC20Mintable';
 import assetFactory from '~client/apis/assetFactory';
 import noteFactory from '~client/apis/noteFactory';
-
-const clientContracts = [
-    'ACE',
-    'AZTECAccountRegistry',
-];
 
 const availableWeb3Apis = [
     'useContract',
@@ -16,41 +8,7 @@ const availableWeb3Apis = [
     'deploy',
 ];
 
-export default function generateApis({
-    contractsConfigs,
-}) {
-    const {
-        networkId,
-    } = Web3Service;
-
-    contractsConfigs
-        .filter(({ config }) => clientContracts.indexOf(config.contractName) >= 0)
-        .forEach(({
-            config,
-            address,
-        }) => {
-            if (!address) {
-                throw new ApiError('input.contract.address.empty', {
-                    messageOptions: {
-                        contractName: config.contractName,
-                    },
-                    networkId,
-                    contractName: config.contractName,
-                });
-            }
-            Web3Service.registerContract(config, {
-                address,
-            });
-        });
-
-    Web3Service.registerInterface(ERC20Mintable, {
-        name: 'ERC20',
-    });
-
-    Web3Service.registerInterface(ZkAsset, {
-        name: 'ZkAsset',
-    });
-
+export default function generateApis() {
     const web3 = {
         account: () => ({
             ...Web3Service.account,
