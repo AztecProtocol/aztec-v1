1946646f92dd58082901b232382d9327733a892b
thomas-waite <thomas.waite4@gmail.com>
2019-11-25 11:35:48 +0000

    refactor(integration-test): add improved error feedback

--- packages/integration-test/src/setup.js
+++ packages/integration-test/src/setup.js
@@ -17,22 +17,20 @@ class Setup {
      *
      * Can be used to setup a local development testing environment for an integration test
      *
-     * @param {String[]} contractsToDeploy - array of the names of contracts to deploy
-     * @param {String} NETWORK - ID of the network to set the integration test up for.
-     *                           If deploying and testing locally, this will typically be a
-     *                           large number.
+     * @param {Object} config - config used to modify the test environment setup
+     * @param {String} accounts - Ethereum addresses to be used in the test
      */
     constructor(config, accounts) {
+        this.config = config;
         this.NETWORK = config.NETWORK;
+        this.runAdjustSupplyTests = config.runAdjustSupplyTests;
         this.contractsToDeploy = config.contractsToDeploy;
+
         this.provider = web3.currentProvider;
         this.accounts = accounts;
-        this.config = config;
-        this.runAdjustSupplyTests = config.runAdjustSupplyTests;
 
-        const testNetworks = ['ropsten', 'rinkeby', 'kovan'];
-
-        if (testNetworks.includes(this.NETWORK)) {
+        const networks = ['ropsten', 'rinkeby', 'kovan', 'mainnet'];
+        if (networks.includes(this.NETWORK)) {
             console.log('Configuring integration test for', this.NETWORK, 'network');
             this.networkId = networkIDs[capitaliseFirstChar(this.NETWORK)];
             this.getAddresses();
@@ -41,7 +39,7 @@ class Setup {
             this.getTransactionTestingAddresses();
         } else {
             throw new AztecError(codes.UNSUPPORTED_NETWORK, {
-                message: 'Network not recognised, please input: `ropsten`, `rinkeby` or `kovan`',
+                message: 'Network not recognised, please input: `ropsten`, `rinkeby`, `kovan` or `mainnet`',
                 inputNetwork: this.NETWORK,
             });
         }
@@ -51,7 +49,14 @@ class Setup {
      * @method getAddresses() - get the addresses for the desired contracts to deploy
      */
     getAddresses() {
-        this.contractAddresses = getContractAddressesForNetwork(this.networkId);
+        try {
+            this.contractAddresses = getContractAddressesForNetwork(this.networkId);
+        } catch (err) {
+            throw new AztecError(codes.NO_CONTRACT_ADDRESSES, {
+                message: 'No contract addresses exist for the given network ID in @aztec/contract-addresses',
+                networkId: this.networkId,
+            });
+        }
     }
 
     /**
@@ -68,13 +73,39 @@ class Setup {
      */
     getContractPromise(nameOfContract) {
         const contractObject = {};
+        let extractedContractArtifact;
+        let extractedContractAddress;
+
+        try {
+            extractedContractArtifact = contractArtifacts[nameOfContract];
+        } catch (err) {
+            throw new AztecError(codes.NO_CONTRACT_ARTIFACT, {
+                message: 'Requested contract does not exist in @aztec/contract-artifacts',
+                requestedContract: nameOfContract,
+            });
+        }
+
+        try {
+            extractedContractAddress = this.contractAddresses[nameOfContract];
+        } catch (err) {
+            throw new AztecError(codes.NO_CONTRACT_ADDRESS, {
+                message: 'Requested contract does not have a deployed address in @aztec/contract-addresses',
+                requestedContract: nameOfContract,
+            });
+        }
+
         contractObject[nameOfContract] = async () => {
-            const truffleRepresentation = this.getTruffleContractRepresentation(contractArtifacts[nameOfContract]);
-            return truffleRepresentation.at(this.contractAddresses[nameOfContract]);
+            const truffleRepresentation = this.getTruffleContractRepresentation(extractedContractArtifact);
+            return truffleRepresentation.at(extractedContractAddress);
         };
         return contractObject;
     }
 
+    /**
+     * @method getTruffleContractRepresentation - get the Truffle contract representation of a contract, for a specific
+     * abi, bytecode and provider
+     * @param {Object} contractArtifact - truffle artifact for which the Truffle contract wrapper should be generated
+     */
     getTruffleContractRepresentation(contractArtifact) {
         const truffleRepresentation = TruffleContract({ abi: contractArtifact.abi, bytecode: contractArtifact.bytecode });
         truffleRepresentation.setProvider(this.provider);
@@ -87,7 +118,7 @@ class Setup {
      * @returns {Object} Truffle contracts, representing the contract deployed at a specific address
      */
     async getContracts() {
-        const contractObject = this.contractPromises.reduce(
+        const allContractObjects = this.contractPromises.reduce(
             async (previousPromiseAccumulator, currentContractPromise, currentIndex) => {
                 const accumulator = await previousPromiseAccumulator;
                 const deployedContract = await currentContractPromise[this.contractsToDeploy[currentIndex]]();
@@ -97,8 +128,8 @@ class Setup {
             {},
         );
 
-        this.contractObject = contractObject;
-        return contractObject;
+        this.allContractObjects = allContractObjects;
+        return allContractObjects;
     }
 
     /**
@@ -118,17 +149,19 @@ class Setup {
     }
 
     /**
-     * @method fundPublicOwnerAccount - fund the publicOwner account with ERC20 tokens if required
+     * @method fundPublicOwnerAccount - fund the publicOwner account with ERC20 tokens, if the balance is less
+     * than the number required for the test suite to run.
+     *
+     * Num of tokens required for test suite to run given by numTestTokens on the config object.
      *
      * @param scalingFactor - factor to convert between AZTEC note value and ERC20 token value
      *
      */
     async fundPublicOwnerAccount(scalingFactor) {
-        const { ACE: ace, ERC20Mintable: erc20 } = await this.contractObject;
+        const { ACE: ace, ERC20Mintable: erc20 } = await this.allContractObjects;
 
         const publicOwnerBalance = await erc20.balanceOf(this.publicOwner);
         if (publicOwnerBalance < this.config.numTestTokens.mul(scalingFactor)) {
-            console.log('inside if statement');
             const tokensTransferred = new BN(this.config.numTestTokens);
             await erc20.mint(this.publicOwner, scalingFactor.mul(tokensTransferred), this.opts);
             await erc20.approve(ace.address, scalingFactor.mul(tokensTransferred), this.opts);
