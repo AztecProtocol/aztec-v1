7e928c37899dac43bef801b78130fbdd895d7239
Malkevych Bohdan <bomalkevych@gmail.com>
2019-11-25 09:22:08 +0200

    fix(extension): fixed lambda

--- packages/aws-lambda/signer/signerFunction/utils/signatureHash.js
+++ packages/aws-lambda/signer/signerFunction/utils/signatureHash.js
@@ -1,5 +1,5 @@
 const web3Service = require('../services/Web3Service');
 
 module.exports = (txSignature) => {
-    return web3Service.web3.sha3(txSignature);
+    return web3Service.web3.utils.keccak256(txSignature);
 };
