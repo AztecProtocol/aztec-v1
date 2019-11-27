50fee47eef1cf6f7551b7853d65a203911643d89
Joe Andrews <joe@fuuzik.com>
2019-11-27 16:36:10 +0000

    feat(extension): remove stray console.logs

--- packages/extension/migrations/13_AZTEC_ACCOUNT_REGISTRY.js
+++ packages/extension/migrations/13_AZTEC_ACCOUNT_REGISTRY.js
@@ -11,8 +11,8 @@ module.exports = deployer => deployer.deploy(
     AZTECAccountRegistry,
     ACE.address,
     // TODO change to be from env
-    // '0x6794d16143e537a51d6745D3ae6bc99502b4331C',
-    '0x24b934a7d1dd72bd8645a4958b0ff46bd29a5845'
+    '0x6794d16143e537a51d6745D3ae6bc99502b4331C',
+    // '0x24b934a7d1dd72bd8645a4958b0ff46bd29a5845'
 ).then(async (contract) => {
     const WEB3_PROVIDER_URL = 'http://127.0.0.1:8545';
     const web3 = new Web3(WEB3_PROVIDER_URL);
