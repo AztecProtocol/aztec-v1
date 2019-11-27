a866f3ce360182171e423c8bcc4f70819058b508
Leila Wang <leizciw@gmail.com>
2019-11-27 19:01:38 +0000

    feat(extension): display larger number or fraction properly

--- packages/extension/src/ui/utils/formatNumber.js
+++ packages/extension/src/ui/utils/formatNumber.js
@@ -1,14 +1,52 @@
-import numeral from 'numeral';
+const isFloatValue = value => `${value}`.indexOf('.') >= 0;
 
-export default function formatNumber(value, decimal) {
-    let format = '0,0';
-    if (decimal !== 0) {
-        const hasDecimal = (value | 0) !== value; // eslint-disable-line no-bitwise
-        if (hasDecimal) {
-            const trailingZero = '0'.padEnd((decimal || `${value}`.length) - 1, '0');
-            format = `0,0.0[${trailingZero}]`;
-        }
+const shiftRight10 = (intValue, count) => {
+    const strVal = `${intValue}`;
+    const cutAt = Math.max(0, strVal.length - count);
+    const intStr = strVal.substr(0, cutAt) || '0';
+    const fractionStr = strVal.length > count
+        ? strVal.substr(cutAt)
+        : strVal.padStart(count, '0');
+    return fractionStr
+        ? `${intStr}.${fractionStr}`
+        : intStr;
+};
+
+const formatInt = (intValue) => {
+    const intStr = `${intValue}`;
+    const segCount = Math.ceil(intStr.length / 3);
+    const segments = [];
+    for (let i = 0; i < segCount; i += 1) {
+        const end = intStr.length - (3 * i);
+        const start = Math.max(0, end - 3);
+        segments[segCount - i - 1] = intStr.substring(start, end);
+    }
+    return segments.join(',');
+};
+
+const formatFraction = (fractionValue, decimals) => {
+    if (!fractionValue
+        || decimals === 0
+        || fractionValue.match(/^0{0,}$/)
+    ) {
+        return '';
+    }
+    return fractionValue
+        .substr(0, decimals)
+        .replace(/0{1,}$/, '');
+};
+
+export default function formatNumber(value, decimals) {
+    let floatValue = value;
+    if (decimals > 0 && !isFloatValue(floatValue)) {
+        floatValue = shiftRight10(floatValue, decimals);
     }
 
-    return numeral(value).format(format);
+    const [intValue, fractionValue] = `${floatValue}`.split('.');
+    const intStr = formatInt(intValue);
+    const fractionStr = formatFraction(fractionValue, decimals);
+
+    return fractionStr
+        ? `${intStr}.${fractionStr}`
+        : intStr;
 }
