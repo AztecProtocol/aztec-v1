ef82ecca6952b24ad512c1273f1d46b3291a7da7
Joe Andrews <joe@fuuzik.com>
2019-11-28 11:22:58 +0000

    fix(extension): fix for no api key

--- packages/extension/src/utils/checkQuota.js
+++ packages/extension/src/utils/checkQuota.js
@@ -8,19 +8,21 @@ export default async () => {
     const apiKey = await get('apiKey');
     const networkId = await get('networkId');
     // we need to check the quota
-    const lambdaResponse = await window.fetch(`${SIGNING_PROVIDER}/Stage/${parseInt(networkId)}/${apiKey}`, {
+    if (apiKey) {
+        const lambdaResponse = await window.fetch(`${SIGNING_PROVIDER}/Stage/${parseInt(networkId)}/${apiKey}`, {
 
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-    });
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+        });
 
-    const {
-        data: {
-            hasFreeTransactions,
-        },
-    } = await lambdaResponse.json();
+        const {
+            data: {
+                hasFreeTransactions,
+            },
+        } = await lambdaResponse.json();
+    }
 
-    return hasFreeTransactions;
+    return !!apiKey && hasFreeTransactions;
 };
