265b3c8e5711d965532d12e1a54982e0c17f6a92
Arnaud Schenk <arnaud@creditmint.io>
2019-12-02 17:39:17 +0000

    build(aztec.js): remove print

--- packages/contract-addresses/build.sh
+++ packages/contract-addresses/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo $CHANGED_MODULES;
-
-
 if [ "$CI" = true ] && [ ! -d "$(pwd)/lib" ] || [[ "contract-addresses" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
     echo "Re-building dev bundle";
     yarn clean && babel --copy-files --out-dir ./lib --root-mode upward ./src;
