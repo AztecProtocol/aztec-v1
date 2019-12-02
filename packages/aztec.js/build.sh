f4e33feca761c16b99c48f3fc36e21236248e0bb
Arnaud Schenk <arnaud@creditmint.io>
2019-12-02 17:12:56 +0000

    build(aztec.js): fix dist path

--- packages/aztec.js/build.sh
+++ packages/aztec.js/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 echo $CHANGED_MODULES;
+
 if [ "$CI" = true ]; then
     echo "CI is true";
 fi
 
-if [ ! -d "dist" ]; then
+if [ ! -d "$(pwd)/dist" ]; then
     echo "dist does not exist";
 fi
 
@@ -15,12 +16,12 @@ fi
 
 
 if [ "$PROD" = true ]; then
-    if [ "$CI" = true ] && [ ! -d "dist" ] || [[ "aztec.js" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
+    if [ "$CI" = true ] && [ ! -d "$(pwd)/dist" ] || [[ "aztec.js" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
     echo "Re-building prod bundle";
     ./node_modules/.bin/webpack --config ./webpack.prod.js;
     fi
 else
-    if [ "$CI" = true ] && [ ! -d "dist" ] || [[ "aztec.js" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
+    if [ "$CI" = true ] && [ ! -d "$(pwd)/dist" ] || [[ "aztec.js" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
     echo "Re-building dev bundle";
     ./node_modules/.bin/webpack --config ./webpack.dev.js;
     fi
