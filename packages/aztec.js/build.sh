0b47bf88029d570bfadca7908c3d32112496f9ac
Arnaud Schenk <arnaud@creditmint.io>
2019-12-02 14:02:04 +0000

    build(aztec.js): get cache key to have commit value

--- packages/aztec.js/build.sh
+++ packages/aztec.js/build.sh
@@ -1,5 +1,6 @@
 echo $CHANGED_MODULES;
 
 if [ "$CI" = true ] && [ ! -d "dist" ] || [[ "aztec.js" =~ $CHANGED_MODULES ]]; then
+  echo "Re-building bundle";
   ./node_modules/.bin/webpack --config ./webpack.dev.js;
 fi
