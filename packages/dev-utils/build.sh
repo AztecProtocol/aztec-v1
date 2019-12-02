6a7d71e3d08c4a55494f991e4807f8bd9075ef2d
Arnaud Schenk <arnaud@creditmint.io>
2019-12-02 17:22:41 +0000

    build(aztec.js): fix dist path

--- packages/dev-utils/build.sh
+++ packages/dev-utils/build.sh
@@ -3,7 +3,7 @@
 echo $CHANGED_MODULES;
 
 
-if [ "$CI" = true ] && [ ! -d "lib" ] || [[ "dev-utils" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
+if [ "$CI" = true ] &&  [ ! -d "$(pwd)/lib" ] || [[ "dev-utils" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
     echo "Re-building dev bundle";
     yarn clean && babel --copy-files --out-dir ./lib --root-mode upward ./src;
 fi
