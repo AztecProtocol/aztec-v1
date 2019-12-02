7c427c7f221e9f2275e4ae2fc5420eb0fa0dc386
Arnaud Schenk <arnaud@creditmint.io>
2019-12-02 15:44:47 +0000

    build(): add packages to cache

---
+++ packages/contract-addresses/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo $CHANGED_MODULES;
+
+
+if [ "$CI" = true ] && [ ! -d "lib" ] || [[ "contract-addresses" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
+    echo "Re-building dev bundle";
+    yarn clean && babel --copy-files --out-dir ./lib --root-mode upward ./src;
+fi
