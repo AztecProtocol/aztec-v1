7d80a97e3a04f1992109cbd186007e766a516c50
Arnaud Schenk <arnaud@creditmint.io>
2019-12-02 14:44:25 +0000

    build(aztec.js): attempt to remove parse error from script

--- packages/aztec.js/build.sh
+++ packages/aztec.js/build.sh
@@ -2,7 +2,7 @@
 
 echo $CHANGED_MODULES;
 
-if [ "$CI" = true ] && [ ! -d "dist" ] || [[ "aztec.js" =~ $CHANGED_MODULES ]]; then
+if [ "$CI" = true ] && [ ! -d "dist" ] || [ "aztec.js" =~ $CHANGED_MODULES ]; then
   echo "Re-building bundle";
   ./node_modules/.bin/webpack --config ./webpack.dev.js;
 fi
