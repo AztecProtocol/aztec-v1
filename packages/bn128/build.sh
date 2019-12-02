bbf930e3cfa76d3f81a8ebef7709ad817abb2145
Arnaud Schenk <arnaud@creditmint.io>
2019-12-02 18:07:36 +0000

    build(aztec.js): circleci does not pass env variables between steps

--- packages/bn128/build.sh
+++ packages/bn128/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CHANGED_MODULES=$(cat /tmp/changed-modules);
+
 if [ "$PROD" = true ]; then
     if [ "$CI" = true ] && [ ! -d "$(pwd)/dist" ] || [[ "bn128" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
     echo "Re-building prod bundle";
