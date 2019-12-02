3a76ec415ed9f48e35d0f77ad7f1d182b626742e
Arnaud Schenk <arnaud@creditmint.io>
2019-12-02 15:26:19 +0000

    build(): add prod build script, add caching build to bn128

--- packages/bn128/build.sh
+++ packages/bn128/build.sh
@@ -3,12 +3,12 @@
 echo $CHANGED_MODULES;
 
 if [ "$PROD" = true ]; then
-    if [ "$CI" = true ] && [ ! -d "dist" ] || [[ "aztec.js" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
+    if [ "$CI" = true ] && [ ! -d "dist" ] || [[ "bn128" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
     echo "Re-building prod bundle";
     ./node_modules/.bin/webpack --config ./webpack.prod.js;
     fi
 else
-    if [ "$CI" = true ] && [ ! -d "dist" ] || [[ "aztec.js" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
+    if [ "$CI" = true ] && [ ! -d "dist" ] || [[ "bn128" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
     echo "Re-building dev bundle";
     ./node_modules/.bin/webpack --config ./webpack.dev.js;
     fi
