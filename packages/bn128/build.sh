3a8e4d5994a099bf37581125f4916054e284e157
Arnaud Schenk <arnaud@creditmint.io>
2019-12-02 15:30:49 +0000

    build(bn128): fix build script to use correct webpack

--- packages/bn128/build.sh
+++ packages/bn128/build.sh
@@ -5,11 +5,11 @@ echo $CHANGED_MODULES;
 if [ "$PROD" = true ]; then
     if [ "$CI" = true ] && [ ! -d "dist" ] || [[ "bn128" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
     echo "Re-building prod bundle";
-    ./node_modules/.bin/webpack --config ./webpack.prod.js;
+    webpack --config ./webpack.prod.js;
     fi
 else
     if [ "$CI" = true ] && [ ! -d "dist" ] || [[ "bn128" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
     echo "Re-building dev bundle";
-    ./node_modules/.bin/webpack --config ./webpack.dev.js;
+    webpack --config ./webpack.dev.js;
     fi
 fi
