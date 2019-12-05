fdc91716cf5ab0129f8f97803f554cf0757768b3
Arnaud Schenk <arnaud@creditmint.io>
2019-12-05 21:13:11 +0000

    feat(note-access): simplify metadata transformation fns

--- packages/note-access/src/metadata/toString.js
+++ packages/note-access/src/metadata/toString.js
@@ -1,39 +1,25 @@
-import { toChecksumAddress } from 'web3-utils';
-import { DYNAMIC_VAR_CONFIG_LENGTH, MIN_BYTES_VAR_LENGTH } from '../config/constants';
+import { DYNAMIC_VAR_CONFIG_LENGTH } from '../config/constants';
 import config from '../config/metadata';
 
-const toConfigVar = (num) => num.toString(16).padStart(DYNAMIC_VAR_CONFIG_LENGTH, '0');
-const ensureMinVarSize = (str, len) => str.padStart(Math.max(len || str.length, MIN_BYTES_VAR_LENGTH), '0');
+const to32ByteOffset = (num) => num.toString(16).padStart(DYNAMIC_VAR_CONFIG_LENGTH, '0');
 
-export default function toString(metadataObj) {
-    const metadata = {};
-    let accumOffset = 0;
-    config.forEach(({ name, length, startAt }) => {
-        let data = '';
-        if (metadataObj[name] || (!startAt && length)) {
-            const dataArr = Array.isArray(metadataObj[name]) ? metadataObj[name] : [metadataObj[name] || ''];
-            data = dataArr
-                .map((v) => (name === 'addresses' && toChecksumAddress(v)) || v)
-                .map((v) => `${v}`.replace(/^0x/, ''))
-                .map((v) => ensureMinVarSize(v, length))
-                .join('');
+export default function toString(metadata) {
+    let totalDataLength = 0;
+    const stringifiedMetadata = config.reduce((acc, type) => {
+        const { name, length, startAt: offset } = type;
+        acc[name] = type._toString(metadata[name]);
+
+        // Need to find and set the offset of dynamic types.
+        if (offset) {
+            // A character is half a byte
+            acc[offset] = to32ByteOffset(totalDataLength / 2);
         }
 
-        if (startAt) {
-            // a character is half a bytes
-            // the total length should always be even
-            metadata[startAt] = toConfigVar(accumOffset / 2);
+        totalDataLength += (offset) ? acc[name].length : length;
+        return acc;
+    }, {});
 
-            const numberOfVars =
-                length !== undefined ? data.length / Math.max(length, MIN_BYTES_VAR_LENGTH) : Math.min(1, data.length);
-            data = `${toConfigVar(numberOfVars)}${data}`;
-        }
-
-        metadata[name] = data;
-        accumOffset += startAt ? data.length : length;
-    });
-
-    const str = config.map(({ name }) => metadata[name]).join('');
+    const str = config.map(({ name }) => stringifiedMetadata[name]).join('');
 
     return `0x${str}`;
 }
