cc450048a6ffcc21a4bc332201375b8f3720e8c7
Leila Wang <leizciw@gmail.com>
2019-12-06 11:18:47 +0000

    feat(extension): reduce delay time for closing popup

--- packages/extension/src/ui/helpers/returnAndClose.js
+++ packages/extension/src/ui/helpers/returnAndClose.js
@@ -1,7 +1,10 @@
 import ConnectionService from '~uiModules/services/ConnectionService';
 import closeWindow from '~ui/utils/closeWindow';
+import {
+    closeWindowDelay,
+} from '~/ui/config/settings';
 
-export default function returnAndClose(data, closeDelay = 1000) {
+export default function returnAndClose(data, closeDelay = closeWindowDelay) {
     ConnectionService.returnToClient(data);
     closeWindow(closeDelay);
 }
