5276e1af6b481792e0a8822d5d14a909ea35af1c
Joe Andrews <joe@fuuzik.com>
2019-12-10 18:18:27 +0000

    feat(extension): update migrations for ganache

--- packages/extension/migrations/13_AZTEC_ACCOUNT_REGISTRY.js
+++ packages/extension/migrations/13_AZTEC_ACCOUNT_REGISTRY.js
@@ -10,7 +10,7 @@ module.exports = (deployer, network) => deployer.deploy(
     AZTECAccountRegistry,
     ACE.address,
     // TODO change to be from env
-    '0x6794d16143e537a51d6745D3ae6bc99502b4331C', // RINKEBY
+    network === 'development' ?: '0x24b934a7d1dd72bd8645a4958b0ff46bd29a5845' '0x6794d16143e537a51d6745D3ae6bc99502b4331C', // RINKEBY
     // '0x24b934a7d1dd72bd8645a4958b0ff46bd29a5845'
 ).then(async (contract) => {
 
