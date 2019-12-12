d3930cfc08abd126ce253e591d88c038a39750fd
Malkevych Bohdan <bomalkevych@gmail.com>
2019-12-12 13:09:26 +0200

    fix(extension): minor improvements

--- packages/aws-lambda/signer/signerFunction/config/contracts.js
+++ packages/aws-lambda/signer/signerFunction/config/contracts.js
@@ -8,10 +8,14 @@ const AZTECAccountRegistryGSNConfig = {
     },
     config: AZTECAccountRegistryGSN,
     networks: {
+        1: process.env.MAIN_AZTECAccountRegistryGSN,
+        3: process.env.ROPSTEN_AZTECAccountRegistryGSN,
         4: process.env.RINKEBY_AZTECAccountRegistryGSN,
+        5: process.env.GOERLI_AZTECAccountRegistryGSN,
+        42: process.env.KOVAN_AZTECAccountRegistryGSN,
     },
 };
 
 module.exports = {
     AZTECAccountRegistryGSNConfig,
-}
+};
