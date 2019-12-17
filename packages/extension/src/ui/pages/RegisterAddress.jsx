f6e33505187925fce5c48b9d62133994de6aebe9
Leila Wang <leizciw@gmail.com>
2019-12-17 12:02:21 +0000

    fix(extension): don't register via gsn if account is empty in ganache

--- packages/extension/src/ui/pages/RegisterAddress.jsx
+++ packages/extension/src/ui/pages/RegisterAddress.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import AnimatedTransaction from '~ui/views/handlers/AnimatedTransaction/index';
 import {
     gsnConfigShape,
 } from '~ui/config/propTypes';
+import {
+    getNetworkName,
+} from '~/utils/network';
+import Web3Service from '~/helpers/Web3Service';
+import AnimatedTransaction from '~ui/views/handlers/AnimatedTransaction';
 import { linkAccountSteps } from '~ui/config/steps';
-
 
 const RegisterAddress = ({
     currentAccount,
+    linkedPublicKey,
     initialStep,
     initialData,
     gsnConfig,
 }) => {
     const {
         isGSNAvailable,
-        proxyContract,
     } = gsnConfig;
-    const steps = isGSNAvailable ? linkAccountSteps.gsn : linkAccountSteps.metamask;
+    // send via gsn will not work if there is no linkedPublicKey on chain when using ganache
+    const steps = isGSNAvailable
+        && (getNetworkName(Web3Service.networkId) !== 'ganache' || linkedPublicKey)
+        ? linkAccountSteps.gsn
+        : linkAccountSteps.metamask;
+
     return (
         <AnimatedTransaction
             steps={steps}
@@ -29,19 +37,22 @@ const RegisterAddress = ({
         />
     );
 };
+
 RegisterAddress.propTypes = {
+    initialStep: PropTypes.number,
+    initialData: PropTypes.object, // eslint-disable-line react/forbid-prop-types
     currentAccount: PropTypes.shape({
         address: PropTypes.string.isRequired,
         linkedPublicKey: PropTypes.string.isRequired,
     }).isRequired,
-    initialStep: PropTypes.number,
-    initialData: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+    linkedPublicKey: PropTypes.string,
     gsnConfig: gsnConfigShape.isRequired,
 };
 
 RegisterAddress.defaultProps = {
     initialStep: 0,
     initialData: {},
+    linkedPublicKey: '',
 };
 
 export default RegisterAddress;
