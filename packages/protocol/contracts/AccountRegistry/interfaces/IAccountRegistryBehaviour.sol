228d621dd448f90b565034dfb6b28242f6e8c330
thomas-waite <thomas.waite4@gmail.com>
2019-12-17 13:35:39 +0000

    feat(protocol): add interfaces for AccountRegistry contracts

---
+++ packages/protocol/contracts/AccountRegistry/interfaces/IAccountRegistryBehaviour.sol
@@ -0,0 +1,36 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+
+interface IAccountRegistryBehaviour {
+
+    struct AZTECAccount {
+        address account;
+        bytes linkedPublicKey;
+    }
+
+    function registerAZTECExtension(
+        address _account,
+        bytes calldata _linkedPublicKey,
+        bytes calldata _spendingPublicKey,
+        bytes calldata _signature
+    ) external;
+
+
+    event RegisterExtension(
+        address indexed account,
+        bytes linkedPublicKey,
+        bytes spendingPublicKey 
+    );
+
+    event LogAddress(
+        address account
+    );
+
+    event LogString(
+        string message
+    );
+
+    event LogBytes(
+        bytes32 sig
+    );
+}
