5ab41f9d6d3e5ec3fbf604d2792546f93f7541e7
Leila Wang <leizciw@gmail.com>
2019-12-17 14:48:26 +0000

    refactor(extension): remove outdated tests

--- packages/extension/src/background/utils/__mocks__/connectionUtils.js
+++
@@ -1,7 +0,0 @@
-export const updateActionState = jest.fn();
-
-export const openPopup = jest.fn();
-export const addDomainData = jest.fn();
-export const handleQuery = jest.fn();
-export const generateResponseCode = jest.fn();
-export const generateResponseError = jest.fn();
