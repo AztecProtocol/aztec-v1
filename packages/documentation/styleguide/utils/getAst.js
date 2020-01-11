8faf7125d8bf757b452e399a1f6cde061f94dddc
thomas-waite <thomas.waite4@gmail.com>
2020-01-11 15:52:33 +0000

    feat(extension): compile code in in-page console

---
+++ packages/documentation/styleguide/utils/getAst.js
@@ -0,0 +1,20 @@
+// @flow
+import { Parser } from 'acorn';
+
+export const ACORN_OPTIONS = {
+  ecmaVersion: 2019,
+  sourceType: 'module',
+};
+
+/**
+ * Parse source code with Acorn and return AST, returns undefined in case of errors
+ */
+export default function getAst(code) {
+  try {
+    return Parser.parse(code, {
+      ...ACORN_OPTIONS,
+    });
+  } catch (err) {
+    return undefined;
+  }
+}
