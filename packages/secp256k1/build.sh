0a3e03e944f9076debabeb849a599a329b4715c4
Arnaud Schenk <arnaud@creditmint.io>
2020-01-14 14:01:21 +0000

    feat(): make better use of wsrun for skipping build pipeline steps

--- packages/secp256k1/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CHANGED_MODULES=$(cat /tmp/changed-modules);
-
-if [ "$CI" = true ] && [ ! -d "$(pwd)/lib" ] || [[ "secp256k1" =~ $CHANGED_MODULES ]] || [ ! "$CI" = true ]; then
-    echo "Re-building dev bundle";
-    yarn clean && babel --copy-files --out-dir ./lib --root-mode upward ./src;
-fi
