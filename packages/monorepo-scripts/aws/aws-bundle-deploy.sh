567d2dcc864e28a4c57c0e05e3d385a523f345d0
Arnaud Schenk <arnaud@creditmint.io>
2020-01-23 10:48:42 +0000

    feat(sdk): changes to scripts

--- packages/monorepo-scripts/aws/aws-bundle-deploy.sh
+++ packages/monorepo-scripts/aws/aws-bundle-deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 PACKAGES_TO_DEPLOY='extension,sdk'
-BUILD_FOLDER=''
 
 CWD_PATH=$(pwd);
+BUILD_PATH="$CWD_PATH/build"
 PACKAGE_NAME=$(echo $CWD_PATH | rev | cut -d'/' -f1 | rev)
 
 if [[ $PACKAGES_TO_DEPLOY == *$PACKAGE_NAME* ]]; then
@@ -11,13 +11,12 @@ if [[ $PACKAGES_TO_DEPLOY == *$PACKAGE_NAME* ]]; then
     BUCKET="s3://$PACKAGE_NAME.aztecprotocol.com";
     BUCKET_PATH="$1";
 
-    # IS_EMPTY=$(aws s3 ls "$BUCKET/$BUCKET_PATH/");
-    # if [[ ! -z  "$IS_EMPTY" ]]; then
-    #     # exit 1;
-    # fi
+    IS_EMPTY=$(aws s3 ls "$BUCKET/$BUCKET_PATH/");
+    if [[ ! -z  "$IS_EMPTY" ]]; then
+        exit 1;
+    fi
 
-    BUILD_PATH=$BUNDLE_PATH;
-    aws s3 sync $BUILD_PATH "$BUCKET/$BUCKET_PATH/"
+    aws s3 sync $BUILD_PATH "$BUCKET/$BUCKET_PATH/" --acl public-read;
 
 fi
 
