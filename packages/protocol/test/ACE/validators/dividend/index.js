001ad56b6b7a2894b408cd2863d36a381b7cc266
thomas-waite <thomas.waite4@gmail.com>
2020-01-30 23:29:12 +0000

    refactor: add tests for za, zb relations correct labelling of residual and target notes update tests start on tests concerning relation of za, zb to K_MAX

--- packages/protocol/test/ACE/validators/dividend/index.js
+++ packages/protocol/test/ACE/validators/dividend/index.js
@@ -4,6 +4,7 @@ const bn128 = require('@aztec/bn128');
 const { constants } = require('@aztec/dev-utils');
 const secp256k1 = require('@aztec/secp256k1');
 const BN = require('bn.js');
+const sinon = require('sinon');
 const truffleAssert = require('truffle-assertions');
 const { padLeft, randomHex } = require('web3-utils');
 
@@ -16,6 +17,7 @@ Dividend.abi = DividendInterface.abi;
 let dividendValidator;
 const Keccak = keccak;
 const { publicKey } = secp256k1.generateAccount();
+const { K_MAX } = constants;
 
 const getNotes = async (notionalNoteValue, residualNoteValue, targetNoteValue) => {
     const notionalNote = await note.create(publicKey, notionalNoteValue);
@@ -26,15 +28,15 @@ const getNotes = async (notionalNoteValue, residualNoteValue, targetNoteValue) =
 
 const getDefaultNotes = async () => {
     const notionalNoteValue = 90;
-    const residualNoteValue = 4;
-    const targetNoteValue = 50;
+    const residualNoteValue = 50;
+    const targetNoteValue = 4;
     const { notionalNote, residualNote, targetNote } = await getNotes(notionalNoteValue, residualNoteValue, targetNoteValue);
     const za = 100;
     const zb = 5;
     return { notionalNote, residualNote, targetNote, za, zb };
 };
 
-contract.only('Dividend Validator', (accounts) => {
+contract('Dividend Validator', (accounts) => {
     const sender = accounts[0];
 
     before(async () => {
@@ -61,19 +63,66 @@ contract.only('Dividend Validator', (accounts) => {
         });
 
         it('should validate proof when za = 0', async () => {
+            // (k1 * zb) = (k2 * za) + k3
+            // (10 * 2) = (3 * 0) + 20
+            const za = 0;
+            const zb = 2;
 
+            const k1 = 10;
+            const k2 = 3;
+            const k3 = 20;
+            const notionalNote = await note.create(publicKey, k1);
+            const targetNote = await note.create(publicKey, k2);
+            const residualNote = await note.create(publicKey, k3);
+
+            const proof = new DividendProof(notionalNote, residualNote, targetNote, sender, za, zb);
+            proof.challenge = proof.challenge.add(bn128.groupModulus);
+            proof.constructOutputs();
+            const data = proof.encodeABI();
+            const result = await dividendValidator.validateDividend(data, sender, bn128.CRS, { from: sender });
+            expect(result).to.equal(proof.eth.outputs);
         })
 
         it('should validate proof when za = K_MAX', async () => {
+            // (k1 * zb) = (k2 * za) + k3
+            // (4 * K_MAX/2) = (2 * K_MAX) + 0
+            const za = K_MAX;
+            const zb = K_MAX / 2;
 
-        });
+            const k1 = 4;
+            const k2 = 2;
+            const k3 = 0;
+            const notionalNote = await note.create(publicKey, k1);
+            const targetNote = await note.create(publicKey, k2);
+            const residualNote = await note.create(publicKey, k3);
 
-        it('should validate proof when zb = 0', async () => {
-
+            const proof = new DividendProof(notionalNote, residualNote, targetNote, sender, za, zb);
+            proof.challenge = proof.challenge.add(bn128.groupModulus);
+            proof.constructOutputs();
+            const data = proof.encodeABI();
+            const result = await dividendValidator.validateDividend(data, sender, bn128.CRS, { from: sender });
+            expect(result).to.equal(proof.eth.outputs);
         });
 
         it('should validate proof when zb = K_MAX', async () => {
+            // (k1 * zb) = (k2 * za) + k3
+            // (1 * KMAX) = (3 * 3333333) + 0
+            const za = K_MAX / 2; // K_MAX / 3
+            const zb = K_MAX;
 
+            const k1 = 1;
+            const k2 = 2;
+            const k3 = 0;
+            const notionalNote = await note.create(publicKey, k1);
+            const targetNote = await note.create(publicKey, k2);
+            const residualNote = await note.create(publicKey, k3);
+
+            const proof = new DividendProof(notionalNote, residualNote, targetNote, sender, za, zb);
+            proof.challenge = proof.challenge.add(bn128.groupModulus);
+            proof.constructOutputs();
+            const data = proof.encodeABI();
+            const result = await dividendValidator.validateDividend(data, sender, bn128.CRS, { from: sender });
+            expect(result).to.equal(proof.eth.outputs);
         });
     });
 
@@ -187,25 +236,46 @@ contract.only('Dividend Validator', (accounts) => {
             );
         });
 
-        it('should fail for for za > K_MAX', async () => {
-            const { notionalNote, residualNote, targetNote } = await getDefaultNotes();
-            const za = constants.K_MAX + 100;
-            const zb = 5;
+        it('should fail to validate proof when zb = 0', async () => {
+            // (k1 * zb) = (k2 * za) + k3
+            // (10 * 0) = (3 * 0) + 0
+            // results in \bar{k3} = 0, which upon exponentiating will produce a 
+            // point at infinity. This should revert
+            const za = 0;
+            const zb = 0;
+
+            const k1 = 10;
+            const k2 = 3;
+            const k3 = 0;
+            const notionalNote = await note.create(publicKey, k1);
+            const targetNote = await note.create(publicKey, k2);
+            const residualNote = await note.create(publicKey, k3);
+
             const proof = new DividendProof(notionalNote, residualNote, targetNote, sender, za, zb);
+            proof.challenge = proof.challenge.add(bn128.groupModulus);
+            proof.constructOutputs();
             const data = proof.encodeABI();
             await truffleAssert.reverts(
-                dividendValidator.validateDividend(data, sender, bn128.CRS),
+                dividendValidator.validateDividend(data, sender, bn128.CRS, { from: sender }),
                 truffleAssert.ErrorType.REVERT,
             );
         });
 
-        it('should fail for za < 0', async () => {
-            // 0 is the lower bound for za
-            const { notionalNote, residualNote, targetNote } = await getDefaultNotes();
-            const za = -5
-            const zb = 100;
+        it('should fail for za > K_MAX', async () => {
+            // (k1 * zb) = (k2 * za) + k3
+            // (1 * 1) = (0 * K_MAX + 1) + 1
+            //  => K_MAX - 1 = 1 
 
-            // note that the check for 0 < za < K_MAX occcurs at the start of the validator
+            const za = K_MAX + 1;
+            const zb = 1;
+
+            const k1 = 1;
+            const k2 = 0;
+            const k3 = 1;
+            const notionalNote = await note.create(publicKey, k1);
+            const targetNote = await note.create(publicKey, k2);
+            const residualNote = await note.create(publicKey, k3);
+
             const proof = new DividendProof(notionalNote, residualNote, targetNote, sender, za, zb);
             const data = proof.encodeABI();
             await truffleAssert.reverts(
@@ -215,9 +285,19 @@ contract.only('Dividend Validator', (accounts) => {
         });
 
         it('should fail for for zb > K_MAX', async () => {
-            const { notionalNote, residualNote, targetNote } = await getDefaultNotes();
-            const za = 100;
-            const zb = constants.K_MAX + 100;
+            // (k1 * zb) = (k2 * za) + k3
+            // (1 * K_MAX + 100) = (1 * K_MAX - 1) + 101
+            //  => K_MAX - 1 = 1 
+            const za = K_MAX - 1;
+            const zb = K_MAX + 100;
+
+            const k1 = 1;
+            const k2 = 1;
+            const k3 = 101;
+            const notionalNote = await note.create(publicKey, k1);
+            const targetNote = await note.create(publicKey, k2);
+            const residualNote = await note.create(publicKey, k3);
+
             const proof = new DividendProof(notionalNote, residualNote, targetNote, sender, za, zb);
             const data = proof.encodeABI();
             await truffleAssert.reverts(
@@ -226,9 +306,27 @@ contract.only('Dividend Validator', (accounts) => {
             );
         });
 
-        it('should fail for zb < 0', async () => {
-            // 0 is the lower bound for zb
+        it('should fail for large za, zb', async () => {
+            // (k1 * zb) = (k2 * za) + k3
+            // (1 * 4294967293) = (1 * 4294967295) + 0
+            //  => 4294967295 = 4294967295
 
+            const za = 2**32 - 1; // 4294967295
+            const zb = 2**32 - 1; // 4294967295
+
+            const k1 = 1;
+            const k2 = 1;
+            const k3 = 0;
+            const notionalNote = await note.create(publicKey, k1);
+            const targetNote = await note.create(publicKey, k2);
+            const residualNote = await note.create(publicKey, k3);
+
+            const proof = new DividendProof(notionalNote, residualNote, targetNote, sender, za, zb);
+            const data = proof.encodeABI();
+            await truffleAssert.reverts(
+                dividendValidator.validateDividend(data, sender, bn128.CRS),
+                truffleAssert.ErrorType.REVERT,
+            );
         });
 
         it('should fail if malformed proof data', async () => {
