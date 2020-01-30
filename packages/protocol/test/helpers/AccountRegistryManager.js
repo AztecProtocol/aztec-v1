89cbf404d466d50125fcb3cc53171145cb901506
thomas-waite <thomas.waite4@gmail.com>
2020-01-30 18:56:47 +0000

    test(protocol): update tests with AZTECaddress

--- packages/protocol/test/helpers/AccountRegistryManager.js
+++ packages/protocol/test/helpers/AccountRegistryManager.js
@@ -8,12 +8,15 @@ const { ACCOUNT_REGISTRY_SIGNATURE } = devUtils.constants.eip712;
 
 function createSignature(proxyAddress) {
     const { privateKey, address } = secp256k1.generateAccount();
+    const { address: AZTECaddress } = secp256k1.generateAccount();
+
     const linkedPublicKey = keccak256('0x01');
     const spendingPublicKey = keccak256('0x0');
     const domain = signer.generateAccountRegistryDomainParams(proxyAddress);
     const message = {
         account: address,
         linkedPublicKey,
+        AZTECaddress,
     };
 
     const encodedTypedData = typedData.encodeTypedData({
@@ -32,6 +35,7 @@ function createSignature(proxyAddress) {
     return {
         address,
         linkedPublicKey,
+        AZTECaddress,
         spendingPublicKey,
         sig,
     };
