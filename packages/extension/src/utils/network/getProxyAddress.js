150a420cca00768918b8b72136d4de4d3f7ec2b8
Leila Wang <leizciw@gmail.com>
2020-01-30 12:15:57 +0000

    refactor(sdk): return empty address if contract config is incomplete

--- packages/extension/src/utils/network/getProxyAddress.js
+++ packages/extension/src/utils/network/getProxyAddress.js
@@ -4,25 +4,31 @@ import {
 } from '~/utils/log';
 import Web3Service from '~/helpers/Web3Service';
 
-export default async function getProxyContract(contractName, networkId) {
+export default async function getProxyAddress(contractName, networkId) {
     const {
         managerContractName,
     } = contracts[contractName] || {};
     if (!managerContractName) {
-        warnLog(`Contract ${contractName} has no manager in '~/config/contracts'`);
+        warnLog(`Contract '${contractName}' has no manager in '~/config/contracts'`);
+        return '';
     }
+
     const {
         config,
         networks,
     } = contracts[managerContractName] || {};
-
     if (!config) {
-        warnLog(`Contract ${managerContractName} is not defined in '~/config/contracts'`);
+        warnLog(`Contract '${managerContractName}' is not defined in '~/config/contracts'`);
+        return '';
     }
 
     let managerAddress = (networks && networks[networkId]);
     if (!managerAddress && config && config.networks[networkId]) {
         ({ address: managerAddress } = config.networks[networkId]);
+    }
+    if (!managerAddress) {
+        warnLog(`Address of contract '${managerContractName}' is not defined in its artifact.`);
+        return '';
     }
 
     Web3Service.registerInterface(config, { name: managerContractName });
@@ -32,7 +38,6 @@ export default async function getProxyContract(contractName, networkId) {
         .at(managerAddress)
         .method('proxyAddress')
         .call();
-
 
     return proxyAddress;
 }
