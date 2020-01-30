2c6edda9fc62bd583c226a411f2b78b021ec9614
Leila Wang <leizciw@gmail.com>
2020-01-30 18:06:28 +0000

    refactor(sdk): move common contract config to a seperate file

--- packages/extension/src/config/contracts/production.js
+++ packages/extension/src/config/contracts/production.js
@@ -1,41 +1,13 @@
 import AccountRegistry from '~contracts/Behaviour20200106.json';
 import AccountRegistryManager from '~contracts/AccountRegistryManager.json';
-import ACE from '~contracts/IAZTEC.json';
+import IAZTEC from '~contracts/IAZTEC.json';
 import IERC20 from '~contracts/IERC20Mintable.json';
 import IZkAsset from '~contracts/IZkAsset.json';
 
 export default {
-    AccountRegistry: {
-        name: 'AccountRegistry',
-        events: {
-            registerExtension: 'RegisterExtension',
-        },
-        config: AccountRegistry,
-        isProxyContract: true,
-        managerContractName: 'AccountRegistryManager',
-    },
-    AccountRegistryManager: {
-        name: 'AccountRegistryManager',
-        config: AccountRegistryManager,
-    },
-    ACE: {
-        name: 'ACE',
-        events: {
-            сreateNoteRegistry: 'CreateNoteRegistry',
-        },
-        config: ACE,
-    },
-    ZkAsset: {
-        name: 'IZkAsset',
-        events: {
-            createNote: 'CreateNote',
-            updateNoteMetaData: 'UpdateNoteMetaData',
-            destroyNote: 'DestroyNote',
-        },
-        config: IZkAsset,
-    },
-    ERC20: {
-        name: 'ERC20',
-        config: IERC20,
-    },
+    AccountRegistry,
+    AccountRegistryManager,
+    ACE: IAZTEC,
+    ZkAsset: IZkAsset,
+    ERC20: IERC20,
 };
