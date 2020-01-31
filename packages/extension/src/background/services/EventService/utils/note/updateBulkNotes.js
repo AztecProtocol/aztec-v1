f76f82a6bade4d9ebc899bff71869d2242e3fdc5
Leila Wang <leizciw@gmail.com>
2020-01-31 16:15:21 +0000

    fix(sdk): add notes to NoteService after saved to indexedDB successfully

--- packages/extension/src/background/services/EventService/utils/note/updateBulkNotes.js
+++ packages/extension/src/background/services/EventService/utils/note/updateBulkNotes.js
@@ -1,6 +1,37 @@
+import {
+    warnLog,
+} from '~/utils/log';
 import Note from '~/background/database/models/note';
-
+import NoteService from '~/background/services/NoteService';
+import Web3Service from '~/helpers/Web3Service';
 
 export default async function updateBulkNotes(notes, networkId) {
-    return Promise.all(notes.map(note => Note.update(note, { networkId })));
+    const updatedIds = await Promise.all(
+        notes.map(async (note) => {
+            let updated;
+            try {
+                updated = await Note.update(note, { networkId });
+            } catch (e) {
+                updated = '';
+                warnLog('Failed to update note in indexedDB', e);
+            }
+            return updated;
+        }),
+    );
+
+    const updatedNotes = notes.filter(note => updatedIds.indexOf(note.id) >= 0);
+
+    if (updatedNotes.length) {
+        const {
+            address,
+        } = Web3Service.account;
+
+        NoteService.addNotes(
+            networkId,
+            address,
+            updatedNotes.filter(({ owner }) => owner === address),
+        );
+    }
+
+    return updatedNotes;
 }
--- packages/extension/src/background/services/EventService/utils/note/updateNote.js
+++ packages/extension/src/background/services/EventService/utils/note/updateNote.js
@@ -1,6 +1,30 @@
+import {
+    warnLog,
+} from '~/utils/log';
 import Note from '~/background/database/models/note';
-
+import NoteService from '~/background/services/NoteService';
+import Web3Service from '~/helpers/Web3Service';
 
 export default async function updateNote(note, networkId) {
-    return Note.update(note, { networkId });
+    let updated;
+    try {
+        updated = await Note.update(note, { networkId });
+    } catch (e) {
+        warnLog('Failed to update note in indexedDB', e);
+        return null;
+    }
+
+    if (updated) {
+        const {
+            address,
+        } = Web3Service.account;
+
+        NoteService.addNotes(
+            networkId,
+            address,
+            [note],
+        );
+    }
+
+    return updated;
 }
