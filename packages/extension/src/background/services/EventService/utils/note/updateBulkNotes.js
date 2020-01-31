8ca5baf9cf004494c23abbd2fde5c03f6326f2c8
Leila Wang <leizciw@gmail.com>
2020-01-31 16:28:49 +0000

    refactor(sdk): remove redundant index array

--- packages/extension/src/background/services/EventService/utils/note/updateBulkNotes.js
+++ packages/extension/src/background/services/EventService/utils/note/updateBulkNotes.js
@@ -6,20 +6,20 @@ import NoteService from '~/background/services/NoteService';
 import Web3Service from '~/helpers/Web3Service';
 
 export default async function updateBulkNotes(notes, networkId) {
-    const updatedIds = await Promise.all(
+    const updatedNotes = [];
+
+    await Promise.all(
         notes.map(async (note) => {
             let updated;
             try {
                 updated = await Note.update(note, { networkId });
+                updatedNotes.push(note);
             } catch (e) {
-                updated = '';
                 warnLog('Failed to update note in indexedDB', e);
             }
             return updated;
         }),
     );
-
-    const updatedNotes = notes.filter(note => updatedIds.indexOf(note.id) >= 0);
 
     if (updatedNotes.length) {
         const {
