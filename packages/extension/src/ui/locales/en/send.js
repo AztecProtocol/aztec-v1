dcc17e31edee5291e438bf6949e56a45a3850f92
Leila Wang <leizciw@gmail.com>
2020-01-31 18:15:25 +0000

    fix(sdk): change ZkToken and ZkAsset to start with lowercase z

--- packages/extension/src/ui/locales/en/send.js
+++ packages/extension/src/ui/locales/en/send.js
@@ -1,7 +1,7 @@
 export default {
-    title: 'Send ZkTokens',
+    title: 'Send zkTokens',
     approve: {
-        description: `A signature is required to send ZkTokens.
+        description: `A signature is required to send zkTokens.
             The SDK will pick the most suitable notes for the transaction.
             Check the transaction details are correct before proceeding.
         `,
@@ -9,7 +9,7 @@ export default {
     },
     approved: 'Approved',
     sign: {
-        description: `A MetaMask signature is required to send ZkTokens.
+        description: `A MetaMask signature is required to send zkTokens.
             The signature should contain the following values:
         `,
     },
@@ -19,7 +19,7 @@ export default {
         `,
     },
     send: {
-        step: 'Sending ZkTokens',
+        step: 'Sending zkTokens',
         description: `The SDK has picked the most suitable notes for this transaction.
             If everything looks good hit send!
         `,
