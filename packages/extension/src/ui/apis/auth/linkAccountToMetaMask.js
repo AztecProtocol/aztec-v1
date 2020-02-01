cfccbb016243d4cc3c5a8cce747fd933dc1b5dd8
Joe Andrews <joe@fuuzik.com>
2020-02-01 01:27:38 +0000

    feat(sdk): fix register bug

--- packages/extension/src/ui/apis/auth/linkAccountToMetaMask.js
+++ packages/extension/src/ui/apis/auth/linkAccountToMetaMask.js
@@ -1,16 +1,13 @@
 import secp256k1 from '@aztec/secp256k1';
 import decodePrivateKey from '~/background/utils/decodePrivateKey';
-import AuthService from '~/background/services/AuthService';
 import ConnectionService from '~/ui/services/ConnectionService';
 
 export default async function linkAccountToMetaMask({
     address,
     linkedPublicKey,
+    keyStore,
+    pwDerivedKey,
 }) {
-    const keyStore = await AuthService.getKeyStore();
-    const {
-        pwDerivedKey,
-    } = await AuthService.getSession(address) || {};
     const privateKey = '0x'.concat(decodePrivateKey(keyStore, pwDerivedKey));
     const {
         address: aliasAddress,
