7e84f74759aa02c9eb7edef404990700b86306d8
thomas-waite <thomas.waite4@gmail.com>
2020-02-24 17:13:27 +0000

    test(protocol): check that legacy deposit() still callable add failure test

--- packages/protocol/test/AccountRegistry/epochs/20200220/Behaviour20200220.js
+++ packages/protocol/test/AccountRegistry/epochs/20200220/Behaviour20200220.js
@@ -6,18 +6,18 @@ const {
 } = require('aztec.js');
 const { generateAccount } = require('@aztec/secp256k1');
 const truffleAssert = require('truffle-assertions');
-const { randomHex } = require('web3-utils');
+const { randomHex, toChecksumAddress } = require('web3-utils');
 
 const ACE = artifacts.require('./ACE');
 const AccountRegistryManager = artifacts.require('./AccountRegistry/AccountRegistryManager');
 const Behaviour20200106 = artifacts.require('./Behaviour20200106/epochs/20200106/Behaviour20200106');
 const Behaviour20200220 = artifacts.require('./Behaviour20200106/epochs/20200106/Behaviour20200220');
-const DAI = artifacts.require('./test/ERC20/DAI/dai');
+const DAI = artifacts.require('./test/ERC20/DAI/Dai');
 const ZkAsset = artifacts.require('ZkAssetOwnable');
 
 const standardAccount = require('../../../helpers/getOwnerAccount');
 
-contract.only('Behaviour20200220', async (accounts) => {
+contract('Behaviour20200220', async (accounts) => {
     let behaviour20200220;
     let manager;
     let proxyContract;
@@ -30,9 +30,6 @@ contract.only('Behaviour20200220', async (accounts) => {
 
     // Signature params
     const chainID = 4;
-    const nonce = 0;
-    const expiry = -1; // a massive number in the contract
-    const allowed = true;
 
     beforeEach(async () => {
         ace = await ACE.deployed();
@@ -69,8 +66,12 @@ contract.only('Behaviour20200220', async (accounts) => {
 
     describe('Success states', async () => {
         it('should update allowance using direct DAI.permit() call', async () => {
+            const nonce = 0;
+            const expiry = -1; // a massive number in the contract
+            const allowed = true;
+
             const holderAccount = generateAccount();
-            const { address: holderAddress } = holderAccount;
+            const { address: userAddress } = holderAccount;
             const spender = randomHex(20);
             const verifyingContract = dai.address;
 
@@ -80,34 +81,76 @@ contract.only('Behaviour20200220', async (accounts) => {
             const s = `0x${permitSig.slice(64, 128)}`;
             const v = `0x${permitSig.slice(128, 130)}`;
 
-            const { receipt } = await dai.permit(holderAddress, spender, nonce, expiry, allowed, v, r, s);
+            const { receipt } = await dai.permit(userAddress, spender, nonce, expiry, allowed, v, r, s);
             expect(receipt.status).to.equal(true);
 
-            const allowance = await dai.allowance.call(holderAddress, spender);
+            const allowance = await dai.allowance.call(userAddress, spender);
             expect(allowance.toString()).to.not.equal('0');
         });
 
+        it('should emit Approval event when permit() is called', async () => {
+            const nonce = 0;
+            const expiry = -1; // a massive number in the contract
+            const allowed = true;
+
+            const holderAccount = generateAccount();
+            const { address: userAddress } = holderAccount;
+            const spender = randomHex(20);
+            const verifyingContract = dai.address;
+
+            const permitSig = signPermit(chainID, verifyingContract, holderAccount, spender, nonce, expiry, allowed).slice(2);
+
+            const r = `0x${permitSig.slice(0, 64)}`;
+            const s = `0x${permitSig.slice(64, 128)}`;
+            const v = `0x${permitSig.slice(128, 130)}`;
+
+            const { receipt } = await dai.permit(userAddress, spender, nonce, expiry, allowed, v, r, s);
+            const event = receipt.logs.find((l) => l.event === 'Approval');
+
+            const emittedUserAddress = event.args.src;
+            const emittedSpenderAddress = event.args.guy;
+
+            expect(emittedUserAddress).to.equal(toChecksumAddress(userAddress));
+            expect(emittedSpenderAddress).to.equal(toChecksumAddress(spender));
+        });
+
         it('should update allowance using indirect proxyContract.permit() call', async () => {
+            const nonce = 0;
+            const expiry = -1; // a massive number in the contract
+            const allowed = true;
+
             const linkedTokenAddress = dai.address;
             const holderAccount = generateAccount();
-            const { address: holderAddress } = holderAccount;
+            const { address: userAddress } = holderAccount;
 
             const spender = proxyContract.address;
             const signature = signPermit(chainID, linkedTokenAddress, holderAccount, spender, nonce, expiry, allowed);
 
-            const { receipt } = await proxyContract.permit(linkedTokenAddress, holderAddress, nonce, signature);
+            const { receipt } = await proxyContract.permit(
+                linkedTokenAddress,
+                userAddress,
+                nonce,
+                allowed,
+                expiry,
+                spender,
+                signature,
+            );
             expect(receipt.status).to.equal(true);
 
-            const allowance = await dai.allowance.call(holderAddress, spender);
+            const allowance = await dai.allowance.call(userAddress, spender);
             expect(allowance.toString()).to.not.equal('0');
         });
 
         it('should perform deposit, making use of permit() call, without calling approve()', async () => {
-            const { publicKey, address: holderAddress } = standardAccount;
+            const nonce = 0;
+            const expiry = -1; // a massive number in the contract
+            const allowed = true;
+
+            const { publicKey, address: userAddress } = standardAccount;
 
             // mint DAI tokens
             const tokensMinted = 500;
-            await dai.mint(holderAddress, tokensMinted, opts);
+            await dai.mint(userAddress, tokensMinted, opts);
 
             const inputNotes = [];
             const outputNotes = [await note.create(publicKey, 10), await note.create(publicKey, 5)];
@@ -119,7 +162,7 @@ contract.only('Behaviour20200220', async (accounts) => {
             const spender = proxyContract.address;
             const signature = signPermit(chainID, dai.address, standardAccount, spender, nonce, expiry, allowed);
 
-            expect((await dai.balanceOf(holderAddress)).toNumber()).to.equal(tokensMinted);
+            expect((await dai.balanceOf(userAddress)).toNumber()).to.equal(tokensMinted);
             expect((await dai.balanceOf(ace.address)).toNumber()).to.equal(0);
             expect((await dai.balanceOf(proxyContract.address)).toNumber()).to.equal(0);
 
@@ -128,18 +171,54 @@ contract.only('Behaviour20200220', async (accounts) => {
             const proofHash = depositProof.hash;
             const depositAmount = publicValue * -1;
 
-            const { receipt } = await proxyContract.deposit(
+            const { receipt } = await proxyContract.methods[
+                'deposit(address,address,bytes32,bytes,uint256,bytes,uint256)'
+            ](zkAsset.address, userAddress, proofHash, proofData, depositAmount, signature, nonce, { from: userAddress });
+
+            expect(receipt.status).to.equal(true);
+            expect((await dai.balanceOf(userAddress)).toNumber()).to.equal(tokensMinted - depositAmount);
+            expect((await dai.balanceOf(ace.address)).toNumber()).to.equal(depositAmount);
+            expect((await dai.balanceOf(proxyContract.address)).toNumber()).to.equal(0);
+        });
+
+        it('should be able to call old Behaviour20200106.deposit() function for alternative approve flow', async () => {
+            const { publicKey, address: userAddress } = standardAccount;
+
+            // mint DAI tokens
+            const tokensMinted = 500;
+            await dai.mint(userAddress, tokensMinted, opts);
+
+            const inputNotes = [];
+            const outputNotes = [await note.create(publicKey, 10), await note.create(publicKey, 5)];
+            const publicValue = -15;
+            const sender = proxyContract.address;
+            const publicOwner = proxyContract.address;
+
+            const depositProof = new JoinSplitProof(inputNotes, outputNotes, sender, publicValue, publicOwner);
+
+            expect((await dai.balanceOf(userAddress)).toNumber()).to.equal(tokensMinted);
+            expect((await dai.balanceOf(ace.address)).toNumber()).to.equal(0);
+            expect((await dai.balanceOf(proxyContract.address)).toNumber()).to.equal(0);
+
+            // Note: No er20.approve() is being made, relying on permit() call in deposit() instead
+            const proofData = depositProof.encodeABI(zkAsset.address);
+            const proofHash = depositProof.hash;
+            const depositAmount = publicValue * -1;
+
+            // legacy flow - call dai.approve()
+            await dai.approve(proxyContract.address, depositAmount, { from: userAddress });
+
+            const { receipt } = await proxyContract.methods['deposit(address,address,bytes32,bytes,uint256)'](
                 zkAsset.address,
-                holderAddress,
+                userAddress,
                 proofHash,
                 proofData,
                 depositAmount,
-                signature,
-                nonce,
-                { from: holderAddress },
+                { from: userAddress },
             );
+
             expect(receipt.status).to.equal(true);
-            expect((await dai.balanceOf(holderAddress)).toNumber()).to.equal(tokensMinted - depositAmount);
+            expect((await dai.balanceOf(userAddress)).toNumber()).to.equal(tokensMinted - depositAmount);
             expect((await dai.balanceOf(ace.address)).toNumber()).to.equal(depositAmount);
             expect((await dai.balanceOf(proxyContract.address)).toNumber()).to.equal(0);
         });
@@ -147,11 +226,12 @@ contract.only('Behaviour20200220', async (accounts) => {
 
     describe('Failure states', async () => {
         it('should fail to perform deposit if permit signature is fake', async () => {
-            const { publicKey, address: holderAddress } = standardAccount;
+            const nonce = 0;
+            const { publicKey, address: userAddress } = standardAccount;
 
             // mint DAI tokens
             const tokensMinted = 500;
-            await dai.mint(holderAddress, tokensMinted, opts);
+            await dai.mint(userAddress, tokensMinted, opts);
 
             const inputNotes = [];
             const outputNotes = [await note.create(publicKey, 10), await note.create(publicKey, 5)];
@@ -162,20 +242,70 @@ contract.only('Behaviour20200220', async (accounts) => {
             const depositProof = new JoinSplitProof(inputNotes, outputNotes, sender, publicValue, publicOwner);
             const signature = randomHex(65);
 
-            expect((await dai.balanceOf(holderAddress)).toNumber()).to.equal(tokensMinted);
+            expect((await dai.balanceOf(userAddress)).toNumber()).to.equal(tokensMinted);
             expect((await dai.balanceOf(ace.address)).toNumber()).to.equal(0);
             expect((await dai.balanceOf(proxyContract.address)).toNumber()).to.equal(0);
 
-            // Note: No er20.approve() is being made, relying on permit() call in deposit() instead
             const proofData = depositProof.encodeABI(zkAsset.address);
             const proofHash = depositProof.hash;
             const depositAmount = publicValue * -1;
 
             await truffleAssert.reverts(
-                proxyContract.deposit(zkAsset.address, holderAddress, proofHash, proofData, depositAmount, signature, nonce, {
-                    from: holderAddress,
-                }),
+                proxyContract.methods['deposit(address,address,bytes32,bytes,uint256,bytes,uint256)'](
+                    zkAsset.address,
+                    userAddress,
+                    proofHash,
+                    proofData,
+                    depositAmount,
+                    signature,
+                    nonce,
+                    { from: userAddress },
+                ),
                 'Dai/invalid-permit',
+            );
+        });
+
+        it('should fail to perform deposit if nonce is incorrect', async () => {
+            // incorrect nonce is defined as one which != nonces[user]++
+            const nonce = 5;
+            const expiry = -1; // a massive number in the contract
+            const allowed = true;
+            const { publicKey, address: userAddress } = standardAccount;
+
+            // mint DAI tokens
+            const tokensMinted = 500;
+            await dai.mint(userAddress, tokensMinted, opts);
+
+            const inputNotes = [];
+            const outputNotes = [await note.create(publicKey, 10), await note.create(publicKey, 5)];
+            const publicValue = -15;
+            const sender = proxyContract.address;
+            const publicOwner = proxyContract.address;
+
+            const depositProof = new JoinSplitProof(inputNotes, outputNotes, sender, publicValue, publicOwner);
+            const spender = proxyContract.address;
+            const signature = signPermit(chainID, dai.address, standardAccount, spender, nonce, expiry, allowed);
+
+            expect((await dai.balanceOf(userAddress)).toNumber()).to.equal(tokensMinted);
+            expect((await dai.balanceOf(ace.address)).toNumber()).to.equal(0);
+            expect((await dai.balanceOf(proxyContract.address)).toNumber()).to.equal(0);
+
+            const proofData = depositProof.encodeABI(zkAsset.address);
+            const proofHash = depositProof.hash;
+            const depositAmount = publicValue * -1;
+
+            await truffleAssert.reverts(
+                proxyContract.methods['deposit(address,address,bytes32,bytes,uint256,bytes,uint256)'](
+                    zkAsset.address,
+                    userAddress,
+                    proofHash,
+                    proofData,
+                    depositAmount,
+                    signature,
+                    nonce,
+                    { from: userAddress },
+                ),
+                'Dai/invalid-nonce',
             );
         });
     });
