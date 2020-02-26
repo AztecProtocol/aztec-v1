55f9abe1143342acb4cc876e63eabc8c5ffafc48
Joe Andrews <joe@fuuzik.com>
2020-02-26 21:47:20 +0000

    feat(protocol): add promo manager

---
+++ packages/protocol/contracts/PromoManager/PromoMananger.sol
@@ -0,0 +1,134 @@
+
+
+pragma solidity >=0.5.0 <0.6.0;
+
+import "@aztec/protocol/contracts/interfaces/IACE.sol";
+import "@aztec/protocol/contracts/interfaces/IZkAsset.sol";
+import "@aztec/protocol/contracts/libs/NoteUtils.sol";
+import "@aztec/protocol/contracts/AccountRegistry/GSNRecipientTimestampSignature.sol";
+
+import "@openzeppelin/contracts-ethereum-package/contracts/GSN/Context.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import "../interfaces/IERC20Mintable.sol";
+
+/**
+ * @title InterestPool implementation
+ * @author AZTEC
+ * Note the behaviour contract version naming convention is based on the date on which the contract
+ * was created, in the format: YYYYMMDD
+ * 
+ * Copyright 2020 Spilsbury Holdings Ltd 
+ *
+ * Licensed under the GNU Lesser General Public Licence, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **/
+contract PromoManager is GSNRecipientTimestampSignature {
+    using NoteUtils for bytes;
+    using SafeMath for uint256;
+
+
+
+    mapping(bytes32 => bool) public codeRedemptions;
+    mapping(bytes32 => bytes32) public codeToTotalNotes;
+    mapping(bytes32 => address) public userCommitToCode;
+
+    event GSNTransactionProcessed(bytes32 indexed signatureHash, bool indexed success, uint actualCharge);
+    event LogAddress(address conrtrac);
+    event LogBytes(bytes32 bb);
+
+    IACE ace;
+
+    uint24 JOIN_SPLIT_PROOF = 65793;
+
+    IERC20Mintable DAI;
+    IZkAsset zkDAI;
+    bytes32 unallocatedNoteHash;
+    struct Note {
+        address owner;
+        bytes32 noteHash;
+    }
+
+
+    function _noteCoderToStruct(bytes memory note) internal pure returns (Note memory codedNote) {
+        (address owner, bytes32 noteHash,) = note.extractNote();
+        return Note(owner, noteHash );
+    }
+
+
+
+    function initialize(address _aceAddress, address _trustedGSNSignerAddress, address _daiAddress, address
+                        _zkDaiAddress, bytes32 _unallocatedNoteHash) initializer public {
+        GSNRecipientTimestampSignature.initialize(_trustedGSNSignerAddress);
+        DAI = IERC20Mintable(_daiAddress);
+        zkDAI = IZkAsset(_zkDaiAddress);
+        ace = IACE(_aceAddress);
+        unallocatedNoteHash = _unallocatedNoteHash;
+    }
+
+
+    /**
+     * @dev Sets a mapping of a code hash to a noteHash 
+     **/
+
+    function setCodes(bytes32[] memory _codeHashes, bytes memory _proofData) public {
+        (bytes memory _proofOutputs) = ace.validateProof(JOIN_SPLIT_PROOF, address(this), _proofData);
+        // inputNotes1 is the un allocated balance
+        (bytes memory _proofInputNotes, bytes memory _proofOutputNotes, ,) = _proofOutputs.get(0).extractProofOutput();
+        for (uint i = 0; i < _codeHashes.length; i += 1) {
+            codeToTotalNotes[_codeHashes[i]] = _noteCoderToStruct(_proofOutputNotes.get(i.add(1))).noteHash;
+        }
+        require(_noteCoderToStruct(_proofInputNotes.get(0)).noteHash == unallocatedNoteHash, 'hash incorrect');
+
+        // zkDAI.confidentialApprove(_noteCoderToStruct(_proofInputNotes.get(0)).noteHash, address(this), true, '');
+        // zkDAI.confidentialApprove(_noteCoderToStruct(_proofInputNotes.get(0)).noteHash, address(zkDAI), true, '');
+        zkDAI.confidentialTransferFrom(JOIN_SPLIT_PROOF, _proofOutputs.get(0));
+        // unallocatedNoteHash = _noteCoderToStruct(_proofOutputNotes.get(0)).noteHash;
+    }
+
+    function claim1(bytes32 _codeHash) public {
+        require(address(userCommitToCode[_codeHash]) == address(0));
+        userCommitToCode[_codeHash] = _msgSender();
+    }
+
+    function claim2(string memory _code, uint256 _challenge, bytes memory _proofData) public {
+        bytes32 codeCommitHash = keccak256(abi.encode(_code, _challenge, _msgSender()));
+        bytes32 codeHash = keccak256(abi.encode(_code));
+        emit LogBytes(codeHash);
+        require(userCommitToCode[codeCommitHash] == _msgSender(), 'code');
+        require(!codeRedemptions[codeHash], 'code redeemed');
+        codeRedemptions[codeHash] = true;
+
+        (bytes memory _proofOutputs) = ace.validateProof(JOIN_SPLIT_PROOF, address(this), _proofData);
+        (bytes memory _proofInputNotes, bytes memory _proofOutputNotes, ,) = _proofOutputs.get(0).extractProofOutput();
+        // here we cheeck that proof input notes == 
+        require(codeToTotalNotes[codeHash] == _noteCoderToStruct(_proofInputNotes.get(0)).noteHash, 'bad note');
+        require(_proofInputNotes.getLength() == 1, 'bad length');
+        // zkDAI.confidentialApprove(codeToTotalNotes[codeHash], address(this), true, '');
+        zkDAI.confidentialTransferFrom(JOIN_SPLIT_PROOF, _proofOutputs.get(0));
+    }
+
+
+    /**
+     * @dev Emits an event, annoucing that the relayed call has been successfully executed
+     * @param context - second argument in the tuple returned by acceptRelayedCall
+     * @param success - bool specifying whether the relayed call was successfully executed
+     * @param actualCharge - estimate of the transaction gas cost
+     * @param preRetVal - the return value of preRelayedCall
+     */
+
+    function _postRelayedCall(bytes memory context, bool success, uint256 actualCharge, bytes32 preRetVal) internal {
+        (bytes memory approveData) = abi.decode(context, (bytes));
+        emit GSNTransactionProcessed(keccak256(approveData), success, actualCharge);
+    }
+
+
+}
---
+++ packages/protocol/test/PromoMananger/PromoManager.spec.js
@@ -0,0 +1,192 @@
+/* global artifacts, expect, contract, beforeEach, web3, it:true */
+
+const truffleAssert = require('truffle-assertions');
+const { keccak256, randomHex } = require('web3-utils');
+const Web3EthAbi = require('web3-eth-abi');
+const BN = require('bn.js');
+const { JoinSplitProof, signer, note,
+} = require('aztec.js');
+
+const PromoManager = artifacts.require('./PromoManager/PromoManager');
+const ERC20Mintable = artifacts.require('./ERC20Mintable');
+const ACE = artifacts.require('./ACE');
+const secp256k1 = require('@aztec/secp256k1');
+const helpers = require('../helpers/ERC1724');
+
+const ZkAsset = artifacts.require('./ZkAsset');
+
+contract('PromoManager', async (accounts) => {
+    const trustedGSNSignerAddress = randomHex(20);
+    let ace;
+    let erc20;
+    const publicOwner = accounts[0];
+    const scalingFactor = new BN(10);
+    const tokensTransferred = new BN(100000);
+    const sender = accounts[0];
+
+    beforeEach(async () => {
+        ace = await ACE.at(ACE.address);
+        erc20 = await ERC20Mintable.new({ from: accounts[0] });
+        console.log('doing first before');
+    });
+
+    describe.only('Success states', async () => {
+        let zkAsset;
+        let initialBehaviourAddress;
+        const managerAccount = secp256k1.generateAccount();
+        const scalingFactor = new BN(10);
+        let fundNote;
+        const totalValue = 100;
+        let promoManager;
+
+        beforeEach(async () => {
+            console.log('doing second before');
+            const opts = { from: accounts[0], gas: 4700000 };
+
+            // [fundNote] = await helpers.getNotesForAccount(managerAccount, [totalValue]);
+            fundNote = await note.create(managerAccount.publicKey, 100);
+            // fundNote.owner = managerAccount.address;
+            zkAsset = await ZkAsset.new(ace.address, erc20.address, scalingFactor);
+            // fundNote.owner = promoManager.address;
+            await erc20.mint(accounts[0], scalingFactor.mul(tokensTransferred), opts);
+            await erc20.approve(ace.address, scalingFactor.mul(tokensTransferred), opts);
+
+            const proof = new JoinSplitProof([], [fundNote], accounts[0], -totalValue, accounts[0]);
+            await ace.publicApprove(zkAsset.address, proof.hash, totalValue, { from: accounts[0] });
+            const data = proof.encodeABI(zkAsset.address);
+            const { receipt: firstProofReceipt } = await zkAsset.methods['confidentialTransfer(bytes,bytes)'](data, '0x', {
+                from: accounts[0],
+            });
+            expect(firstProofReceipt.status).to.equal(true);
+
+            promoManager = await PromoManager.new(ace.address, managerAccount.address, erc20.address, zkAsset.address, fundNote.noteHash);
+        });
+        describe('Initialisation', async () => {
+
+            it('should initialise with a note hash', async () => {
+
+                const {receipt} = await promoManager.initialize(ace.address, managerAccount.address,erc20.address, zkAsset.address, fundNote.noteHash);
+                expect(receipt.status).to.equal(true);
+
+            });
+
+            it('should be able to call setCodes', async () => {
+                await promoManager.initialize(ace.address, managerAccount.address, erc20.address, zkAsset.address, fundNote.noteHash);
+                const [code1,remainder] = await helpers.getNotesForAccount(managerAccount, [10, 90]);
+                // code1.owner = promoManager.address;
+                // remainder.owner = promoManager.address;
+                const proofSetCodes = new JoinSplitProof([fundNote], [remainder, code1], promoManager.address, 0 ,accounts[0]);
+                console.log('signNote',
+
+                    zkAsset.address,
+                    fundNote.noteHash,
+                    promoManager.address,
+                    true,
+                    managerAccount.privateKey,
+                );
+                const signature = signer.signNoteForConfidentialApprove(
+                    zkAsset.address,
+                    fundNote.noteHash,
+                    promoManager.address,
+                    true,
+                    managerAccount.privateKey,
+                );
+
+                console.log(zkAsset.address, promoManager.address);
+
+
+                const { receipt: approveReceipt } = await zkAsset.confidentialApprove(fundNote.noteHash, promoManager.address,true ,signature);
+                console.log(approveReceipt);
+                const loggedApprovalStatus = await zkAsset.confidentialApproved.call(fundNote.noteHash, promoManager.address);
+                expect(loggedApprovalStatus).to.equal(true);
+                expect(approveReceipt.status).to.equal(true);
+                const proofDataSetCodes = proofSetCodes.encodeABI(promoManager.address);
+
+                // spend note
+
+                const {receipt} = await promoManager.setCodes([keccak256(Web3EthAbi.encodeParameters(['string'], ['1234']))], proofDataSetCodes);
+                expect(receipt.status).to.equal(true);
+
+            });
+
+            it('should be able to call claim1 a code', async () => {
+
+                await promoManager.initialize(ace.address, managerAccount.address, erc20.address, zkAsset.address, fundNote.noteHash);
+                const [code1,remainder] = await helpers.getNotesForAccount(managerAccount, [10, 90]);
+                // code1.owner = promoManager.address;
+                // remainder.owner = promoManager.address;
+                const proofSetCodes = new JoinSplitProof([fundNote], [code1, remainder], promoManager.address,0 ,accounts[0]);
+                console.log('signNote',
+
+                    zkAsset.address,
+                    fundNote.noteHash,
+                    promoManager.address,
+                    true,
+                    managerAccount.privateKey,
+                );
+                const signature = signer.signNoteForConfidentialApprove(
+                    zkAsset.address,
+                    fundNote.noteHash,
+                    promoManager.address,
+                    true,
+                    managerAccount.privateKey,
+                );
+
+                console.log(zkAsset.address, promoManager.address);
+
+
+                const { receipt: approveReceipt } = await zkAsset.confidentialApprove(fundNote.noteHash, promoManager.address,true ,signature);
+                console.log(approveReceipt);
+                const loggedApprovalStatus = await zkAsset.confidentialApproved.call(fundNote.noteHash, promoManager.address);
+                expect(loggedApprovalStatus).to.equal(true);
+
+                const proofDataSetCodes = proofSetCodes.encodeABI(promoManager.address);
+
+                // spend note
+
+                const {receipt} = await promoManager.setCodes([keccak256(Web3EthAbi.encodeParameter('string', '1234'))], proofDataSetCodes);
+                expect(receipt.status).to.equal(true);
+
+                const {receipt: receipt2} = await promoManager.claim1(keccak256(Web3EthAbi.encodeParameters(['string', 'uint256', 'address'], ['1234', 55, accounts[0]])));
+                expect(receipt2.status).to.equal(true);
+
+            });
+            it.only('should be able to call claim2 a code', async () => {
+
+                await promoManager.initialize(ace.address, managerAccount.address, erc20.address, zkAsset.address, fundNote.noteHash);
+                const [code1,remainder] = await helpers.getNotesForAccount(managerAccount, [10, 90]);
+                const proofSetCodes = new JoinSplitProof([fundNote], [remainder, code1], promoManager.address,0 ,accounts[0]);
+                const proofDataSetCodes = proofSetCodes.encodeABI(promoManager.address);
+                const signature = signer.signNoteForConfidentialApprove(
+                    zkAsset.address,
+                    fundNote.noteHash,
+                    promoManager.address,
+                    true,
+                    managerAccount.privateKey,
+                );
+                await zkAsset.confidentialApprove(fundNote.noteHash, promoManager.address,true ,signature);
+                const signature = signer.signNoteForConfidentialApprove(
+                    zkAsset.address,
+                    code1.noteHash,
+                    promoManager.address,
+                    true,
+                    managerAccount.privateKey,
+                );
+                await zkAsset.confidentialApprove(code1.noteHash, promoManager.address,true ,signature);
+
+                const {receipt} = await promoManager.setCodes([keccak256(Web3EthAbi.encodeParameters(['string'], ['1234']))], proofDataSetCodes);
+                expect(receipt.status).to.equal(true);
+
+                const {receipt: receipt2} = await promoManager.claim1(keccak256(Web3EthAbi.encodeParameters(['string', 'uint256', 'address'], ['1234', 55, accounts[0]])));
+
+                expect(receipt2.status).to.equal(true);
+                const [code1New] = await helpers.getNotesForAccount(managerAccount, [10]);
+                const proof2 = new JoinSplitProof([code1], [code1New], promoManager.address,0 ,accounts[0]);
+                const proofData2 = proof2.encodeABI(promoManager.address);
+                const {receipt: receipt3} = await promoManager.claim2('1234', 55, proofData2);
+                expect(receipt3.status).to.equal(true);
+            });
+        });
+    });
+});
+
