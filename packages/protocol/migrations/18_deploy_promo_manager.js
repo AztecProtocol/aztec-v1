1fabdab32ce7baead6158cf079e7fdb149a21a73
Joe Andrews <joe@fuuzik.com>
2020-02-27 17:45:41 +0000

    feat(protocol): add migration script

--- packages/protocol/migrations/18_deploy_promo_manager.js
+++ packages/protocol/migrations/18_deploy_promo_manager.js
@@ -5,20 +5,20 @@ const { getContractAddressesForNetwork, NetworkId: networkIds } = require('@azte
 dotenv.config();
 const PromoManager = artifacts.require('./PromoManager/PromoManager');
 const ACE = artifacts.require('./ACE');
+const ZkAsset = artifacts.require('./ZkAsset');
 const ERC20 = artifacts.require('./ERC20Mintable');
 
-module.exports = (deployer, network) => {
-    return deployer.deploy(PromoManager).then(async (promoManager) => {
+module.exports = async (deployer, network) => {
+
+    ace = await ACE.deployed();
+    erc20 = await ERC20.deployed();
+    zkAsset = await ZkAsset.deployed();
+    return deployer.deploy(PromoManager, ace.address, zkAsset.address).then(async (promoManager) => {
 
         // Set the GSN signer address
-        ace = await ACE.deployed();
-        erc20 = await ERC20.deployed();
         await promoManager.initialize(
-            ace.address,
-            process.env.LOCAL_TRUSTED_GSN_SIGNER_ADDRESS,
-            erc20.address,
-            zkAsset.address,
             process.env.PROMO_NOTE_HASH,
+            process.env.LOCAL_TRUSTED_GSN_SIGNER_ADDRESS,
         );
     });
 };
